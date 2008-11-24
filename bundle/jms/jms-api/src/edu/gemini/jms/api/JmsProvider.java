d5099cc43f886fd9e2a6921460f4a52dc252108a
anunez <anunez@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2008-11-24 20:52:09 +0000

    Added documentation
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@15643 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- bundle/jms/jms-api/src/edu/gemini/jms/api/JmsProvider.java
+++ bundle/jms/jms-api/src/edu/gemini/jms/api/JmsProvider.java
@@ -3,10 +3,17 @@ package edu.gemini.jms.api;
 import javax.jms.ConnectionFactory;
 
 /**
- *
+ * Provides access to the factory to construct
+ * JMS objects, hidding the details of the specific JMS
+ * provider in use
  */
 public interface JmsProvider {
 
+    /**
+     * Gets the connection factory associated
+     * to the current JMS provider
+     * @return a JMS ConnectionFactory
+     */
     ConnectionFactory getConnectionFactory();
     
 }
