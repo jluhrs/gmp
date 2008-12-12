5509ad5fdb86ee10db06933b8ef5f183d27bcfc2
anunez <anunez@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2008-12-12 14:37:20 +0000

    First version of the Services bundle
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@15809 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- bundle/giapi-gmp/gmp-util/src/edu/gemini/aspen/gmp/util/jms/GmpKeys.java
+++ bundle/giapi-gmp/gmp-util/src/edu/gemini/aspen/gmp/util/jms/GmpKeys.java
@@ -5,11 +5,25 @@ package edu.gemini.aspen.gmp.util.jms;
  */
 public class GmpKeys {
 
+    //Common keys
+    public final static String GMP_PREFIX  = "GMP";
+    public final static String GMP_SEPARATOR = ":";
+
+
     //Handler Response Keys
     public final static String GMP_HANDLER_RESPONSE_KEY = "GMP_HANDLER_RESPONSE";
     public final static String GMP_HANDLER_RESPONSE_ERROR_KEY = "GMP_HANDLER_RESPONSE_ERROR";
 
     public final static String GMP_SEQUENCE_COMMAND_KEY = "GMP_SEQUENCE_COMMAND";
     public final static String GMP_ACTIVITY_KEY = "GMP_ACTIVITY";
+
+
+    //Services Requests keys
+    public final static String GMP_UTIL_REQUEST_DESTINATION = GMP_PREFIX + GMP_SEPARATOR + "UTIL_REQUEST_DESTINATION";
+    public final static String GMP_UTIL_REQUEST_TYPE = "REQUEST_TYPE";
+    public final static int GMP_UTIL_REQUEST_PROPERTY = 0;
+    public final static String GMP_UTIL_PROPERTY = "PROPERTY";
+    
+
 
 }
