438f68cd62731d34f54cc45cec5be3b867b5834c
anunez <anunez@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2008-12-15 19:54:20 +0000

    Introduced Service interface, and some extra refactoring to the Service Bundle
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@16253 ee6ba543-07f8-4773-b02c-01d50e9d00ba

---
+++ bundle/giapi-gmp/gmp-services/src/edu/gemini/aspen/gmp/services/properties/PropertyHolder.java
@@ -0,0 +1,18 @@
+package edu.gemini.aspen.gmp.services.properties;
+
+/**
+ * Storage infraestructure for properties. 
+ */
+public interface PropertyHolder {
+
+    /**
+     * Get the value associated to the given property.
+     * 
+     * @param key The property key
+     * @return Value associated to the property key, or
+     * <code>null</code> if there is no property associated
+     * to the key.
+     */
+    String getProperty(String key);
+
+}
--- bundle/giapi-gmp/gmp-services/src/edu/gemini/aspen/gmp/services/properties/PropertyService.java
+++ bundle/giapi-gmp/gmp-services/src/edu/gemini/aspen/gmp/services/properties/PropertyService.java
@@ -1,22 +1,46 @@
 package edu.gemini.aspen.gmp.services.properties;
 
-import java.util.Map;
+import edu.gemini.aspen.gmp.services.core.JmsService;
+import edu.gemini.aspen.gmp.services.core.ServiceType;
+import edu.gemini.aspen.gmp.util.jms.GmpKeys;
+
+import javax.jms.*;
+import java.util.logging.Logger;
 
 /**
  *
  */
-public class PropertyService {
+public class PropertyService extends JmsService {
 
+    private static final Logger LOG = Logger.getLogger(PropertyService.class.getName());
 
-    private final Map<String, String> _properties;
-    
-    public PropertyService(PropertyConfig config) {
-        _properties = config.getProperties();
+    private PropertyHolder _properties;
+
+    public PropertyService(PropertyHolder holder) {
+        _properties = holder;
     }
 
-    public String getProperty(String key) {
-        return _properties.get(key);
+    public void process(MapMessage message) throws JMSException {
+
+        if (message == null) return;
+
+        String key = message.getString(GmpKeys.GMP_UTIL_PROPERTY);
+
+        String reply = _properties.getProperty(key);
+
+        Destination destination = message.getJMSReplyTo();
+        if (destination == null) {
+            LOG.info("Invalid destination received. Can't reply to request");
+            return;
+        }
+
+        MessageProducer replyProducer = session.createProducer(destination);
+
+        Message replyMessage = session.createTextMessage(reply);
+        replyProducer.send(replyMessage);
     }
 
-
+    public ServiceType getType() {
+        return ServiceType.PROPERTY_SERVICE;
+    }
 }
