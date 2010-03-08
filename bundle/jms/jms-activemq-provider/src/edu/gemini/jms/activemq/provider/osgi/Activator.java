5cbf257e1154594b5b05346ce048a2d36d32ba61
anunez <anunez@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2010-03-08 21:10:54 +0000

    Added configuration option to specify location of JMS broker
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@24505 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- bundle/jms/jms-activemq-provider/src/edu/gemini/jms/activemq/provider/osgi/Activator.java
+++ bundle/jms/jms-activemq-provider/src/edu/gemini/jms/activemq/provider/osgi/Activator.java
@@ -9,22 +9,31 @@ import org.osgi.framework.ServiceRegistration;
 import java.util.logging.Logger;
 
 /**
- *
+ * Activator class for the JMS Provided based on ActiveMQ
  */
 public class Activator implements BundleActivator {
 
     private static final Logger LOG = Logger.getLogger(Activator.class.getName());
+    private static final String BROKER_URL_PROP = "edu.gemini.jms.activemq.provider.url";
     private ServiceRegistration _registration;
 
     private JmsProvider _provider = null;
     
     public void start(BundleContext bundleContext) throws Exception {
-        _provider = new ActiveMQJmsProvider();
+
+        String brokerUrl = bundleContext.getProperty(BROKER_URL_PROP);
+
+        if (brokerUrl != null) {
+            _provider = new ActiveMQJmsProvider(brokerUrl);
+        } else {
+            brokerUrl = "default";
+            _provider = new ActiveMQJmsProvider();
+        }
         //advertise the ActiveMQ provider in the OSGi framework
         _registration = bundleContext.registerService(
                 JmsProvider.class.getName(),
                 _provider, null);
-        LOG.info("JMS Provider initialized");
+        LOG.info("JMS Provider initialized (" + brokerUrl + ")");
     }
 
     public void stop(BundleContext bundleContext) throws Exception {
