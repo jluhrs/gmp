8c05145ce8143093dbcb39494cce202afabe0038
nbarriga <nbarriga@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2010-09-30 20:41:38 +0000

    Added test stub.
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@27367 ee6ba543-07f8-4773-b02c-01d50e9d00ba

---
+++ bundle/giapi/giapi-cas/src-test/edu/gemini/aspen/giapi/cas/GiapiCasTest.java
@@ -0,0 +1,22 @@
+package edu.gemini.aspen.giapi.cas;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Class GiapiCasTest
+ *
+ * @author Nicolas A. Barriga
+ *         Date: Sep 30, 2010
+ */
+public class GiapiCasTest{
+    @Before
+    public void setUp(){}
+
+    @Test
+    public void testTest(){}
+
+    @After
+    public void tearDown(){}
+}
