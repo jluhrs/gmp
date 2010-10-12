9c49dcf5e222bdeff6e7424cca8b1a02ff503f39
nbarriga <nbarriga@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2010-10-12 12:23:25 +0000

    Fix for the StressTest to work on linux.
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@27448 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- bundle/giapi/giapi-cas/src-test/edu/gemini/aspen/giapi/cas/StressTest.java
+++ bundle/giapi/giapi-cas/src-test/edu/gemini/aspen/giapi/cas/StressTest.java
@@ -55,6 +55,7 @@ public class StressTest extends TestCase {
             final int iters = Integer.MAX_VALUE;
             final int stepiters=1000000;
             int i=0;
+	        int count =1;
             try {
 
                 for (i = 0; i < iters; i++) {
@@ -66,17 +67,21 @@ public class StressTest extends TestCase {
                     //channel.getContext().pendIO(1);
                     //((CAJChannel)channel).getTransport().flush();
                     Thread.yield();
+		            if(i%1000==0)Thread.sleep(10);
                     if (i % stepiters == 0) {
+			            System.gc();
+			            Thread.sleep(1);
                         long now = System.currentTimeMillis();
                         long elapsedtotal = now - first;
                         long elapsedlast = now-last;
                         last=now;
-                        Double ratetotal = new Double((double)i / elapsedtotal * 1000.0);
+                        Double ratetotal = new Double((double)i*count / elapsedtotal * 1000.0);
                         Double ratelast = new Double((double) stepiters / elapsedlast * 1000.0);
                          long freemem=Runtime.getRuntime().freeMemory();
                         long totalmem=Runtime.getRuntime().totalMemory();
                         long usedmem=totalmem-freemem;
-                        LOG.info("Iteration:" + i +
+                        LOG.info("Count:" + count + 
+				" Iteration:" + i +
                                 ", Total Rate:" + ratetotal.intValue() +
                                 "[updates/s], Last Rate: " + ratelast.intValue() +
                                 "[updates/s], Used mem:"+usedmem/1024+
@@ -84,7 +89,10 @@ public class StressTest extends TestCase {
 
 
                     }
-                    if(i==(iters-1))i=0;
+                    if(i==(iters-1)){
+                        i=0;
+                        count++;
+                    }
 
                 }
             } catch (OutOfMemoryError ex) {
