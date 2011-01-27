ee414c4dde1791c72ffecf8b70f39a697a05d759
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-01-27 17:15:31 +0000

    Converted to use iPOJO and ConfigAdmin to setup and configure ActiveMQ Broker
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@29810 ee6ba543-07f8-4773-b02c-01d50e9d00ba

---
+++ jms-activemq-broker/src/main/java/edu/gemini/jms/activemq/broker/ActiveMQBrokerComponent.java
@@ -0,0 +1,69 @@
+package edu.gemini.jms.activemq.broker;
+
+import edu.gemini.jms.api.Broker;
+import org.apache.felix.ipojo.annotations.*;
+
+import java.util.logging.Logger;
+
+import static edu.gemini.jms.activemq.broker.ActiveMQBroker.activemq;
+import static edu.gemini.jms.activemq.broker.ConfigDefaults.*;
+
+/**
+ * Thin wrapper of ActiveMQBroker to expose the configuration to the ConfigAdmin service
+ *
+ * @author cquiroz
+ */
+@Component(name = "ActiveMQBroker", managedservice = "edu.gemini.jms.activemq.broker.ActiveMQBroker")
+@Instantiate(name = "ActiveMQBroker")
+public class ActiveMQBrokerComponent {
+    private static final Logger LOG = Logger.getLogger(ActiveMQBroker.class.getName());
+
+    private Broker _broker = null;
+
+    @Property(name = "useJmx", value = "" + BROKER_PERSISTENT, mandatory = true)
+    private boolean useJmx;
+
+    @Property(name = "persistent", value = "" + BROKER_PERSISTENT, mandatory = true)
+    private boolean isPersistent;
+
+    @Property(name = "brokerName", value = BROKER_NAME, mandatory = true)
+    private String brokerName;
+
+    @Property(name = "brokerUrl", value = BROKER_URL, mandatory = true)
+    private String url;
+
+    @Property(name = "deleteMsgOnStartup", value = "" + BROKER_DELETE_MESSAGES_ON_STARTUP, mandatory = true)
+    private boolean deleteMsgOnStartup;
+
+    @Property(name = "useAdvisoryMessages", value = "" + BROKER_USE_ADVISORY_MESSAGES, mandatory = true)
+    private boolean useAdvisoryMessages;
+
+    @Property(name = "jmxRmiServerPort", value = "" + BROKER_JMX_RMI_PORT, mandatory = true)
+    private int jmxRmiServerPort;
+
+    @Property(name = "jmxConnectorPort", value = "" + BROKER_JMX_CONNECTOR_PORT, mandatory = true)
+    private int jmxConnectorPort;
+
+    @Updated
+    private void updated() {
+        LOG.info("Updated configuration of ActiveMQ broker");
+        if (_broker == null) {
+            _broker = activemq()
+                    .name(brokerName)
+                    .url(url)
+                    .useJmx(useJmx)
+                    .useAdvisoryMessages(useAdvisoryMessages)
+                    .deleteMsgOnStartup(deleteMsgOnStartup)
+                    .jmxConnectorPort(jmxConnectorPort)
+                    .jmxRrmiServerPort(jmxRmiServerPort)
+                    .build();
+        } else {
+            LOG.warning("Cannot reconfigure a running ActiveMQ Broker");
+        }
+    }
+
+    @Validate
+    public void start() {
+        _broker.start();
+    }
+}
