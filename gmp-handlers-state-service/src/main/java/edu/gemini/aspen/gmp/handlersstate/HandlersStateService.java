9c803f4d30c711bb968519568390cedbb842ef95
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-01-28 18:15:19 +0000

    Changed the HandlersStateService in GIAPI-133 as indicated in CR-GIAPI-14 #comment Changed interface to use Configuration instead of ConfigPath
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@29838 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- gmp-handlers-state-service/src/main/java/edu/gemini/aspen/gmp/handlersstate/HandlersStateService.java
+++ gmp-handlers-state-service/src/main/java/edu/gemini/aspen/gmp/handlersstate/HandlersStateService.java
@@ -1,23 +1,27 @@
 package edu.gemini.aspen.gmp.handlersstate;
 
-import edu.gemini.aspen.giapi.commands.ConfigPath;
+import edu.gemini.aspen.giapi.commands.Configuration;
 
 /**
  * Main exportable interface of the bundle
+ * <p/>
+ * HandlersStateService defines a service that can indicate the availability
+ * of handlers or consumers for a given configuration. It can be used to detect
+ * whether a configuration is fully handled by message consumers or if some parts are
+ * left unhandled
  *
- * HandlersStateService defines an interface for a service that can indicate the availability
- * of handlers or consumers for a given path. It can be used to detect whether a given
- * given config path is being handled by a message consumer in ActiveMQ
- *
- * @cquiroz
+ * @author cquiroz
  */
 public interface HandlersStateService {
     /**
-     * Indicates whether the given path is being handled by a given JMS client
+     * Indicates whether the given configuration is being handled by one ore more JMS client
      *
-     * @param path The {@link ConfigPath} being tested
+     * This will indicate if the full configuration has listeners able to respond in such a way that
+     * one or more handles can
+     *
+     * @param path The {@link Configuration} being tested
      * @return <code>true</code> if there is a handler registered for the path,
      *         <code>false</code> otherwise
      */
-    public boolean isPathHandled(ConfigPath path);
+    public boolean isConfigurationHandled(Configuration path);
 }
--- gmp-handlers-state-service/src/main/java/edu/gemini/aspen/gmp/handlersstate/impl/HandlersStateServiceImpl.java
+++ gmp-handlers-state-service/src/main/java/edu/gemini/aspen/gmp/handlersstate/impl/HandlersStateServiceImpl.java
@@ -1,14 +1,84 @@
 package edu.gemini.aspen.gmp.handlersstate.impl;
 
 import edu.gemini.aspen.giapi.commands.ConfigPath;
+import edu.gemini.aspen.giapi.commands.Configuration;
 import edu.gemini.aspen.gmp.handlersstate.HandlersStateService;
+import edu.gemini.jms.api.BaseMessageConsumer;
+import edu.gemini.jms.api.DestinationData;
+import edu.gemini.jms.api.DestinationType;
+import edu.gemini.jms.api.JmsProvider;
+import org.apache.felix.ipojo.annotations.*;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import java.util.logging.Logger;
 
 /**
  * Implementation of
  */
+@Component(name = "Handlers State Service")
+@Instantiate
+@Provides
 public class HandlersStateServiceImpl implements HandlersStateService {
+    private static final Logger LOG = Logger.getLogger(HandlersStateService.class.getName());
+
+    @Requires
+    private JmsProvider _provider;
+
     @Override
-    public boolean isPathHandled(ConfigPath path) {
+    public boolean isConfigurationHandled(Configuration path) {
         return false;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    /**
+ * JMS Message consumer to receive status items.
+ */
+private class StatusConsumer extends BaseMessageConsumer {
+
+    public StatusConsumer(String clientName) {
+        super(clientName,
+                new DestinationData("ActiveMQ.Advisory.*",
+                        DestinationType.TOPIC));
+        setMessageListener(new MessageListener() {
+            @Override
+            public void onMessage(Message message) {
+                LOG.info("AAAAAAAAAAAAAAAAAA" + message.toString());
+
+            }
+        });
+    }
+
+
+
+
+}
+
+
+
+    public boolean isPathHandled(ConfigPath path) {
+        System.out.println(path);
+        return false;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Validate
+    public void validated() {
+        LOG.info("Starting the HandlersState Service " + _provider);
+        try {
+            new JMXConsumerStateHolder();
+            new StatusConsumer("ME").startJms(_provider);
+        } catch (JMSException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+//        _provider.getConnectionFactory().createConnection().
+    }
+
+    @Invalidate
+    public void invalidate() {
+        
+    }
+
+    public void advisoryMessage(String message) {
+        System.out.println(message);
     }
 }
