641649d89571ca6b79d9f731e7b67fc4ec40b7ec
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-01-28 19:29:35 +0000

    Interface that defines how a ConsumerStateHolder looks like
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@29842 ee6ba543-07f8-4773-b02c-01d50e9d00ba

---
+++ gmp-handlers-state-service/src/main/java/edu/gemini/aspen/gmp/handlersstate/impl/ConsumerStateHolder.java
@@ -0,0 +1,8 @@
+package edu.gemini.aspen.gmp.handlersstate.impl;
+
+/**
+ * Stores a set of JMS Consumers reflecting the current state of the ActiveMQ Broker
+ * 
+ */
+public interface ConsumerStateHolder {
+}
