78d180887f4a85a8495bc7f3fd1e2308562b59a2
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-01-31 18:49:44 +0000

    Unit test for the consumer holder using JMX
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@29878 ee6ba543-07f8-4773-b02c-01d50e9d00ba

---
+++ gmp-handlers-state-service/src/test/java/edu/gemini/aspen/gmp/handlersstate/impl/JMXConsumerStateHolderTest.java
@@ -0,0 +1,72 @@
+package edu.gemini.aspen.gmp.handlersstate.impl;
+
+import org.apache.activemq.broker.jmx.BrokerView;
+import org.apache.activemq.broker.jmx.BrokerViewMBean;
+import org.apache.activemq.broker.jmx.TopicSubscriptionView;
+import org.apache.activemq.broker.jmx.TopicSubscriptionViewMBean;
+import org.junit.Test;
+
+import javax.management.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Test for consumer holder as found with JMX.
+ */
+public class JMXConsumerStateHolderTest {
+
+    @Test
+    public void testNoMBeanServer() {
+        assertNoSubscribersFoundViaJMX();
+    }
+
+    private void assertNoSubscribersFoundViaJMX() {
+        JMXConsumerStateHolder stateHolder = new JMXConsumerStateHolder();
+        assertTrue(stateHolder.getJmxBasedSubscribers().isEmpty());
+    }
+
+    @Test
+    public void testWithMBeanServerAndNoBrokerView() {
+        MBeanServerFactory.createMBeanServer();
+        assertNoSubscribersFoundViaJMX();
+    }
+
+    @Test
+    public void testWithMBeanServerAndBrokerView() throws MalformedObjectNameException, MBeanRegistrationException, InstanceAlreadyExistsException, NotCompliantMBeanException {
+        ObjectName amqObjectNamePattern = new ObjectName("org.apache.activemq:BrokerName=gmp,Type=Broker");
+        MBeanServer beanServer = MBeanServerFactory.createMBeanServer();
+        BrokerViewMBean brokerViewMBean = mock(BrokerView.class);
+        beanServer.registerMBean(brokerViewMBean, amqObjectNamePattern);
+        assertNoSubscribersFoundViaJMX();
+    }
+
+    @Test
+    public void testWithSubscribers() throws MalformedObjectNameException, MBeanRegistrationException, InstanceAlreadyExistsException, NotCompliantMBeanException {
+        ObjectName amqObjectNamePattern = new ObjectName("org.apache.activemq:BrokerName=gmp,Type=Broker");
+        MBeanServer beanServer = MBeanServerFactory.createMBeanServer();
+        BrokerViewMBean brokerViewMBean = mock(BrokerView.class);
+        beanServer.registerMBean(brokerViewMBean, amqObjectNamePattern);
+
+        ObjectName[] topicSubscribers = new ObjectName[] {
+                new ObjectName("org.apache.activemq:BrokerName=gmp,Type=TopicSubscriber,name=a"),
+                new ObjectName("org.apache.activemq:BrokerName=gmp,Type=TopicSubscriber,name=b")
+        };
+        when(brokerViewMBean.getTopicSubscribers()).thenReturn(topicSubscribers);
+
+        TopicSubscriptionViewMBean topicSubscriberViewMBean = mock(TopicSubscriptionView.class);
+        when(topicSubscriberViewMBean.getClientId()).thenReturn("client1");
+        when(topicSubscriberViewMBean.getDestinationName()).thenReturn("destination1");
+        beanServer.registerMBean(topicSubscriberViewMBean, topicSubscribers[0]);
+        topicSubscriberViewMBean = mock(TopicSubscriptionView.class);
+        when(topicSubscriberViewMBean.getClientId()).thenReturn("client2");
+        when(topicSubscriberViewMBean.getDestinationName()).thenReturn("destination2`");
+        beanServer.registerMBean(topicSubscriberViewMBean, topicSubscribers[1]);
+
+        JMXConsumerStateHolder stateHolder = new JMXConsumerStateHolder();
+        assertEquals(2, stateHolder.getJmxBasedSubscribers().size());
+    }
+
+}
