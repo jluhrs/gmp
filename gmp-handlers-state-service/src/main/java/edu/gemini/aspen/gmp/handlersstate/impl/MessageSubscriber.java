6c672c105d9afca94b94bc747925a490f5eed99a
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-01-31 18:31:54 +0000

    Added equals/hashCode to MessageSubscriber and tests
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@29876 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- gmp-handlers-state-service/src/main/java/edu/gemini/aspen/gmp/handlersstate/impl/MessageSubscriber.java
+++ gmp-handlers-state-service/src/main/java/edu/gemini/aspen/gmp/handlersstate/impl/MessageSubscriber.java
@@ -11,6 +11,37 @@ class MessageSubscriber {
     public MessageSubscriber(String clientId, String destinationName) {
         this.clientId = clientId;
         this.destinationName = destinationName;
+        if (clientId == null || destinationName == null) {
+            throw new IllegalArgumentException("Cannot accept null values clientId: " + clientId + ", destinationName:" + destinationName);
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        MessageSubscriber that = (MessageSubscriber) o;
+
+        if (!clientId.equals(that.clientId)) {
+            return false;
+        }
+        if (!destinationName.equals(that.destinationName)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = clientId.hashCode();
+        result = 31 * result + destinationName.hashCode();
+        return result;
     }
 
     @Override
