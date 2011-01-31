d75c473d7783d4aa882c3ed1c6c8c3908c26abd0
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-01-31 14:57:18 +0000

    Completed the assembly, importing all the bundles from a local maven repository
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@29865 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- gmp-server/src/main/etc/bin/start.sh
+++ gmp-server/src/main/etc/bin/start.sh
@@ -7,5 +7,5 @@ abspath=$(cd ${0%/*} && echo $PWD/${0##*/})
 # to get the path only - not the script name - add
 script_path=`dirname "$abspath"`
 
-#exec java -jar -Dfelix.config.properties=file:conf/bundle.properties felix/org.apache.felix.main-3.0.2.jar
-exec java -jar
+# Will start pax-runner reading the configuration from the file bin/runner.args
+exec java -jar $script_path/pax-runner-1.5.0.jar
