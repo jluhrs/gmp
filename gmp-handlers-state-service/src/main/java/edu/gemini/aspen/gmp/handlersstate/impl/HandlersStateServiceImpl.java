ad6c4ba070110bf40ca38998ab42fc338acfb11e
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-02-04 12:08:46 +0000

    Cleaned up main class, and link to ConsumerStateHolder classes
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@30004 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- gmp-handlers-state-service/src/main/java/edu/gemini/aspen/gmp/handlersstate/impl/HandlersStateServiceImpl.java
+++ gmp-handlers-state-service/src/main/java/edu/gemini/aspen/gmp/handlersstate/impl/HandlersStateServiceImpl.java
@@ -1,10 +1,13 @@
 package edu.gemini.aspen.gmp.handlersstate.impl;
 
+import edu.gemini.aspen.giapi.commands.ConfigPath;
 import edu.gemini.aspen.giapi.commands.Configuration;
 import edu.gemini.aspen.gmp.handlersstate.HandlersStateService;
 import edu.gemini.jms.api.JmsProvider;
 import org.apache.felix.ipojo.annotations.*;
 
+import javax.jms.JMSException;
+import java.util.Set;
 import java.util.logging.Logger;
 
 /**
@@ -19,49 +22,45 @@ public class HandlersStateServiceImpl implements HandlersStateService {
     @Requires
     private JmsProvider _provider;
 
+    private JMXConsumerStateHolder jmxConsumerStateHolder;
+    private AdvisoriesConsumerStateHolder advisoriesConsumerStateHolder;
+    private volatile boolean started = false;
+
     @Override
     public boolean isConfigurationHandled(Configuration path) {
+        Set<ConfigPath> pathKeys = path.getKeys();
+//        for (ConfigPath p:pathKeys) {
+//            System.out.println(p.getName());
+//            System.out.println(p.getParent());
+//            System.out.println(p.getReferencedName());
+//            System.out.println(p.split()[0]);
+//        }
+
+
         return false;
     }
 
-    /**
-     * JMS Message consumer to receive status items.
-     */
-/*
-    private class StatusConsumer extends BaseMessageConsumer {
-
-        public StatusConsumer(String clientName) {
-            super(clientName,
-                    new DestinationData("ActiveMQ.Advisory.*",
-                            DestinationType.TOPIC));
-            setMessageListener(new MessageListener() {
-                @Override
-                public void onMessage(Message message) {
-                }
-            });
-        }
-    }
-*/
-
     @Validate
     public void validated() {
-        LOG.info("Starting the HandlersState Service " + _provider);
-/*
-        try {
-            new JMXConsumerStateHolder();
-            new StatusConsumer("ME").startJms(_provider);
-        } catch (JMSException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        if (!started) {
+            LOG.info("Starting the HandlersState Service " + _provider);
+            jmxConsumerStateHolder = new JMXConsumerStateHolder();
+            advisoriesConsumerStateHolder = new AdvisoriesConsumerStateHolder();
+            try {
+                advisoriesConsumerStateHolder.startJms(_provider);
+            } catch (JMSException e) {
+                LOG.severe("Not possible to start a consumer state holder using advisory messages");
+            }
+            started = true;
         }
-*/
     }
 
     @Invalidate
     public void invalidate() {
-
+        if (started) {
+            started = false;
+            advisoriesConsumerStateHolder.stopJms();
+        }
     }
 
-    public void advisoryMessage(String message) {
-        System.out.println(message);
-    }
 }
