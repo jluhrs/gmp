8ca1130e1b7f3cd4bd3bb71cadab83d616c40250
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-02-09 15:44:32 +0000

    As PcsUpdate is immutable equals and hashCode are simplified eliminating null checks
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@30200 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- gmp-pcs-updater/src/main/java/edu/gemini/aspen/gmp/pcs/model/PcsUpdate.java
+++ gmp-pcs-updater/src/main/java/edu/gemini/aspen/gmp/pcs/model/PcsUpdate.java
@@ -5,7 +5,7 @@ import com.google.common.collect.ImmutableList;
 import java.util.List;
 
 /**
- * A class that holds an update for the PCS system.
+ * An immutable class that holds an update for the PCS system.
  */
 public final class PcsUpdate {
 
@@ -14,46 +14,50 @@ public final class PcsUpdate {
      */
     private final List<Double> _zernikes;
 
-    /**
-     * Constructor
-     */
     public PcsUpdate(Double[] values) {
         _zernikes = ImmutableList.copyOf(values);
     }
 
     /**
      * Get the list of zernikes as an array of doubles.
+     *
      * @return an array of doubles with the zernikes
-     * stored in this update
+     *         stored in this update
      */
     public Double[] getZernikes() {
         Double[] results = new Double[_zernikes.size()];
-         _zernikes.toArray(results);
+        _zernikes.toArray(results);
         return results;
     }
 
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
 
         PcsUpdate pcsUpdate = (PcsUpdate) o;
 
-        if (_zernikes != null ? !_zernikes.equals(pcsUpdate._zernikes) : pcsUpdate._zernikes != null) return false;
+        if (!_zernikes.equals(pcsUpdate._zernikes)) {
+            return false;
+        }
         //everything matches
         return true;
     }
 
     @Override
     public int hashCode() {
-        return _zernikes != null ? _zernikes.hashCode() : 0;
+        return _zernikes.hashCode();
     }
 
     @Override
     public String toString() {
         return "PcsUpdate{" +
-               "zernikes=" + _zernikes +
-               '}';
+                "zernikes=" + _zernikes +
+                '}';
     }
 }
