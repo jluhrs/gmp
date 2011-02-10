12087b1c527a686d85d24a5986b78b31e1ac1a9f
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-02-10 19:37:08 +0000

    EpicsUpdateImpl is now immutable and cannot be constructed with null values
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@30874 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- gmp-epics-access/src/main/java/edu/gemini/aspen/gmp/epics/EpicsUpdateImpl.java
+++ gmp-epics-access/src/main/java/edu/gemini/aspen/gmp/epics/EpicsUpdateImpl.java
@@ -4,12 +4,16 @@ package edu.gemini.aspen.gmp.epics;
  * A simple implementation for an Epics Update.
  */
 public class EpicsUpdateImpl implements EpicsUpdate {
-
-    private String channelName;
-    private Object channelData;
-
+    private final String channelName;
+    private final Object channelData;
 
     public EpicsUpdateImpl(String channelName, Object channelData) {
+        if (channelName == null) {
+            throw new IllegalArgumentException("Channel name cannot be null");
+        }
+        if (channelData == null) {
+            throw new IllegalArgumentException("Channel data cannot be null");
+        }
         this.channelName = channelName;
         this.channelData = channelData;
     }
@@ -22,7 +26,6 @@ public class EpicsUpdateImpl implements EpicsUpdate {
         return channelData;
     }
 
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -30,9 +33,9 @@ public class EpicsUpdateImpl implements EpicsUpdate {
 
         EpicsUpdateImpl that = (EpicsUpdateImpl) o;
 
-        if (channelData != null ? !channelData.equals(that.channelData) : that.channelData != null)
+        if (!channelData.equals(that.channelData))
             return false;
-        if (channelName != null ? !channelName.equals(that.channelName) : that.channelName != null)
+        if (!channelName.equals(that.channelName))
             return false;
         //everything is fine, the objects are the same
         return true;
@@ -40,8 +43,8 @@ public class EpicsUpdateImpl implements EpicsUpdate {
 
     @Override
     public int hashCode() {
-        int result = channelName != null ? channelName.hashCode() : 0;
-        result = 31 * result + (channelData != null ? channelData.hashCode() : 0);
+        int result = channelName.hashCode();
+        result = 31 * result + channelData.hashCode();
         return result;
     }
 }
