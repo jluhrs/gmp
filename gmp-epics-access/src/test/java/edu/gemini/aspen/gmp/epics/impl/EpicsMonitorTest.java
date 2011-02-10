9a5f8a3495ee058a3a9261c299e57d4b5e523a1d
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-02-10 21:21:33 +0000

    Extra test for the method EpicsMonitor.channelChanged
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@31194 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- gmp-epics-access/src/test/java/edu/gemini/aspen/gmp/epics/impl/EpicsMonitorTest.java
+++ gmp-epics-access/src/test/java/edu/gemini/aspen/gmp/epics/impl/EpicsMonitorTest.java
@@ -1,16 +1,18 @@
 package edu.gemini.aspen.gmp.epics.impl;
 
 import edu.gemini.aspen.gmp.epics.EpicsRegistrar;
+import edu.gemini.aspen.gmp.epics.EpicsUpdateImpl;
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public class EpicsMonitorTest {
     @Test
     public void testConnected() {
-        EpicsRegistrar registrar = null;
-        EpicsMonitor epicsMonitor = new EpicsMonitor(registrar);
+        EpicsMonitor epicsMonitor = new EpicsMonitor(null);
         assertFalse(epicsMonitor.isConnected());
         epicsMonitor.connected();
         assertTrue(epicsMonitor.isConnected());
@@ -18,12 +20,19 @@ public class EpicsMonitorTest {
 
     @Test
     public void testDisconnected() {
-        EpicsRegistrar registrar = null;
-        EpicsMonitor epicsMonitor = new EpicsMonitor(registrar);
+        EpicsMonitor epicsMonitor = new EpicsMonitor(null);
         assertFalse(epicsMonitor.isConnected());
         epicsMonitor.connected();
         epicsMonitor.disconnected();
         assertFalse(epicsMonitor.isConnected());
     }
 
+    @Test
+    public void testChannelChanged() {
+        EpicsRegistrar registrar = mock(EpicsRegistrar.class);
+        EpicsMonitor epicsMonitor = new EpicsMonitor(registrar);
+
+        epicsMonitor.channelChanged("X.val1", Integer.valueOf(1));
+        verify(registrar).processEpicsUpdate(new EpicsUpdateImpl("X.val1", Integer.valueOf(1)));
+    }
 }
