9d84367f1043dfe06e68d5eb023e8b0c09f606b2
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-02-11 12:21:38 +0000

    Initial unit test for EpicsUpdaterThread testing whether the common path works
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@31512 ee6ba543-07f8-4773-b02c-01d50e9d00ba

---
+++ gmp-epics-access/src/test/java/edu/gemini/aspen/gmp/epics/impl/EpicsUpdaterThreadTest.java
@@ -0,0 +1,47 @@
+package edu.gemini.aspen.gmp.epics.impl;
+
+import edu.gemini.aspen.gmp.epics.EpicsUpdate;
+import edu.gemini.aspen.gmp.epics.EpicsUpdateImpl;
+import edu.gemini.aspen.gmp.epics.EpicsUpdateListener;
+import org.junit.Test;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static org.junit.Assert.assertTrue;
+
+public class EpicsUpdaterThreadTest {
+    private String channel = "X.val1";
+    private EpicsUpdate epicsUpdate = new EpicsUpdateImpl(channel, Integer.valueOf(1));
+    private CountDownLatch latch = new CountDownLatch(1);
+    private AtomicBoolean passed = new AtomicBoolean(false);
+
+    @Test
+    public void testUpdateOnRegisteredListener() throws InterruptedException {
+        EpicsUpdaterThread epicsUpdater = new EpicsUpdaterThread();
+        try {
+            EpicsUpdateListener listener = createListener();
+
+            epicsUpdater.registerInterest(channel, listener);
+            epicsUpdater.start();
+            epicsUpdater.processEpicsUpdate(epicsUpdate);
+
+            // Wait on the latch for up to 2 secs;
+            latch.await(2000, TimeUnit.MILLISECONDS);
+            assertTrue(passed.get());
+        } finally {
+            epicsUpdater.stop();
+        }
+    }
+
+    private EpicsUpdateListener createListener() {
+        return new EpicsUpdateListener() {
+                    @Override
+                    public void onEpicsUpdate(EpicsUpdate update) {
+                        passed.set(epicsUpdate.equals(update));
+                        latch.countDown();
+                    }
+                };
+    }
+}
