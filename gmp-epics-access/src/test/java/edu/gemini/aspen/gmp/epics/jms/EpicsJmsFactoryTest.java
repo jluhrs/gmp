f238a51f9ab98ebb37f0c359bcb2d125a7f5116d
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-02-11 15:16:55 +0000

    Additional test for extra epic types on the MessageFactory
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@31518 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- gmp-epics-access/src/test/java/edu/gemini/aspen/gmp/epics/jms/EpicsJmsFactoryTest.java
+++ gmp-epics-access/src/test/java/edu/gemini/aspen/gmp/epics/jms/EpicsJmsFactoryTest.java
@@ -21,9 +21,7 @@ public class EpicsJmsFactoryTest {
     public void createMessageWithIntArray() throws JMSException {
         when(session.createBytesMessage()).thenReturn(bm);
         int[] channelData = {4, 5};
-        EpicsUpdate epicsUpdate = new EpicsUpdateImpl(channelName, channelData);
-        Message message = EpicsJmsFactory.createMessage(session, epicsUpdate);
-        assertNotNull(message);
+        createMessage(channelData);
         // TODO The fact that we need to verify low level protocol here indicates a wrong level of abstraction
         // for EpicsJMSFactory
         verify(bm).writeByte((byte)2);
@@ -33,19 +31,74 @@ public class EpicsJmsFactoryTest {
         verify(bm).writeInt(channelData[1]);
     }
 
+    private void createMessage(Object channelData) throws JMSException {
+        EpicsUpdate epicsUpdate = new EpicsUpdateImpl(channelName, channelData);
+        Message message = EpicsJmsFactory.createMessage(session, epicsUpdate);
+        assertNotNull(message);
+    }
+
     @Test
     public void createMessageWithDoubleArray() throws JMSException {
         when(session.createBytesMessage()).thenReturn(bm);
         double[] channelData = {4, 5};
-        EpicsUpdate epicsUpdate = new EpicsUpdateImpl(channelName, channelData);
-        Message message = EpicsJmsFactory.createMessage(session, epicsUpdate);
-        assertNotNull(message);
-        // TODO The fact that we need to verify low level protocol here indicates a wrong level of abstraction
-        // for EpicsJMSFactory
+        createMessage(channelData);
+
         verify(bm).writeByte((byte)3);
         verify(bm).writeUTF(channelName);
         verify(bm).writeInt(channelData.length);
         verify(bm).writeDouble(channelData[0]);
         verify(bm).writeDouble(channelData[1]);
+    }
+
+    @Test
+    public void createMessageWithFloatArray() throws JMSException {
+        when(session.createBytesMessage()).thenReturn(bm);
+        float[] channelData = {4, 5};
+        createMessage(channelData);
+
+        verify(bm).writeByte((byte)4);
+        verify(bm).writeUTF(channelName);
+        verify(bm).writeInt(channelData.length);
+        verify(bm).writeFloat(channelData[0]);
+        verify(bm).writeFloat(channelData[1]);
+    }
+
+    @Test
+    public void createMessageWithShortArray() throws JMSException {
+        when(session.createBytesMessage()).thenReturn(bm);
+        short[] channelData = {4, 5};
+        createMessage(channelData);
+
+        verify(bm).writeByte((byte)1);
+        verify(bm).writeUTF(channelName);
+        verify(bm).writeInt(channelData.length);
+        verify(bm).writeShort(channelData[0]);
+        verify(bm).writeShort(channelData[1]);
+    }
+
+    @Test
+    public void createMessageWithByteArray() throws JMSException {
+        when(session.createBytesMessage()).thenReturn(bm);
+        byte[] channelData = {4, 5};
+        createMessage(channelData);
+
+        verify(bm).writeByte((byte)6);
+        verify(bm).writeUTF(channelName);
+        verify(bm).writeInt(channelData.length);
+        verify(bm).writeByte(channelData[0]);
+        verify(bm).writeByte(channelData[1]);
+    }
+
+    @Test
+    public void createMessageWithStringArray() throws JMSException {
+        when(session.createBytesMessage()).thenReturn(bm);
+        String[] channelData = {"a", "b"};
+        createMessage(channelData);
+
+        verify(bm).writeByte((byte)5);
+        verify(bm).writeUTF(channelName);
+        verify(bm).writeInt(channelData.length);
+        verify(bm).writeUTF(channelData[0]);
+        verify(bm).writeUTF(channelData[1]);
     }
 }
