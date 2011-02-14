98a398b33457798897cf6ee6d0dd7f9ce0621a73
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-02-14 18:44:05 +0000

    Added test for the normal path of onMessage on EpicsConfigRequestConsumer
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@31551 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- gmp-epics-access/src/test/java/edu/gemini/aspen/gmp/epics/jms/EpicsConfigRequestConsumerTest.java
+++ gmp-epics-access/src/test/java/edu/gemini/aspen/gmp/epics/jms/EpicsConfigRequestConsumerTest.java
@@ -4,6 +4,7 @@ import com.google.common.collect.ImmutableSet;
 import edu.gemini.aspen.giapi.util.jms.JmsKeys;
 import edu.gemini.aspen.gmp.epics.EpicsConfiguration;
 import edu.gemini.jms.api.JmsProvider;
+import org.junit.Before;
 import org.junit.Test;
 
 import javax.jms.*;
@@ -17,46 +18,75 @@ import static org.mockito.Mockito.*;
 
 public class EpicsConfigRequestConsumerTest {
 
-    @Test
-    public void testConstruction() throws JMSException {
-        EpicsConfiguration configuration = new EpicsConfiguration() {
-            @Override
-            public Set<String> getValidChannelsNames() {
-                return ImmutableSet.of("X.val1");
-            }
-        };
+    private JmsProvider provider;
+    private EpicsConfiguration configuration;
+    private Session session;
+    private String channelName = "X.val1";
 
-        JmsProvider provider = buildMocks();
-        EpicsConfigRequestConsumer consumer = new EpicsConfigRequestConsumer(provider, configuration);
-        assertNotNull(consumer);
-    }
-
-    private JmsProvider buildMocks() throws JMSException {
-        JmsProvider provider = mock(JmsProvider.class);
+    @Before
+    public void setupMocks() throws JMSException {
+        provider = mock(JmsProvider.class);
         ConnectionFactory connectionFactory = mock(ConnectionFactory.class);
         when(provider.getConnectionFactory()).thenReturn(connectionFactory);
         Connection connection = mock(Connection.class);
         when(connectionFactory.createConnection()).thenReturn(connection);
-        Session session = mock(Session.class);
+        session = mock(Session.class);
         when(connection.createSession(anyBoolean(), anyInt())).thenReturn(session);
 
         Queue destination = mock(Queue.class);
         when(session.createQueue(anyString())).thenReturn(destination);
         MessageConsumer messageConsumer = mock(MessageConsumer.class);
         when(session.createConsumer(destination)).thenReturn(messageConsumer);
-        return provider;
+        configuration = new EpicsConfiguration() {
+            @Override
+            public Set<String> getValidChannelsNames() {
+                return ImmutableSet.of(channelName);
+            }
+        };
+    }
+
+    @Test
+    public void testConstruction() throws JMSException {
+        EpicsConfigRequestConsumer consumer = new EpicsConfigRequestConsumer(provider, configuration);
+        assertNotNull(consumer);
+    }
+
+    @Test
+    public void onMessage() throws JMSException {
+        EpicsConfigRequestConsumer consumer = new EpicsConfigRequestConsumer(provider, configuration);
+        Message message = mock(Message.class);
+
+        when(message.getBooleanProperty(JmsKeys.GMP_GEMINI_EPICS_CHANNEL_PROPERTY)).thenReturn(true);
+        Destination destination = mock(Destination.class);
+        when(message.getJMSReplyTo()).thenReturn(destination);
+
+        MessageProducer replyProducer = mock(MessageProducer.class);
+        when(session.createProducer(destination)).thenReturn(replyProducer);
+        MapMessage replyMessage = mock(MapMessage.class);
+        when(session.createMapMessage()).thenReturn(replyMessage);
+
+        consumer.onMessage(message);
+        
+        verify(replyMessage).setBoolean(channelName, true);
+        verify(replyProducer).send(replyMessage);
+    }
+
+    @Test
+    public void onMessageWithNoReplyAddress() throws JMSException {
+        EpicsConfigRequestConsumer consumer = new EpicsConfigRequestConsumer(provider, configuration);
+        Message message = mock(Message.class);
+
+        when(message.getBooleanProperty(JmsKeys.GMP_GEMINI_EPICS_CHANNEL_PROPERTY)).thenReturn(true);
+        when(message.getJMSReplyTo()).thenReturn(null);
+        consumer.onMessage(message);
+
+        verify(message).getBooleanProperty(JmsKeys.GMP_GEMINI_EPICS_CHANNEL_PROPERTY);
+        verify(message).getJMSReplyTo();
+        verifyNoMoreInteractions(message);
     }
 
     @Test
     public void onMessageWithoutChannelProperty() throws JMSException {
-        EpicsConfiguration configuration = new EpicsConfiguration() {
-            @Override
-            public Set<String> getValidChannelsNames() {
-                return ImmutableSet.of("X.val1");
-            }
-        };
-
-        JmsProvider provider = buildMocks();
         EpicsConfigRequestConsumer consumer = new EpicsConfigRequestConsumer(provider, configuration);
         Message message = mock(Message.class);
 
