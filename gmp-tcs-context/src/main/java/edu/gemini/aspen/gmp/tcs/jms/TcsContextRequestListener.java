ec4fdacf2db86c9c5682ad2859e77738b4d2c377
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-02-15 18:58:01 +0000

    Cleaned up TcsContextFetcher and added UnitTest for TcsContextRequestListener
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@31598 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- gmp-tcs-context/src/main/java/edu/gemini/aspen/gmp/tcs/jms/TcsContextRequestListener.java
+++ gmp-tcs-context/src/main/java/edu/gemini/aspen/gmp/tcs/jms/TcsContextRequestListener.java
@@ -15,28 +15,29 @@ import java.util.logging.Level;
  * The Listener to receive TCS Context requests
  */
 public class TcsContextRequestListener implements MessageListener {
-
-
     private static final Logger LOG = Logger.getLogger(TcsContextRequestListener.class.getName());
-
     public static final String DESTINATION_NAME = JmsKeys.GMP_TCS_CONTEXT_DESTINATION;
 
     /**
      * Message producer used to send the TCS Context back to the requester
      */
-    private JmsTcsContextDispatcher _dispatcher;
+    private final JmsTcsContextDispatcher _dispatcher;
 
     /**
-     * TCS Context fetcher, used to obtain the TCS Context. 
+     * TCS Context fetcher, used to obtain the TCS Context.
      */
     private TcsContextFetcher _fetcher;
 
     /**
      * Constructor. Takes as an argument the JMS dispatcher that will
      * be used to reply back to the requester.
+     *
      * @param dispatcher JMS Dispatcher that sends back the TCS Context
      */
     public TcsContextRequestListener(JmsTcsContextDispatcher dispatcher) {
+        if (dispatcher == null) {
+            throw new IllegalArgumentException("Cannot construct TcsContextRequestListener with a null dispatcher");
+        }
         _dispatcher = dispatcher;
     }
 
@@ -51,38 +52,39 @@ public class TcsContextRequestListener implements MessageListener {
         _fetcher = fetcher;
     }
 
-
     /**
      * Receives the request. Gets the destination to reply,
      * obtains the TCS context (if possible) and send it
      * back to the requester.
+     *
      * @param message A message with a TCS Context request.
      */
     public void onMessage(Message message) {
-
         try {
-            Destination d = message.getJMSReplyTo();
-
-            if (d == null) {
-                return; //nothing to do, since we don't know where to reply
-            }
-
-            double[] context = null;
-
-            if (_fetcher != null) {
-                context = _fetcher.getTcsContext();
-            }
-
-            if (context != null) {
-                _dispatcher.send(context, d);
-            }
-
+            sendContextIfAllowed(message);
         } catch (JMSException e) {
             LOG.warning("Problems sending TCS Context back");
         } catch (TcsContextException e) {
             LOG.log(Level.WARNING, "Problem obtaining TCS Context", e);
         }
+    }
 
+    private void sendContextIfAllowed(Message message) throws JMSException, TcsContextException {
+        Destination replyDestination = message.getJMSReplyTo();
+        if (canDispatchContext(replyDestination)) {
+            dispatchContext(replyDestination);
+        }
+    }
 
+    private boolean canDispatchContext(Destination d) {
+        return _fetcher != null && d!= null;
+    }
+
+    private void dispatchContext(Destination replyDestination) throws TcsContextException, JMSException {
+        double[] context = _fetcher.getTcsContext();
+
+        if (context != null) {
+            _dispatcher.send(context, replyDestination);
+        }
     }
 }
--- gmp-tcs-context/src/test/java/edu/gemini/aspen/gmp/tcs/model/jms/TcsContextRequestListenerTest.java
+++ gmp-tcs-context/src/test/java/edu/gemini/aspen/gmp/tcs/model/jms/TcsContextRequestListenerTest.java
@@ -2,12 +2,68 @@ package edu.gemini.aspen.gmp.tcs.model.jms;
 
 import edu.gemini.aspen.gmp.tcs.jms.JmsTcsContextDispatcher;
 import edu.gemini.aspen.gmp.tcs.jms.TcsContextRequestListener;
+import edu.gemini.aspen.gmp.tcs.model.TcsContextException;
+import edu.gemini.aspen.gmp.tcs.model.TcsContextFetcher;
+import org.junit.Before;
 import org.junit.Test;
 
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.*;
+
 public class TcsContextRequestListenerTest {
-    @Test
-    public void testConstructor() {
-        JmsTcsContextDispatcher dispatcher = null;
-        new TcsContextRequestListener(dispatcher);
+    private double[] context = new double[]{1, 2, 3};
+    private JmsTcsContextDispatcher contextDispatcher;
+    private TcsContextFetcher fetcher;
+    private Message message;
+    private Destination destination;
+    private TcsContextRequestListener listener;
+
+    @Before
+    public void setUp() throws Exception {
+        contextDispatcher = mock(JmsTcsContextDispatcher.class);
+        fetcher = mock(TcsContextFetcher.class);
+        message = mock(Message.class);
+        destination = mock(Destination.class);
+        when(message.getJMSReplyTo()).thenReturn(destination);
+        listener = new TcsContextRequestListener(contextDispatcher);
     }
+
+    @Test
+    public void testOnMessage() throws TcsContextException, JMSException {
+        listener.registerTcsContextFetcher(fetcher);
+
+        when(fetcher.getTcsContext()).thenReturn(context);
+        listener.onMessage(message);
+
+        verify(fetcher).getTcsContext();
+        verify(contextDispatcher).send(eq(context), eq(destination));
+    }
+
+    @Test
+    public void testOnMessageWithoutFetcher() throws TcsContextException, JMSException {
+        when(fetcher.getTcsContext()).thenReturn(context);
+
+        listener.onMessage(message);
+
+        verifyZeroInteractions(contextDispatcher);
+    }
+
+    @Test
+    public void testOnMessageWithNullContext() throws TcsContextException, JMSException {
+        when(fetcher.getTcsContext()).thenReturn(null);
+
+        listener.onMessage(message);
+
+        verifyZeroInteractions(contextDispatcher);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testOnMessageWithNoDispatcher() throws TcsContextException, JMSException {
+        new TcsContextRequestListener(null);
+    }
+
 }
