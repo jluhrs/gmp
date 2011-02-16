346b7df4fd865d25d0626a3ea29f41e10c8b8a85
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-02-16 15:33:28 +0000

    Updated documentation of bundles gmp-epics-access, gmp-tcs-context, gmp-epics-simulator, gmp-services and added to the general documentation of gmp-server
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@31651 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- gmp-tcs-context/src/main/java/edu/gemini/aspen/gmp/tcs/model/TcsContextFetcher.java
+++ gmp-tcs-context/src/main/java/edu/gemini/aspen/gmp/tcs/model/TcsContextFetcher.java
@@ -10,10 +10,9 @@ public interface TcsContextFetcher {
      * Get the TCS Context as an array of doubles
      *
      * @return an array of double values with the TCS context, or
-     *         <code>null</code> if the context cannot be read
+     *         <code>an empty array</code> if the context cannot be read
      * @throws TcsContextException in case there is an exception
      *                             trying to get the TCS Context.
      */
     double[] getTcsContext() throws TcsContextException;
-
 }
