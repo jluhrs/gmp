37ee778a49a67566b00d63f30619e1563c501981
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-02-18 14:14:24 +0000

    Added scripts to build the deployment package and the deployment with the documentation
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@31787 ee6ba543-07f8-4773-b02c-01d50e9d00ba

---
+++ gmp-server/buildrelease.sh
@@ -0,0 +1,13 @@
+#!//bin/sh
+#==========================
+# bash - find path to script
+#==========================
+abspath=$(cd ${0%/*} && echo $PWD/${0##*/})
+
+# to get the path only - not the script name - add
+script_path=`dirname "$abspath"`
+
+cd $script_path/../
+mvn clean install
+cd $script_path
+mvn -Pproduction doxygen:report assembly:assembly
