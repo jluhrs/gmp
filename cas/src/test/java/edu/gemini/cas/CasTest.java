9d0251d7f79e1814207da84a1394e08d962d5dca
nbarriga <nbarriga@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-02-18 14:34:58 +0000

    Cosmetic fixes to CasTest.
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@31789 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- cas/src/test/java/edu/gemini/cas/CasTest.java
+++ cas/src/test/java/edu/gemini/cas/CasTest.java
@@ -9,6 +9,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -20,37 +21,34 @@ import java.util.logging.Logger;
  */
 public class CasTest {
     private static final Logger LOG = Logger.getLogger(CasTest.class.getName());
-    private String varname="nico:test1";
+    private String varname = "nico:test1";
+    private ChannelAccessServer giapicas;
 
     @Before
-    public void setUp() {
+    public void setUp() throws Exception{
+        giapicas = new ChannelAccessServer();
+        giapicas.start();
+    }
+
+    @After
+    public void tearDown() throws Exception{
+        giapicas.stop();
     }
 
     /**
      * Just starts and stops the server
      */
     @Test
-    public void testStartStop() {
+    public void testStartStop() throws Exception {
 
+        giapicas.stop();
+
+        giapicas.start();
         try {
-            ChannelAccessServer giapicas = new ChannelAccessServer();
-
-
             giapicas.start();
-
-            giapicas.stop();
-
-            giapicas.start();
-            try{
-                giapicas.start();
-                fail();
-            }catch(IllegalStateException ex){
-
-            }
-            giapicas.stop();
-        } catch (Exception ex) {
-            LOG.log(Level.SEVERE, ex.getMessage(),ex);
             fail();
+        } catch (IllegalStateException ex) {
+
         }
     }
 
@@ -58,164 +56,121 @@ public class CasTest {
      * Starts the server, adds a PV, reads from it, removes the PV, checks it is correctly removed and stops the server
      */
     @Test
-    public void testAddTwice() {
+    public void testAddTwice() throws Exception {
 
+        IChannel ch = giapicas.createIntegerChannel(varname, 1);
+        DBR dbr = ch.getValue();
+
+
+        int num = dbr.getCount();
+        assertEquals(1, num);
+        Object obj = dbr.getValue();
+        int[] objarr = (int[]) obj;
+        assertEquals(0, objarr[0]);
+
+        IChannel ch2 = giapicas.createIntegerChannel(varname, 1);
+
+        assertEquals(ch, ch2);
+
+        giapicas.destroyChannel(ch);
+        giapicas.destroyChannel(ch2);
+        ch = giapicas.createFloatChannel(varname, 1);
+        ch2 = giapicas.createFloatChannel(varname, 1);
+        assertEquals(ch, ch2);
+
+        giapicas.destroyChannel(ch);
+        giapicas.destroyChannel(ch2);
+        ch = giapicas.createDoubleChannel(varname, 1);
+        ch2 = giapicas.createDoubleChannel(varname, 1);
+        assertEquals(ch, ch2);
+
+        giapicas.destroyChannel(ch);
+        giapicas.destroyChannel(ch2);
+        ch = giapicas.createStringChannel(varname, 1);
+        ch2 = giapicas.createStringChannel(varname, 1);
+        assertEquals(ch, ch2);
+        giapicas.destroyChannel(ch2);
         try {
-            ChannelAccessServer giapicas = new ChannelAccessServer();
-            giapicas.start();
-
-            IChannel ch= giapicas.createIntegerChannel(varname,1);
-            DBR dbr = ch.getValue();
-
-
-
-            int num = dbr.getCount();
-            assertEquals(1, num);
-            Object obj = dbr.getValue();
-            int[] objarr = (int[]) obj;
-            assertEquals(0, objarr[0]);
-
-            IChannel ch2= giapicas.createIntegerChannel(varname,1);
-
-            assertEquals(ch,ch2);
-
-            giapicas.destroyChannel(ch);
-            giapicas.destroyChannel(ch2);
-            ch= giapicas.createFloatChannel(varname,1);
-            ch2= giapicas.createFloatChannel(varname,1);
-            assertEquals(ch,ch2);
-
-            giapicas.destroyChannel(ch);
-            giapicas.destroyChannel(ch2);
-            ch= giapicas.createDoubleChannel(varname,1);
-            ch2= giapicas.createDoubleChannel(varname,1);
-            assertEquals(ch,ch2);
-
-            giapicas.destroyChannel(ch);
-            giapicas.destroyChannel(ch2);
-            ch= giapicas.createStringChannel(varname,1);
-            ch2= giapicas.createStringChannel(varname,1);
-            assertEquals(ch,ch2);
-            giapicas.destroyChannel(ch2);
-            try{
-                ch.getValue();
-                fail();
-            }catch(RuntimeException ex){
-                //OK   
-            }finally{
-                giapicas.stop();
-            }
-        } catch (Exception ex) {
-            LOG.log(Level.SEVERE, ex.getMessage(),ex);
+            ch.getValue();
             fail();
+        } catch (RuntimeException ex) {
+            //OK
         }
     }
 
     @Test
-    public void testAddTwiceWrongType() {
+    public void testAddTwiceWrongType() throws Exception {
+
+        IChannel ch = giapicas.createIntegerChannel(varname, 1);
+        DBR dbr = ch.getValue();
+
+
+        int num = dbr.getCount();
+        assertEquals(1, num);
+        Object obj = dbr.getValue();
+        int[] objarr = (int[]) obj;
+        assertEquals(0, objarr[0]);
 
         try {
-            ChannelAccessServer giapicas = new ChannelAccessServer();
-            giapicas.start();
-
-            IChannel ch= giapicas.createIntegerChannel(varname,1);
-            DBR dbr = ch.getValue();
-
-
-
-            int num = dbr.getCount();
-            assertEquals(1, num);
-            Object obj = dbr.getValue();
-            int[] objarr = (int[]) obj;
-            assertEquals(0, objarr[0]);
-
-            try{
-                IChannel ch2= giapicas.createFloatChannel(varname,1);
-                fail();
-            }catch(RuntimeException ex){
-                //OK
-            }
-            try{
-                IChannel ch2= giapicas.createDoubleChannel(varname,1);
-                fail();
-            }catch(RuntimeException ex){
-                //OK
-            }
-            try{
-                IChannel ch2= giapicas.createStringChannel(varname,1);
-                fail();
-            }catch(RuntimeException ex){
-                //OK
-            }
-
-            giapicas.destroyChannel(ch);
-
-            giapicas.stop();
-
-        } catch (Exception ex) {
-            LOG.log(Level.SEVERE, ex.getMessage(),ex);
+            IChannel ch2 = giapicas.createFloatChannel(varname, 1);
             fail();
+        } catch (RuntimeException ex) {
+            //OK
         }
+        try {
+            IChannel ch2 = giapicas.createDoubleChannel(varname, 1);
+            fail();
+        } catch (RuntimeException ex) {
+            //OK
+        }
+        try {
+            IChannel ch2 = giapicas.createStringChannel(varname, 1);
+            fail();
+        } catch (RuntimeException ex) {
+            //OK
+        }
+
+        giapicas.destroyChannel(ch);
+
     }
 
     /**
      * Starts the server, adds a PV, writes a value, reads it back, checks the value read is correct and stops the server
      */
     @Test
-    public void testWriteVar() {
-        try {
-            ChannelAccessServer giapicas = new ChannelAccessServer();
-            giapicas.start();
-            IChannel ch= giapicas.createIntegerChannel(varname,1);
-            ch.setValue(3);
+    public void testWriteVar() throws Exception {
+        IChannel ch = giapicas.createIntegerChannel(varname, 1);
+        ch.setValue(3);
 
-            DBR dbr = ch.getValue();
+        DBR dbr = ch.getValue();
 
 
-
-            int num = dbr.getCount();
-            assertEquals(1, num);
-            Object obj = dbr.getValue();
-            int[] objarr = (int[]) obj;
-            assertEquals(3, objarr[0]);
-
-            giapicas.stop();
-        } catch (Exception ex) {
-            LOG.log(Level.SEVERE, ex.getMessage(),ex);
-            fail();
-        }
-
+        int num = dbr.getCount();
+        assertEquals(1, num);
+        Object obj = dbr.getValue();
+        int[] objarr = (int[]) obj;
+        assertEquals(3, objarr[0]);
 
     }
-   /**
+
+    /**
      * Starts the server, adds a PV, writes a value, reads it back, checks the value read is correct and stops the server
      */
     @Test
-    public void testWriteArray() {
-        try {
-            ChannelAccessServer giapicas = new ChannelAccessServer();
-            giapicas.start();
-            IChannel ch= giapicas.createIntegerChannel(varname,3);
-            ch.setValue(new Integer[]{3,4,5});
+    public void testWriteArray() throws Exception {
+        IChannel ch = giapicas.createIntegerChannel(varname, 3);
+        ch.setValue(new Integer[]{3, 4, 5});
 
-            DBR dbr = ch.getValue();
+        DBR dbr = ch.getValue();
 
 
-
-            int num = dbr.getCount();
-            assertEquals(3, num);
-            Object obj = dbr.getValue();
-            int[] objarr = (int[]) obj;
-            assertEquals(3, objarr[0]);
-            assertEquals(4, objarr[1]);
-            assertEquals(5, objarr[2]);
-
-            giapicas.stop();
-        } catch (Exception ex) {
-            LOG.log(Level.SEVERE, ex.getMessage(),ex);
-            fail();
-        }
-
+        int num = dbr.getCount();
+        assertEquals(3, num);
+        Object obj = dbr.getValue();
+        int[] objarr = (int[]) obj;
+        assertEquals(3, objarr[0]);
+        assertEquals(4, objarr[1]);
+        assertEquals(5, objarr[2]);
 
     }
 
@@ -223,163 +178,124 @@ public class CasTest {
      * Starts the server, adds one PV of each type, writes a value in each PV, reads it back, checks the value read is correct and stops the server
      */
     @Test
-    public void testWriteVarAllTypes() {
-        try {
-            ChannelAccessServer giapicas = new ChannelAccessServer();
-            giapicas.start();
-            IChannel chI= giapicas.createIntegerChannel("nico:int",1);
-            IChannel chF= giapicas.createFloatChannel("nico:float",1);
-            IChannel chD= giapicas.createDoubleChannel("nico:double",1);
-            IChannel chS= giapicas.createStringChannel("nico:string",1);
+    public void testWriteVarAllTypes() throws Exception {
+        IChannel chI = giapicas.createIntegerChannel("nico:int", 1);
+        IChannel chF = giapicas.createFloatChannel("nico:float", 1);
+        IChannel chD = giapicas.createDoubleChannel("nico:double", 1);
+        IChannel chS = giapicas.createStringChannel("nico:string", 1);
 
 
-            chI.setValue(3);
-            chF.setValue(3.0f);
-            chD.setValue(3.0);
-            chS.setValue("three");
+        chI.setValue(3);
+        chF.setValue(3.0f);
+        chD.setValue(3.0);
+        chS.setValue("three");
 
 
-            DBR dbr[] = new DBR[4];
-            dbr[0]=chI.getValue();
-            dbr[1]=chF.getValue();
-            dbr[2]=chD.getValue();
-            dbr[3]=chS.getValue();
-            Object ret[] = new Object[4];
+        DBR dbr[] = new DBR[4];
+        dbr[0] = chI.getValue();
+        dbr[1] = chF.getValue();
+        dbr[2] = chD.getValue();
+        dbr[3] = chS.getValue();
+        Object ret[] = new Object[4];
 
-            for(int i=0;i<4;i++){
-                assertEquals(1, dbr[i].getCount());
-                ret[i]=dbr[i].getValue();
-            }
-            assertEquals(3, ((int[])ret[0])[0]);
-            assertEquals(3f, ((float[])ret[1])[0],0.00001);
-            assertEquals(3.0, ((double[])ret[2])[0],0.00001);
-            assertEquals("three", ((String[])ret[3])[0]);
-
-            giapicas.stop();
-        } catch (Exception ex) {
-            LOG.log(Level.SEVERE, ex.getMessage(),ex);
-            fail();
+        for (int i = 0; i < 4; i++) {
+            assertEquals(1, dbr[i].getCount());
+            ret[i] = dbr[i].getValue();
         }
-
+        assertEquals(3, ((int[]) ret[0])[0]);
+        assertEquals(3f, ((float[]) ret[1])[0], 0.00001);
+        assertEquals(3.0, ((double[]) ret[2])[0], 0.00001);
+        assertEquals("three", ((String[]) ret[3])[0]);
 
     }
-  /**
+
+    /**
      * Starts the server, adds a PV, writes a value, reads it back, checks the value read is correct and stops the server
      */
     @Test
-    public void testWriteWrongType() {
+    public void testWriteWrongType() throws Exception {
+        IChannel ch = giapicas.createIntegerChannel(varname, 1);
+
+        int exceptions = 0;
+
         try {
-            ChannelAccessServer giapicas = new ChannelAccessServer();
-            giapicas.start();
-            IChannel ch= giapicas.createIntegerChannel(varname,1);
-
-            int exceptions=0;
-
-            try{
-                ch.setValue(3.0f);
-            }catch(IllegalArgumentException ex){
-                //correct
-                exceptions++;
-            }
-            try{
-                ch.setValue(3.0d);
-            }catch(IllegalArgumentException ex){
-                //correct
-                exceptions++;
-            }
-            try{
-                ch.setValue("three");
-            }catch(IllegalArgumentException ex){
-                //correct
-                exceptions++;
-            }
-            assertEquals(3,exceptions);
-            giapicas.stop();
-        } catch (Exception ex) {
-            LOG.log(Level.SEVERE, ex.getMessage(),ex);
-            fail();
+            ch.setValue(3.0f);
+        } catch (IllegalArgumentException ex) {
+            //correct
+            exceptions++;
         }
-
+        try {
+            ch.setValue(3.0d);
+        } catch (IllegalArgumentException ex) {
+            //correct
+            exceptions++;
+        }
+        try {
+            ch.setValue("three");
+        } catch (IllegalArgumentException ex) {
+            //correct
+            exceptions++;
+        }
+        assertEquals(3, exceptions);
 
     }
 
     @Test
-    public void testCreateAlarmChannels() {
-        try {
-            ChannelAccessServer giapicas = new ChannelAccessServer();
-            giapicas.start();
-            IAlarmChannel ch1 = giapicas.createIntegerAlarmChannel("int", 1);
-            IAlarmChannel ch2 = giapicas.createFloatAlarmChannel("float", 1);
-            IAlarmChannel ch3 = giapicas.createDoubleAlarmChannel("double", 1);
-            IAlarmChannel ch4 = giapicas.createStringAlarmChannel("string", 1);
-            giapicas.stop();
-        } catch (Exception ex) {
-            LOG.log(Level.SEVERE, ex.getMessage(), ex);
-            fail();
-        }
+    public void testCreateAlarmChannels() throws Exception {
+        IAlarmChannel ch1 = giapicas.createIntegerAlarmChannel("int", 1);
+        IAlarmChannel ch2 = giapicas.createFloatAlarmChannel("float", 1);
+        IAlarmChannel ch3 = giapicas.createDoubleAlarmChannel("double", 1);
+        IAlarmChannel ch4 = giapicas.createStringAlarmChannel("string", 1);
     }
 
     @Test
-    public void testWriteAlarm() {
-        try {
-            ChannelAccessServer giapicas = new ChannelAccessServer();
-            giapicas.start();
-            IAlarmChannel ch= giapicas.createIntegerAlarmChannel(varname, 1);
-            ch.setAlarm(Status.HIHI_ALARM, Severity.MAJOR_ALARM, "alarm message");
-            ch.setValue(3);
-            DBR dbr = ch.getValue();
+    public void testWriteAlarm() throws Exception {
+        IAlarmChannel ch = giapicas.createIntegerAlarmChannel(varname, 1);
+        ch.setAlarm(Status.HIHI_ALARM, Severity.MAJOR_ALARM, "alarm message");
+        ch.setValue(3);
+        DBR dbr = ch.getValue();
 
-            assertEquals(Severity.MAJOR_ALARM,((STS)dbr).getSeverity());
-            assertEquals(Status.HIHI_ALARM,((STS)dbr).getStatus());
-            ch.clearAlarm();
-            dbr = ch.getValue();
-            assertEquals(Severity.NO_ALARM,((STS)dbr).getSeverity());
-            assertEquals(Status.NO_ALARM,((STS)dbr).getStatus());
+        assertEquals(Severity.MAJOR_ALARM, ((STS) dbr).getSeverity());
+        assertEquals(Status.HIHI_ALARM, ((STS) dbr).getStatus());
+        ch.clearAlarm();
+        dbr = ch.getValue();
+        assertEquals(Severity.NO_ALARM, ((STS) dbr).getSeverity());
+        assertEquals(Status.NO_ALARM, ((STS) dbr).getStatus());
 
-            int num = dbr.getCount();
-            assertEquals(1, num);
-            Object obj = dbr.getValue();
-            int[] objarr = (int[]) obj;
-            assertEquals(3, objarr[0]);
+        int num = dbr.getCount();
+        assertEquals(1, num);
+        Object obj = dbr.getValue();
+        int[] objarr = (int[]) obj;
+        assertEquals(3, objarr[0]);
 
-            //test add again
-            {
-                IChannel ch2= giapicas.createIntegerAlarmChannel(varname,1);
-                assertEquals(ch,ch2);
-            }
-
-            //test add with same name, wrong type
-            try{
-                IChannel ch2= giapicas.createFloatAlarmChannel(varname,1);
-                fail();
-            }catch(RuntimeException ex){
-                //OK
-            }
-            try{
-                IChannel ch2= giapicas.createDoubleAlarmChannel(varname,1);
-                fail();
-            }catch(RuntimeException ex){
-                //OK
-            }
-            try{
-                IChannel ch2= giapicas.createStringAlarmChannel(varname,1);
-                fail();
-            }catch(RuntimeException ex){
-                //OK
-            }
-
-
-            giapicas.stop();
-        } catch (Exception ex) {
-            LOG.log(Level.SEVERE, ex.getMessage(),ex);
-            fail();
+        //test add again
+        {
+            IChannel ch2 = giapicas.createIntegerAlarmChannel(varname, 1);
+            assertEquals(ch, ch2);
         }
 
+        //test add with same name, wrong type
+        try {
+            IChannel ch2 = giapicas.createFloatAlarmChannel(varname, 1);
+            fail();
+        } catch (RuntimeException ex) {
+            //OK
+        }
+        try {
+            IChannel ch2 = giapicas.createDoubleAlarmChannel(varname, 1);
+            fail();
+        } catch (RuntimeException ex) {
+            //OK
+        }
+        try {
+            IChannel ch2 = giapicas.createStringAlarmChannel(varname, 1);
+            fail();
+        } catch (RuntimeException ex) {
+            //OK
+        }
 
     }
 
 
 
-    @After
-    public void tearDown(){}
 }
