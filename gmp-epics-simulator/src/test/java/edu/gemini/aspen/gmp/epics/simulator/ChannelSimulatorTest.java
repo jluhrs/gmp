198681eaa74f52307b92d4f8e7777b977914294c
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-02-22 15:17:32 +0000

    Set the amount of simulation to be less restrictive as it sometimes fails depending on timing
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@32003 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- gmp-epics-simulator/src/test/java/edu/gemini/aspen/gmp/epics/simulator/ChannelSimulatorTest.java
+++ gmp-epics-simulator/src/test/java/edu/gemini/aspen/gmp/epics/simulator/ChannelSimulatorTest.java
@@ -29,6 +29,6 @@ public class ChannelSimulatorTest {
 
         TimeUnit.MILLISECONDS.sleep(PASSES * updateRate);
 
-        verify(registrar, atLeast(PASSES - 1)).processEpicsUpdate(Matchers.<EpicsUpdate>anyObject());
+        verify(registrar, atLeast(PASSES - 2)).processEpicsUpdate(Matchers.<EpicsUpdate>anyObject());
     }
 }
