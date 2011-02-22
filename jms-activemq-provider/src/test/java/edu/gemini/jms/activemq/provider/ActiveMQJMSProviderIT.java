b859abafe11a31d69a5242f7de8cad77af70b9cb
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-02-22 18:46:30 +0000

    Fixed Integration Test, it now uses FileInstall
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@32010 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- jms-activemq-provider/src/test/java/edu/gemini/jms/activemq/provider/ActiveMQJMSProviderIT.java
+++ jms-activemq-provider/src/test/java/edu/gemini/jms/activemq/provider/ActiveMQJMSProviderIT.java
@@ -1,6 +1,7 @@
 package edu.gemini.jms.activemq.provider;
 
 import edu.gemini.jms.api.JmsProvider;
+import org.apache.activemq.ActiveMQConnectionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.Inject;
@@ -16,6 +17,7 @@ import java.io.IOException;
 
 import static org.junit.Assert.*;
 import static org.ops4j.pax.exam.CoreOptions.*;
+import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.cleanCaches;
 
 /**
  * Basic integration test for JMS Provider basically verifying that the service starts and can be configured
@@ -35,6 +37,9 @@ public class ActiveMQJMSProviderIT {
     public static Option[] baseConfig() {
         return options(
                 felix(),
+                cleanCaches(),
+                systemProperty("felix.fileinstall.dir").value(System.getProperty("basedir") + "/src/test/resources/conf/services"),
+                systemProperty("felix.fileinstall.noInitialDelay").value("true"),
                 mavenBundle().artifactId("org.apache.felix.ipojo").groupId("org.apache.felix").versionAsInProject(),
                 mavenBundle().artifactId("com.springsource.javax.jms").groupId("javax.jms").versionAsInProject(),
                 mavenBundle().artifactId("org.osgi.compendium").groupId("org.osgi").version("4.2.0"),
@@ -44,6 +49,10 @@ public class ActiveMQJMSProviderIT {
                 mavenBundle().artifactId("geronimo-annotation_1.0_spec").groupId("org.apache.geronimo.specs").version("1.1.1"),
                 mavenBundle().artifactId("com.springsource.org.apache.commons.logging").groupId("org.apache.commons").version("1.1.1"),
                 mavenBundle().artifactId("jms-api").groupId("edu.gemini.jms").versionAsInProject(),
+                mavenBundle().artifactId("org.apache.felix.configadmin").groupId("org.apache.felix").version("1.2.8"),
+                mavenBundle().artifactId("org.apache.felix.fileinstall").groupId("org.apache.felix").version("3.1.10"),
+                mavenBundle().artifactId("pax-logging-api").groupId("org.ops4j.pax.logging").version("1.6.0"),
+                mavenBundle().artifactId("pax-logging-service").groupId("org.ops4j.pax.logging").version("1.6.0"),
                 mavenBundle().artifactId("jms-activemq-provider").groupId("edu.gemini.jms").version("1.1.0").update()
         );
     }
@@ -65,12 +74,21 @@ public class ActiveMQJMSProviderIT {
 
     @Test
     public void doesServiceExist() throws IOException, BundleException, InterruptedException {
-        assertTrue(isJMSProviderInUse());
+        assertTrue(isJMSProviderAvailable());
+        JmsProvider provider = findJmsProvider();
+        // WARN extra dependency on ActiveMQ API
+        assertEquals("failover:(tcp://localhost:51616)", ((ActiveMQConnectionFactory)provider.getConnectionFactory()).getBrokerURL());
+
     }
 
-    private boolean isJMSProviderInUse() {
-        ServiceReference aggregateReference = context.getServiceReference(JmsProvider.class.getName());
-        return aggregateReference != null;
+    private boolean isJMSProviderAvailable() {
+        ServiceReference providerReference = context.getServiceReference(JmsProvider.class.getName());
+        return providerReference != null;
+    }
+
+    private JmsProvider findJmsProvider() {
+        ServiceReference providerReference = context.getServiceReference(JmsProvider.class.getName());
+        return (JmsProvider)context.getService(providerReference);
     }
 
 }
