1ce68e5e6970bdbe465fc1edf80b29fa50152695
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-02-22 13:21:01 +0000

    Made integration test to use the Configuration provided by FileInstall
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@31998 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- jms-activemq-broker/src/test/java/edu/gemini/jms/activemq/broker/ActiveMQBrokerTest.java
+++ jms-activemq-broker/src/test/java/edu/gemini/jms/activemq/broker/ActiveMQBrokerTest.java
@@ -4,7 +4,6 @@ package edu.gemini.jms.activemq.broker;
  * Integration test for ActiveMQBroker. it attempts to launch and configure an ActiveMQBroker
  */
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.Inject;
@@ -24,7 +23,6 @@ import java.util.concurrent.TimeUnit;
 import static org.ops4j.pax.exam.CoreOptions.*;
 
 @RunWith(JUnit4TestRunner.class)
-@Ignore
 public class ActiveMQBrokerTest {
     @Inject
     private BundleContext context;
@@ -52,24 +50,17 @@ public class ActiveMQBrokerTest {
         //System.out.println(System.getProperties());
         //getClass().getResource("")
         return options(
-
                 felix(),
                 waitForFrameworkStartup(),
                 //systemProperty("ipojo.log.level").value("debug"),
                 systemProperty("felix.cm.loglevel").value("4"),
-                systemProperty("felix.fileinstall.log.level").value("debug"),
-                systemProperty("felix.fileinstall.dir").value(System.getProperty("basedir") + "/src/test/resources/conf"),
+                systemProperty("felix.fileinstall.log.level").value("4"),
+                systemProperty("felix.fileinstall.dir").value(System.getProperty("basedir") + "/src/test/resources/conf/services"),
                 systemProperty("felix.fileinstall.noInitialDelay").value("true"),
-//                  <felix.fileinstall.log.level>4</felix.fileinstall.log.level>
-//        <felix.fileinstall.noInitialDelay>true</felix.fileinstall.noInitialDelay>
-                //systemProperty("felix.cm.loglevel").value("4"),
-//        <felix.fileinstall.dir>${conf.base}/services</felix.fileinstall.dir>
-                provision(
                 mavenBundle().artifactId("org.apache.felix.ipojo").groupId("org.apache.felix").versionAsInProject(),
                 mavenBundle().artifactId("com.springsource.javax.jms").groupId("javax.jms").versionAsInProject(),
                 mavenBundle().artifactId("jms-api").groupId("edu.gemini.jms").versionAsInProject(),
                 mavenBundle().artifactId("org.osgi.compendium").groupId("org.osgi").version("4.2.0"),
-                // mavenBundle().artifactId("giapi-jms-util").groupId("edu.gemini.aspen").versionAsInProject(),
                 mavenBundle().artifactId("org.apache.felix.configadmin").groupId("org.apache.felix").version("1.2.8"),
                 mavenBundle().artifactId("org.apache.felix.fileinstall").groupId("org.apache.felix").version("3.1.10"),
                 mavenBundle().artifactId("pax-logging-api").groupId("org.ops4j.pax.logging").version("1.5.3"),
@@ -78,21 +69,18 @@ public class ActiveMQBrokerTest {
                 mavenBundle().artifactId("geronimo-j2ee-management_1.1_spec").groupId("org.apache.geronimo.specs").version("1.0.1"),
                 mavenBundle().artifactId("kahadb").groupId("org.apache.activemq").version("5.4.2"),
                 mavenBundle().artifactId("geronimo-annotation_1.0_spec").groupId("org.apache.geronimo.specs").version("1.1.1"),
-                mavenBundle().artifactId("com.springsource.org.apache.commons.logging").groupId("org.apache.commons").version("1.1.1")),
-
+                mavenBundle().artifactId("com.springsource.org.apache.commons.logging").groupId("org.apache.commons").version("1.1.1"),
                 mavenBundle().artifactId("jms-activemq-broker").groupId("edu.gemini.jms").version("1.1.0").update().startLevel(6));
     }
 
     @Test
     public void testBrokerRunning() throws BundleException, InterruptedException {
-        System.out.println("STAAAAAT");
-        TimeUnit.MILLISECONDS.sleep(2200L);
-        getStatusServiceBundle();
-        System.out.println("61616");
+        //TimeUnit.MILLISECONDS.sleep(2200L);
+        Bundle bundle = getJmsActiveMQBrokerBundle();
 
     }
 
-    private Bundle getStatusServiceBundle() throws BundleException {
+    private Bundle getJmsActiveMQBrokerBundle() throws BundleException {
         for (Bundle b : context.getBundles()) {
             if ("edu.gemini.jms.activemq-broker".equals(b.getSymbolicName())) {
                 b.start();
