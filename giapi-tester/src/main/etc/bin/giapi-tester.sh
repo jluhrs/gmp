fa43726bc0ef8f4c2b8521be7aac385e75e759d0
nbarriga <nbarriga@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-02-22 19:20:42 +0000

    Added script for giapi-tester
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@32015 ee6ba543-07f8-4773-b02c-01d50e9d00ba

---
+++ giapi-tester/src/main/etc/bin/giapi-tester.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#==========================
+# GIAPI Tester
+#==========================
+#
+# Usage:
+#   ./giapi-tester.sh -?
+
+java -jar giapi-tester-0.1.0-jar-with-dependencies.jar $@
