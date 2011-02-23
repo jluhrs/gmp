8274d9e33e6b9135af4159f21836b7fdf4013412
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-02-23 17:33:23 +0000

    Initial unit test for ConfigPathNavigator
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@32076 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- giapi/src/test/java/edu/gemini/aspen/giapi/commands/ConfigPathNavigatorTest.java
+++ giapi/src/test/java/edu/gemini/aspen/giapi/commands/ConfigPathNavigatorTest.java
@@ -2,8 +2,13 @@ package edu.gemini.aspen.giapi.commands;
 
 import org.junit.Test;
 
+import java.util.Set;
+
 import static edu.gemini.aspen.giapi.commands.ConfigPath.configPath;
 import static edu.gemini.aspen.giapi.commands.DefaultConfiguration.configuration;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 public class ConfigPathNavigatorTest {
     @Test
@@ -14,7 +19,16 @@ public class ConfigPathNavigatorTest {
 //     *   gpi:dc:lamp = Z
 //     *   gpi:ao:inUse = false
         Configuration config = configuration(configPath("gpi:cc:filter.name"), "x'");
-        //config
         ConfigPathNavigator navigator = new ConfigPathNavigator(config);
-       }
+        assertNotNull(navigator);
+    }
+
+    @Test
+    public void testGetRootWithSinglePath() {
+        Configuration config = configuration(configPath("gpi:cc:filter.name"), "x'");
+        ConfigPathNavigator navigator = new ConfigPathNavigator(config);
+        Set<ConfigPath> configPaths = navigator.getRoot();
+        assertEquals(1, configPaths.size());
+        assertTrue(configPaths.contains(configPath("gpi")));
+    }
 }
