d6cfd73c2a9d832e27f35881e8f72f6734018ba8
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-02-23 17:51:57 +0000

    Added test for ConfigPathNavigator.getChildPaths
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@32079 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- giapi/src/test/java/edu/gemini/aspen/giapi/commands/ConfigPathNavigatorTest.java
+++ giapi/src/test/java/edu/gemini/aspen/giapi/commands/ConfigPathNavigatorTest.java
@@ -1,34 +1,63 @@
 package edu.gemini.aspen.giapi.commands;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Set;
 
 import static edu.gemini.aspen.giapi.commands.ConfigPath.configPath;
 import static edu.gemini.aspen.giapi.commands.DefaultConfiguration.configuration;
+import static edu.gemini.aspen.giapi.commands.DefaultConfiguration.copy;
+import static edu.gemini.aspen.giapi.commands.DefaultConfiguration.emptyConfiguration;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 public class ConfigPathNavigatorTest {
+    private Configuration singlePathConfiguration;
+    private Configuration multiplePathConfiguration;
+
+    @Before
+    public void setUp() throws Exception {
+        singlePathConfiguration = configuration(configPath("gpi:cc:filter.name"), "x'");
+        
+        multiplePathConfiguration = copy(emptyConfiguration())
+                .withPath(configPath("gpi:cc:filter.name"), "x")
+                .withPath(configPath("gpi:cc:mirror.pos"), "Y")
+                .withPath(configPath("gpi:dc:exposure"), "1")
+                .withPath(configPath("gpi:dc:lamp"), "Z")
+                .withPath(configPath("gpi:ap:inUse"), "false").build();
+    }
+
     @Test
     public void testConstruction() {
-//             *   gpi:cc:filter.name = X
-//     *   gpi:cc:mirror.pos = Y
-//     *   gpi:dc:exposure = 1
-//     *   gpi:dc:lamp = Z
-//     *   gpi:ao:inUse = false
-        Configuration config = configuration(configPath("gpi:cc:filter.name"), "x'");
-        ConfigPathNavigator navigator = new ConfigPathNavigator(config);
+        ConfigPathNavigator navigator = new ConfigPathNavigator(singlePathConfiguration);
         assertNotNull(navigator);
     }
 
     @Test
     public void testGetRootWithSinglePath() {
-        Configuration config = configuration(configPath("gpi:cc:filter.name"), "x'");
-        ConfigPathNavigator navigator = new ConfigPathNavigator(config);
+        ConfigPathNavigator navigator = new ConfigPathNavigator(singlePathConfiguration);
         Set<ConfigPath> configPaths = navigator.getRoot();
         assertEquals(1, configPaths.size());
         assertTrue(configPaths.contains(configPath("gpi")));
+    }
+
+    @Test
+    public void testGetRootWithMultiplePaths() {
+        ConfigPathNavigator navigator = new ConfigPathNavigator(multiplePathConfiguration);
+        Set<ConfigPath> configPaths = navigator.getRoot();
+        assertEquals(1, configPaths.size());
+        assertTrue(configPaths.contains(configPath("gpi")));
+    }
+
+    @Test
+    public void testChildPaths() {
+        ConfigPathNavigator navigator = new ConfigPathNavigator(multiplePathConfiguration);
+        Set<ConfigPath> childPaths = navigator.getChildPaths(configPath("gpi"));
+        assertEquals(3, childPaths.size());
+        assertTrue(childPaths.contains(configPath("gpi:dc")));
+        assertTrue(childPaths.contains(configPath("gpi:cc")));
+        assertTrue(childPaths.contains(configPath("gpi:ap")));
     }
 }
