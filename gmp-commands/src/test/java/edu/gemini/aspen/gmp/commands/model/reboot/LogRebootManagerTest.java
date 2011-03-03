f323fe9348f561c4bfcd92a393c5bcd8bf24f14a
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-03-03 12:48:17 +0000

    Added trivial test for LogRebootManager
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@32319 ee6ba543-07f8-4773-b02c-01d50e9d00ba

---
+++ gmp-commands/src/test/java/edu/gemini/aspen/gmp/commands/model/reboot/LogRebootManagerTest.java
@@ -0,0 +1,15 @@
+package edu.gemini.aspen.gmp.commands.model.reboot;
+
+import edu.gemini.aspen.giapi.commands.RebootArgument;
+import org.junit.Test;
+
+public class LogRebootManagerTest {
+
+    @Test
+    public void testReboot() {
+        LogRebootManager rebootManager = new LogRebootManager();
+
+        // There is nothing really to test except for not throwing exceptions
+        rebootManager.reboot(RebootArgument.GMP);
+    }
+}
