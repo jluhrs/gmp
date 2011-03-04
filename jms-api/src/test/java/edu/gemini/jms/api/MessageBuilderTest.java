ce4fa3cfaf0819435e42681dffc96363b2e49967
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-03-04 18:14:20 +0000

    Added unit tests for MessageBuilder
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@32377 ee6ba543-07f8-4773-b02c-01d50e9d00ba

---
+++ jms-api/src/test/java/edu/gemini/jms/api/MessageBuilderTest.java
@@ -0,0 +1,49 @@
+package edu.gemini.jms.api;
+
+import com.google.common.collect.ImmutableMap;
+import org.junit.Test;
+import org.mockito.Matchers;
+
+import javax.jms.JMSException;
+import javax.jms.MapMessage;
+
+import java.util.Map;
+
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.*;
+
+public class MessageBuilderTest {
+
+    private MessageBuilder messageBuilder = new MessageBuilder();
+    private MapMessage message = mock(MapMessage.class);
+    private Map<String, Object> messageContent = ImmutableMap.of("item1", (Object) "value1");
+    private Map<String, Object> messageProperties = ImmutableMap.of("item1", (Object) "value1");
+
+    @Test
+    public void testBuildMessage() throws JMSException {
+        messageBuilder.buildMapMessage(message, messageContent);
+
+        verify(message, times(messageContent.size())).setObject(anyString(), Matchers.<Object>anyObject());
+    }
+
+    @Test
+    public void testSetMessageProperties() throws JMSException {
+        messageBuilder.setMessageProperties(message, messageProperties);
+
+        verify(message, times(messageProperties.size())).setObjectProperty(anyString(), Matchers.<Object>anyObject());
+    }
+
+    @Test(expected = MessagingException.class)
+    public void testBuildMessageWithException() throws JMSException {
+        doThrow(new JMSException("Exception")).when(message).setObject(anyString(), Matchers.<Object>anyObject());
+
+        messageBuilder.buildMapMessage(message, messageContent);
+    }
+
+    @Test(expected = MessagingException.class)
+    public void testSetMessagePropertiesWithException() throws JMSException {
+        doThrow(new JMSException("Exception")).when(message).setObjectProperty(anyString(), Matchers.<Object>anyObject());
+
+        messageBuilder.setMessageProperties(message, messageProperties);
+    }
+}
