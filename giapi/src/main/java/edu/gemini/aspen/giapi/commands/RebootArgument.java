b4096cfcdeec19eafe4fbb526e2233cb4571f23c
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-03-07 19:03:14 +0000

    Fixed typo in documentation and reformat of code
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@32428 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- giapi/src/main/java/edu/gemini/aspen/giapi/commands/RebootArgument.java
+++ giapi/src/main/java/edu/gemini/aspen/giapi/commands/RebootArgument.java
@@ -2,7 +2,7 @@ package edu.gemini.aspen.giapi.commands;
 
 /**
  * A class to represent and manipulate the REBOOT arguments in a
- * sequence command. 
+ * sequence command.
  */
 public enum RebootArgument {
 
@@ -15,26 +15,32 @@ public enum RebootArgument {
 
     /**
      * Return the Reboot Argument from a given configuration.
+     *
      * @param config the Reboot sequence command configuration.
      * @return the Reboot Argument contained in the configuration. If the
-     * configuration is empty, the argument is assumed to be
-     * NONE. If the configuration contains an invalid entry for the
-     * Reboot argument, the result will be NULL.
+     *         configuration is empty, the argument is assumed to be
+     *         NONE. If the configuration contains an invalid entry for the
+     *         Reboot argument, the result will be NULL.
      */
     public static RebootArgument parse(Configuration config) {
-
-        if (config == null) return NONE;
+        if (config == null) {
+            return NONE;
+        }
 
         //if the config is empty, return NONE
-        if (config.getKeys().size() == 0) return NONE;
+        if (config.getKeys().size() == 0) {
+            return NONE;
+        }
 
         String value = config.getValue(REBOOT_OPT);
 
-        if (value == null) return null;
+        if (value == null) {
+            return null;
+        }
 
         RebootArgument arg;
         try {
-             arg = RebootArgument.valueOf(value.toUpperCase());
+            arg = RebootArgument.valueOf(value.toUpperCase());
         } catch (IllegalArgumentException e) {
             arg = null;
         }
