292ec8d95ea66f0b11f3c4d22dc82d36f37a1d96
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-03-09 19:56:46 +0000

    Additional Unit Test for ActionMessages built by JmsActionMessageBuilder
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@32558 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- gmp-commands/src/test/java/edu/gemini/aspen/gmp/commands/messaging/JmsActionMessageBuilderTest.java
+++ gmp-commands/src/test/java/edu/gemini/aspen/gmp/commands/messaging/JmsActionMessageBuilderTest.java
@@ -1,7 +1,12 @@
 package edu.gemini.aspen.gmp.commands.messaging;
 
-import edu.gemini.aspen.gmp.commands.model.ActionMessageBuilderTestBase;
+import com.gargoylesoftware.base.testing.EqualsTester;
+import edu.gemini.aspen.gmp.commands.model.ActionMessage;
 import edu.gemini.aspen.gmp.commands.model.ActionMessageBuilder;
+import edu.gemini.aspen.gmp.commands.model.ActionMessageBuilderTestBase;
+import org.junit.Test;
+
+import static edu.gemini.aspen.giapi.commands.ConfigPath.configPath;
 
 /**
  * Test case for the Action Message builder based on JMS
@@ -10,5 +15,16 @@ public class JmsActionMessageBuilderTest extends ActionMessageBuilderTestBase {
 
     protected ActionMessageBuilder getActionMessageBuilder() {
         return new JmsActionMessageBuilder();
+    }
+
+    @Test
+    public void testActionMessageEquality() {
+        JmsActionMessageBuilder messageBuilder = new JmsActionMessageBuilder();
+        ActionMessage a = messageBuilder.buildActionMessage(action);
+        ActionMessage b = messageBuilder.buildActionMessage(action);
+        ActionMessage c = messageBuilder.buildActionMessage(action, configPath("Y"));
+        ActionMessage d = null;
+
+        new EqualsTester(a, b, c, d);
     }
 }
