20ab5b7f255db08f83b8f01254ebfda414c1e504
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-03-10 14:43:39 +0000

    Added tests for exception when binding channels
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@32592 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- epics-service/src/test/java/edu/gemini/epics/impl/EpicsBaseTest.java
+++ epics-service/src/test/java/edu/gemini/epics/impl/EpicsBaseTest.java
@@ -1,36 +1,55 @@
 package edu.gemini.epics.impl;
 
+import edu.gemini.epics.EpicsException;
 import gov.aps.jca.CAException;
 import gov.aps.jca.Channel;
 import gov.aps.jca.Context;
+import gov.aps.jca.TimeoutException;
 import org.junit.Test;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.mockito.Matchers.anyDouble;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class EpicsBaseTest {
+    private static final String CHANNEL_NAME = "tst:tst";
+    private final Context context = mock(Context.class);
+    private final Channel channel = mock(Channel.class);
+
     @Test
     public void testChannelBinding() throws CAException {
-        Context context = mock(Context.class);
-        String channelName = "tst:tst";
-        Channel channel = mock(Channel.class);
-        when(context.createChannel(channelName)).thenReturn(channel);
+        when(context.createChannel(CHANNEL_NAME)).thenReturn(channel);
 
         EpicsBase epicsBase = new EpicsBase(context);
-        epicsBase.bindChannel(channelName);
+        epicsBase.bindChannel(CHANNEL_NAME);
 
-        assertNotNull(epicsBase.getChannel(channelName));
+        assertNotNull(epicsBase.getChannel(CHANNEL_NAME));
     }
 
     @Test
     public void testFindUnknownChannel() {
-        Context context = mock(Context.class);
-        String channelName = "tst:tst";
-
         EpicsBase epicsBase = new EpicsBase(context);
 
-        assertNull(epicsBase.getChannel(channelName));
+        assertNull(epicsBase.getChannel(CHANNEL_NAME));
+    }
+
+    @Test(expected = EpicsException.class)
+    public void testCAExceptionWhileBindingChannel() throws CAException {
+        when(context.createChannel(CHANNEL_NAME)).thenThrow(new CAException());
+
+        EpicsBase epicsBase = new EpicsBase(context);
+        epicsBase.bindChannel(CHANNEL_NAME);
+    }
+
+    @Test(expected = EpicsException.class)
+    public void testTimeoutWhileBindingChannel() throws CAException, TimeoutException {
+        when(context.createChannel(CHANNEL_NAME)).thenReturn(channel);
+        doThrow(new TimeoutException()).when(context).pendIO(anyDouble());
+
+        EpicsBase epicsBase = new EpicsBase(context);
+        epicsBase.bindChannel(CHANNEL_NAME);
     }
 }
