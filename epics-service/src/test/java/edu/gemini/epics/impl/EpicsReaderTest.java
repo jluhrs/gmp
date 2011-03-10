628d6ad45c32c616405af67c112cd7e19c2e1028
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-03-10 15:43:30 +0000

    Added test for time out expcetion
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@32600 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- epics-service/src/test/java/edu/gemini/epics/impl/EpicsReaderTest.java
+++ epics-service/src/test/java/edu/gemini/epics/impl/EpicsReaderTest.java
@@ -4,11 +4,14 @@ import edu.gemini.epics.EpicsException;
 import gov.aps.jca.CAException;
 import gov.aps.jca.Channel;
 import gov.aps.jca.Context;
+import gov.aps.jca.TimeoutException;
 import gov.aps.jca.dbr.DBR_Float;
 import org.junit.Test;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertNull;
+import static org.mockito.Matchers.anyDouble;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -44,10 +47,25 @@ public class EpicsReaderTest {
     @Test(expected = EpicsException.class)
     public void testReadValueWithCAException() throws CAException {
         when(context.createChannel(CHANNEL_NAME)).thenReturn(channel);
-        when(channel.get()).thenThrow(new CAException());
+        float[] simulatedValue = {1, 2};
+        when(channel.get()).thenReturn(new DBR_Float(simulatedValue));
 
         EpicsReader epicsReader = new EpicsReader(context);
         epicsReader.bindChannel(CHANNEL_NAME);
+
+        epicsReader.getValue(CHANNEL_NAME);
+    }
+
+    @Test(expected = EpicsException.class)
+    public void testReadValueWithTimeoutException() throws CAException, TimeoutException {
+        when(context.createChannel(CHANNEL_NAME)).thenReturn(channel);
+        when(channel.get()).thenThrow(new CAException());
+        when(channel.getContext()).thenReturn(context);
+
+        EpicsReader epicsReader = new EpicsReader(context);
+        epicsReader.bindChannel(CHANNEL_NAME);
+
+        doThrow(new TimeoutException()).when(context).pendIO(anyDouble());
 
         epicsReader.getValue(CHANNEL_NAME);
     }
