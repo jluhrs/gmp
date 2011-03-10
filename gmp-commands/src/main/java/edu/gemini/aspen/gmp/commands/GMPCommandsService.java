8928499adb935d21f6578cbc32f3cc3cdf130250
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-03-10 13:15:54 +0000

    Removed iPojo annotations that break the build
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@32582 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- gmp-commands/src/main/java/edu/gemini/aspen/gmp/commands/GMPCommandsService.java
+++ gmp-commands/src/main/java/edu/gemini/aspen/gmp/commands/GMPCommandsService.java
@@ -1,20 +1,17 @@
 package edu.gemini.aspen.gmp.commands;
 
 import edu.gemini.aspen.gmp.commands.model.ActionManager;
-import org.apache.felix.ipojo.annotations.Component;
-import org.apache.felix.ipojo.annotations.Invalidate;
-import org.apache.felix.ipojo.annotations.Validate;
 
 //@Component
 public class GMPCommandsService {
     private final ActionManager _actionManager = new ActionManager();
 
-    @Validate
+    //@Validate
     public void startService() {
         _actionManager.start();
     }
 
-    @Invalidate
+    //@Invalidate
     public void stopService() {
         _actionManager.stop();
     }
