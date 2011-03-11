75fc73c3f7444630c4ef957dd01d88a0669a6f48
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-03-11 20:35:52 +0000

    Deleted Activator, all the functionality is now in the EpicsServiceImpl Component
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@32654 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- epics-service/src/test/java/edu/gemini/epics/impl/EpicsServiceImplTest.java
+++ epics-service/src/test/java/edu/gemini/epics/impl/EpicsServiceImplTest.java
@@ -1,8 +1,10 @@
 package edu.gemini.epics.impl;
 
 import com.google.common.collect.ImmutableMap;
+import edu.gemini.epics.EpicsService;
 import edu.gemini.epics.IEpicsClient;
 import gov.aps.jca.Context;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -12,9 +14,16 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyZeroInteractions;
 
 public class EpicsServiceImplTest {
+    private static final ImmutableMap<String,Object> CHANNELS_TO_READ = ImmutableMap.<String, Object>of(IEpicsClient.EPICS_CHANNELS, new String[] {"tst:tst"});
+    private EpicsServiceImpl epicsService;
+
+    @Before
+    public void setUp() throws Exception {
+        epicsService = new EpicsServiceImpl();
+    }
+
     @Test
     public void testNormalProcessing() {
-        EpicsServiceImpl epicsService = new EpicsServiceImpl();
         epicsService.startService();
 
         assertNotNull(epicsService.getJCAContext());
@@ -24,11 +33,10 @@ public class EpicsServiceImplTest {
 
     @Test
     public void testBindingEpicsClient() {
-        EpicsServiceImpl epicsService = new EpicsServiceImpl();
         epicsService.startService();
 
         IEpicsClient epicsClient = mock(IEpicsClient.class);
-        epicsService.bindEpicsClient(epicsClient, ImmutableMap.<String, Object>of(IEpicsClient.EPICS_CHANNELS, new String[] {"tst:tst"}));
+        epicsService.bindEpicsClient(epicsClient, CHANNELS_TO_READ);
 
         epicsService.stopService();
 
@@ -37,30 +45,38 @@ public class EpicsServiceImplTest {
 
     @Test
     public void testBindingEpicsClientBeforeStartingTheService() {
-        EpicsServiceImpl epicsService = new EpicsServiceImpl();
-
         IEpicsClient epicsClient = mock(IEpicsClient.class);
-        epicsService.bindEpicsClient(epicsClient, ImmutableMap.<String, Object>of(IEpicsClient.EPICS_CHANNELS, new String[]{"tst:tst"}));
+        epicsService.bindEpicsClient(epicsClient, CHANNELS_TO_READ);
         
         verifyZeroInteractions(epicsClient);
         epicsService.startService();
 
         verify(epicsClient).connected();
         epicsService.stopService();
+    }
 
+    @Test
+    public void testUnbindingEpicsClient() {
+        epicsService.startService();
+        IEpicsClient epicsClient = mock(IEpicsClient.class);
+        epicsService.bindEpicsClient(epicsClient, CHANNELS_TO_READ);
+        verify(epicsClient).connected();
+
+        epicsService.unbindEpicsClient(epicsClient);
+        verify(epicsClient).disconnected();
+
+        epicsService.stopService();
     }
 
     @Test(expected = IllegalStateException.class)
     public void testGetContextBeforeReady() {
-        EpicsServiceImpl epicsService = new EpicsServiceImpl();
-
         epicsService.getJCAContext();
     }
 
     @Test
     public void testGetContextPassedInitially() {
         Context context = mock(Context.class);
-        EpicsServiceImpl epicsService = new EpicsServiceImpl(context);
+        EpicsService epicsService = new EpicsServiceImpl(context);
         assertEquals(context, epicsService.getJCAContext());
     }
 }
