87cf3a57c79e150c6447245d1f84a1e4b532d365
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-03-11 18:14:11 +0000

    Converted EpicsWriter to iPojo Service
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@32646 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- epics-service/src/main/java/edu/gemini/epics/impl/EpicsWriter.java
+++ epics-service/src/main/java/edu/gemini/epics/impl/EpicsWriter.java
@@ -5,15 +5,31 @@ import edu.gemini.epics.EpicsService;
 import edu.gemini.epics.IEpicsWriter;
 import gov.aps.jca.CAException;
 import gov.aps.jca.Channel;
+import org.apache.felix.ipojo.annotations.Component;
+import org.apache.felix.ipojo.annotations.Instantiate;
+import org.apache.felix.ipojo.annotations.Provides;
+import org.apache.felix.ipojo.annotations.Requires;
+import org.apache.felix.ipojo.annotations.Validate;
+
+import java.util.logging.Logger;
 
 /**
  * Implementation of the EpicsWriter interface using JCA
  */
-//@Provides(specifications = IEpicsWriter.class)
+@Component
+@Instantiate
+@Provides(specifications = IEpicsWriter.class)
 public class EpicsWriter extends EpicsBase implements IEpicsWriter {
+    private static final Logger LOG = Logger.getLogger(EpicsWriter.class.getName());
 
-    public EpicsWriter(EpicsService epicsService) throws CAException {
+    public EpicsWriter(@Requires EpicsService epicsService) throws CAException {
         super(epicsService);
+        startEpicsWriter();
+    }
+
+    @Validate
+    public void startEpicsWriter() {
+        LOG.info("EpicsWriter ready" );
     }
 
     public void write(String channelName, Double value) throws EpicsException {
