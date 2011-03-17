668649b50e63367860e648b1f7c515a9394cdacf
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-03-17 18:44:30 +0000

    Refactored mocking of Sessions to test JmsArtifacts to base class and move it to giapi-test-support
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@32882 ee6ba543-07f8-4773-b02c-01d50e9d00ba

---
+++ giapi-test-support/src/main/java/edu/gemini/aspen/giapitestsupport/jms/JmsArtifactTestBase.java
@@ -0,0 +1,43 @@
+package edu.gemini.aspen.giapitestsupport.jms;
+
+import edu.gemini.jms.api.JmsProvider;
+import org.mockito.Matchers;
+import org.mockito.Mockito;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+
+public class JmsArtifactTestBase {
+    protected JmsProvider provider;
+
+    protected Session mockSessionProducerAndConsumer(JmsProvider provider) throws JMSException {
+        Session session = mockSessionCreation(provider);
+
+        MessageProducer producer = Mockito.mock(MessageProducer.class);
+        Mockito.when(session.createProducer(Matchers.<Destination>anyObject())).thenReturn(producer);
+        MessageConsumer consumer = Mockito.mock(MessageConsumer.class);
+        Mockito.when(session.createConsumer(Matchers.<Destination>anyObject())).thenReturn(consumer);
+
+        return session;
+    }
+
+    private Session mockSessionCreation(JmsProvider provider) throws JMSException {
+        Session session = Mockito.mock(Session.class);
+        // Mock connection factory
+        ConnectionFactory connectionFactory = Mockito.mock(ConnectionFactory.class);
+        Mockito.when(provider.getConnectionFactory()).thenReturn(connectionFactory);
+
+        // Mock connection
+        Connection connection = Mockito.mock(Connection.class);
+        Mockito.when(connectionFactory.createConnection()).thenReturn(connection);
+
+        // Mock session
+        Mockito.when(connection.createSession(Matchers.anyBoolean(), Matchers.anyInt())).thenReturn(session);
+        return session;
+    }
+}
--- jms-api/src/test/java/edu/gemini/jms/api/JmsMapMessageSenderTest.java
+++ jms-api/src/test/java/edu/gemini/jms/api/JmsMapMessageSenderTest.java
@@ -1,28 +1,20 @@
 package edu.gemini.jms.api;
 
 import com.google.common.collect.ImmutableMap;
+import edu.gemini.aspen.giapitestsupport.jms.JmsArtifactTestBase;
 import org.junit.Test;
-import org.mockito.Matchers;
 
-import javax.jms.Connection;
-import javax.jms.ConnectionFactory;
 import javax.jms.Destination;
 import javax.jms.JMSException;
 import javax.jms.MapMessage;
-import javax.jms.MessageConsumer;
-import javax.jms.MessageProducer;
 import javax.jms.Session;
 import java.util.Map;
 
 import static org.junit.Assert.assertNotNull;
-import static org.mockito.Matchers.anyBoolean;
-import static org.mockito.Matchers.anyInt;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-public class JmsMapMessageSenderTest {
-
-    private JmsProvider provider;
+public class JmsMapMessageSenderTest extends JmsArtifactTestBase {
 
     @Test
     public void testSendMapMessage() {
@@ -54,29 +46,4 @@ public class JmsMapMessageSenderTest {
                 JmsMapMessageSender.MapMessageCreator.NoReplyCreator));
     }
 
-    private Session mockSessionProducerAndConsumer(JmsProvider provider) throws JMSException {
-        Session session = mockSessionCreation(provider);
-
-        MessageProducer producer = mock(MessageProducer.class);
-        when(session.createProducer(Matchers.<Destination>anyObject())).thenReturn(producer);
-        MessageConsumer consumer = mock(MessageConsumer.class);
-        when(session.createConsumer(Matchers.<Destination>anyObject())).thenReturn(consumer);
-
-        return session;
-    }
-
-    private Session mockSessionCreation(JmsProvider provider) throws JMSException {
-        Session session = mock(Session.class);
-        // Mock connection factory
-        ConnectionFactory connectionFactory = mock(ConnectionFactory.class);
-        when(provider.getConnectionFactory()).thenReturn(connectionFactory);
-
-        // Mock connection
-        Connection connection = mock(Connection.class);
-        when(connectionFactory.createConnection()).thenReturn(connection);
-
-        // Mock session
-        when(connection.createSession(anyBoolean(), anyInt())).thenReturn(session);
-        return session;
-    }
 }
