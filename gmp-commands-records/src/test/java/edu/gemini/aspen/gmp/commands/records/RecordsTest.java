a81215e16bf93e1fd6aebe5fca488d9641e08c3c
nbarriga <nbarriga@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-03-22 15:40:27 +0000

    Test MARK and CAR behavior in CAD record.
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@32988 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- gmp-commands-records/src/test/java/edu/gemini/aspen/gmp/commands/records/RecordsTest.java
+++ gmp-commands-records/src/test/java/edu/gemini/aspen/gmp/commands/records/RecordsTest.java
@@ -2,11 +2,16 @@ package edu.gemini.aspen.gmp.commands.records;
 
 import edu.gemini.cas.Channel;
 import edu.gemini.cas.ChannelAccessServer;
+import edu.gemini.cas.ChannelListener;
 import edu.gemini.cas.impl.ChannelAccessServerImpl;
 import gov.aps.jca.CAException;
+import gov.aps.jca.dbr.DBR;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.*;
 
@@ -62,13 +67,68 @@ public class RecordsTest {
 
     }
 
+    private class ChangeListener extends CountDownLatch implements ChannelListener{
+
+        public ChangeListener() {
+            super(1);
+        }
+
+        @Override
+        public void valueChange(DBR dbr) {
+           countDown();
+        }
+
+    }
+
+    private class CARListener extends CountDownLatch implements ChannelListener{
+
+        public CARListener() {
+            super(2);
+        }
+
+        @Override
+        public void valueChange(DBR dbr) {
+            if (getCount() == 2 && "BUSY".equals(((String[]) dbr.getValue())[0])) {
+                countDown();
+            }
+            if (getCount() == 1 && "IDLE".equals(((String[]) dbr.getValue())[0])) {
+                countDown();
+            }
+        }
+
+    }
+
     @Test
-    public void CADTest(){
+    public void CADTest() throws CAException, InterruptedException {
         CADRecordImpl cad = new CADRecordImpl(cas,prefix,cadName,3);
         cad.start();
 
         //test different transitions
         //test that car goes to busy, then to idle
+
+
+        //test mark and CAR
+        Channel<String> a = cas.createChannel(prefix+cadName+".A","");
+        Channel<Integer> mark = cas.createChannel(prefix+cadName+".MARK",0);
+        Channel<CARRecord.Val> carVal = cas.createChannel(prefix+cadName+"C.VAL",CARRecord.Val.IDLE);
+
+        ChangeListener listener = new ChangeListener();
+        mark.registerListener(listener);
+
+        CARListener carListener = new CARListener();
+        carVal.registerListener(carListener);
+
+        a.setValue("anything");
+
+        if(listener.await(1, TimeUnit.SECONDS)){
+            assertEquals(new Integer(1), mark.getVal().get(0));
+        }else{
+            fail();
+        }
+        if(!listener.await(1, TimeUnit.SECONDS)){
+            fail();
+        }
+
 
         cad.stop();
 
