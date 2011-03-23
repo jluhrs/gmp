038730aa289ff6a99fe9612145064af40db8db43
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-03-23 18:48:51 +0000

    Added new tests for NO_COMMAND
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@33055 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- giapi/src/test/java/edu/gemini/aspen/giapi/commands/CommandTest.java
+++ giapi/src/test/java/edu/gemini/aspen/giapi/commands/CommandTest.java
@@ -6,13 +6,18 @@ import org.junit.Test;
 import static edu.gemini.aspen.giapi.commands.ConfigPath.configPath;
 import static edu.gemini.aspen.giapi.commands.DefaultConfiguration.configurationBuilder;
 import static edu.gemini.aspen.giapi.commands.DefaultConfiguration.emptyConfiguration;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 public class CommandTest {
     @Test
     public void testBuildApplyCommand() {
         Command applyCommand = new Command(SequenceCommand.APPLY, Activity.START, emptyConfiguration());
         assertNotNull(applyCommand);
+        assertTrue(applyCommand.isApply());
+        assertFalse(applyCommand.toString().isEmpty());
     }
 
     @Test(expected = IllegalArgumentException.class)
@@ -31,6 +36,16 @@ public class CommandTest {
         Configuration rebootConfiguration = configurationBuilder().withPath(configPath("REBOOT_OPT"), "REBOOT").build();
         Command rebootCommand = new Command(SequenceCommand.REBOOT, Activity.START, rebootConfiguration);
         assertNotNull(rebootCommand);
+
+        assertEquals(SequenceCommand.REBOOT, rebootCommand.getSequenceCommand());
+        assertEquals(Activity.START, rebootCommand.getActivity());
+        assertEquals(rebootConfiguration, rebootCommand.getConfiguration());
+    }
+
+    @Test
+    public void testNoCommand() {
+        Command noCommand = Command.noCommand();
+        assertNotNull(noCommand);
     }
 
     @Test(expected = IllegalArgumentException.class)
@@ -44,6 +59,10 @@ public class CommandTest {
         Command b = new Command(SequenceCommand.APPLY, Activity.START, emptyConfiguration());
         Command c = new Command(SequenceCommand.PARK, Activity.START, emptyConfiguration());
         Command d = new Command(SequenceCommand.APPLY, Activity.START, emptyConfiguration()) {};
+        new EqualsTester(a,b,c,d);
+
+        Configuration simpleConfig = configurationBuilder().withPath(configPath("x:A.v"), "1").build();
+        c = new Command(SequenceCommand.APPLY, Activity.START, simpleConfig);
         new EqualsTester(a,b,c,d);
     }
 }
