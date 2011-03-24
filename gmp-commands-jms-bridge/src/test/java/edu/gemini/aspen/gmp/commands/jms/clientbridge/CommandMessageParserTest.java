b52a7fe91bc0f67456274c05acf50118f402a092
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-03-24 15:06:03 +0000

    Additional unit tests to ensure only FormatExceptions are created
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@33083 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- gmp-commands-jms-bridge/src/test/java/edu/gemini/aspen/gmp/commands/jms/clientbridge/CommandMessageParserTest.java
+++ gmp-commands-jms-bridge/src/test/java/edu/gemini/aspen/gmp/commands/jms/clientbridge/CommandMessageParserTest.java
@@ -11,12 +11,14 @@ import org.junit.Before;
 import org.junit.Test;
 
 import javax.jms.JMSException;
+import javax.jms.MapMessage;
 import javax.jms.Message;
 
 import static edu.gemini.aspen.giapi.commands.ConfigPath.configPath;
 import static edu.gemini.aspen.giapi.commands.DefaultConfiguration.configurationBuilder;
 import static junit.framework.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class CommandMessageParserTest {
     private MapMessageMock msg;
@@ -59,8 +61,29 @@ public class CommandMessageParserTest {
     }
 
     @Test(expected = FormatException.class)
+    public void parseMessageWithoutSequenceKey() throws JMSException {
+        msg.setJMSCorrelationID("1");
+        msg.setStringProperty(JmsKeys.GMP_ACTIVITY_KEY, Activity.PRESET.toString());
+        new CommandMessageParser(msg).readCommand();
+    }
+
+    @Test(expected = FormatException.class)
+    public void parseMessageWithUnknownSequenceKey() throws JMSException {
+        msg.setJMSCorrelationID("1");
+        msg.setStringProperty(JmsKeys.GMP_SEQUENCE_COMMAND_KEY, "FAKECOMMAND");
+        new CommandMessageParser(msg).readCommand();
+    }
+
+    @Test(expected = FormatException.class)
     public void parseNonMapMessage() throws JMSException {
         Message msg = mock(Message.class);
         new CommandMessageParser(msg);
+    }
+
+    @Test(expected = FormatException.class)
+    public void testJmsExceptionsConvertedToFormatException() throws JMSException {
+        MapMessage msg = mock(MapMessage.class);
+        when(msg.getJMSCorrelationID()).thenThrow(new JMSException("Exception"));
+        new CommandMessageParser(msg).readCommand();
     }
 }
