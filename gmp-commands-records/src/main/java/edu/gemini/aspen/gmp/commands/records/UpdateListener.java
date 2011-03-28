91b87c4d52d63c743d7d053f8814366679fa6bc1
nbarriga <nbarriga@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-03-28 12:53:13 +0000

    Small javadoc update.
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@33194 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- gmp-commands-records/src/main/java/edu/gemini/aspen/gmp/commands/records/UpdateListener.java
+++ gmp-commands-records/src/main/java/edu/gemini/aspen/gmp/commands/records/UpdateListener.java
@@ -8,6 +8,9 @@ import java.util.concurrent.CountDownLatch;
 /**
 * This listener is to be used when someone wants to wait for a value change in any Channel.
 *
+* Implementation details: this listener is a CountDownLatch of size 1, meaning that after 1 update, the latch will be
+* released. Inheriting from CountDownLatch looks much cleaner than delegation/composition.
+*
 * @author Nicolas A. Barriga
 *         Date: 3/24/11
 */
