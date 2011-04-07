4e62bce0dd4cb452ee4fe50d64ede771cbe625db
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-04-07 15:06:33 +0000

    Tests for the EpicsClientSubscriber
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@33469 ee6ba543-07f8-4773-b02c-01d50e9d00ba

---
+++ epics-service/src/test/java/edu/gemini/epics/impl/EpicsClientSubscriberTest.java
@@ -0,0 +1,72 @@
+package edu.gemini.epics.impl;
+
+import com.google.common.collect.ImmutableMap;
+import edu.gemini.epics.EpicsClient;
+import edu.gemini.epics.EpicsClientMock;
+import edu.gemini.epics.EpicsObserver;
+import edu.gemini.epics.EpicsService;
+import gov.aps.jca.Channel;
+import gov.aps.jca.Context;
+import gov.aps.jca.event.ConnectionListener;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Matchers;
+
+import java.util.Map;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Tests for {@link EpicsClientSubscriber}
+ */
+public class EpicsClientSubscriberTest {
+    private static final String[] CHANNELS_TO_READ = new String[] {"tst:tst"};
+    private Map<String, Object> serviceProperties = ImmutableMap.<String, Object>of(EpicsClient.EPICS_CHANNELS, CHANNELS_TO_READ);
+    private Context jcaContext;
+    private EpicsService epicsService;
+    private EpicsObserver epicsObserver;
+    private EpicsClientSubscriber subscriber;
+
+    @Before
+    public void setUp() throws Exception {
+        jcaContext = mock(Context.class);
+        
+        Channel channel = mock(Channel.class);
+        when(jcaContext.createChannel(Matchers.<String>any(), Matchers.<ConnectionListener>anyObject())).thenReturn(channel);
+
+        epicsService = new EpicsService(jcaContext);
+        epicsObserver = new EpicsObserverImpl(epicsService);
+        subscriber = new EpicsClientSubscriber(epicsObserver);
+    }
+
+    @Test
+    public void testServiceRegistration() {
+        EpicsClientMock epicsClient = new EpicsClientMock();
+
+        subscriber.bindEpicsClient(epicsClient, serviceProperties);
+
+        assertTrue(epicsClient.wasConnectedCalled());
+    }
+
+    @Test
+    public void testServiceRegistrationWithNoProperties() {
+        EpicsClientMock epicsClient = new EpicsClientMock();
+
+        subscriber.bindEpicsClient(epicsClient, ImmutableMap.<String, Object>of());
+
+        assertFalse(epicsClient.wasConnectedCalled());
+    }
+
+    @Test
+    public void testServiceRegistrationAndUnRegistration() {
+        EpicsClientMock epicsClient = new EpicsClientMock();
+
+        subscriber.bindEpicsClient(epicsClient, serviceProperties);
+        subscriber.unbindEpicsClient(epicsClient);
+
+        assertTrue(epicsClient.wasDisconnectedCalled());
+    }
+}
