61b0bea16ce4d12b2328244205c831b86290992c
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-04-14 20:14:36 +0000

    Added skeleton of gds-keyword-sets-composer and a new scala pom to be used by scala modules
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@33767 ee6ba543-07f8-4773-b02c-01d50e9d00ba

---
+++ gds-keyword-sets-composer/src/main/scala/edu/gemini/aspen/gds/keywordssets/Example.scala
@@ -0,0 +1,11 @@
+package edu.gemini.aspen.gds.keywordssets;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: cquiroz
+ * Date: 4/14/11
+ * Time: 2:59 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class Example {
+}
