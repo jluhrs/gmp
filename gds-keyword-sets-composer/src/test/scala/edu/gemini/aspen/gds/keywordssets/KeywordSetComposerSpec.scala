0f80514d183fde86c77545bab1713617bf08bbaa
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-04-15 19:16:49 +0000

    Added feature spec and mark it as pending
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@33824 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- gds-keyword-sets-composer/src/test/scala/edu/gemini/aspen/gds/keywordssets/KeywordSetComposerSpec.scala
+++ gds-keyword-sets-composer/src/test/scala/edu/gemini/aspen/gds/keywordssets/KeywordSetComposerSpec.scala
@@ -2,8 +2,11 @@ package edu.gemini.aspen.gds.keywordssets
 
 import org.scalatest.junit.JUnitRunner
 import org.junit.runner.RunWith
+import org.scalatest.{FeatureSpec, Spec}
 
 @RunWith(classOf[JUnitRunner])
-class KeywordSetComposerSpec  {
-
+class KeywordSetComposerSpec extends FeatureSpec {
+    feature("Keyword Set should accept init messages") {
+        scenario("init") (pending)
+    }
 }
