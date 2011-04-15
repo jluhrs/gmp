249f66f4bdc56c1016ab68ef512fdc6af479b250
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-04-15 12:41:06 +0000

    Added sample scala module including a test
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@33789 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- gds-keyword-sets-composer/src/main/scala/edu/gemini/aspen/gds/keywordssets/Example.scala
+++ gds-keyword-sets-composer/src/main/scala/edu/gemini/aspen/gds/keywordssets/Example.scala
@@ -1,11 +1,8 @@
-package edu.gemini.aspen.gds.keywordssets;
+package edu.gemini.aspen.gds.keywordssets
 
-/**
- * Created by IntelliJ IDEA.
- * User: cquiroz
- * Date: 4/14/11
- * Time: 2:59 PM
- * To change this template use File | Settings | File Templates.
- */
+import org.apache.felix.ipojo.annotations.{Instantiate, Component}
+
+@Component
+@Instantiate
 class Example {
 }
---
+++ gds-keyword-sets-composer/src/test/scala/edu/gemini/aspen/gds/keywordssets/ExampleTest.scala
@@ -0,0 +1,17 @@
+/*
+ * Created by IntelliJ IDEA.
+ * User: cquiroz
+ * Date: 4/15/11
+ * Time: 9:35 AM
+ */
+package edu.gemini.aspen.gds.keywordssets
+
+import org.junit.Test
+
+class ExampleTest {
+    @Test
+    def testOne() {
+        new Example();
+        System.out.println("test");
+    }
+}
