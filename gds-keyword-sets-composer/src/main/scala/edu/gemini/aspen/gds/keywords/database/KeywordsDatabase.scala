38e1c4837e7fc7b0ede27e78ce7adaeb41cfcb89
nbarriga <nbarriga@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-04-20 20:48:53 +0000

    Added retrieveAll to KeywordsDatabase and some javadoc
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@33960 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- gds-keyword-sets-composer/src/main/scala/edu/gemini/aspen/gds/keywords/database/KeywordsDatabase.scala
+++ gds-keyword-sets-composer/src/main/scala/edu/gemini/aspen/gds/keywords/database/KeywordsDatabase.scala
@@ -4,10 +4,37 @@ import collection.mutable.HashMap
 import org.apache.felix.ipojo.annotations._
 import edu.gemini.aspen.giapi.data.Dataset
 
+/**
+ * Interface for the database
+ */
 trait KeywordsDatabase {
+  /**
+   * Store the keyword
+   *
+   * @param dataSet to which the keywords belong
+   * @param keyword keyword to store
+   * @param value value to associate to the keyword
+   */
   def store(dataSet:Dataset, keyword:String, value:AnyRef)
 
+  /**
+   * Retrieve data from the database
+   *
+   * @param dataSet from which to retrieve data
+   * @param keyword keyword to retrieve
+   *
+   * @return Option containing the value if it was found in the DB
+   */
   def retrieve(dataSet:Dataset, keyword:String):Option[AnyRef]
+
+  /**
+   * Retrieve all the data associated to a given data set
+   *
+   * @param dataSet for which to retrieve data
+   *
+   * @return a HashMap[String, AnyRef] containing the data for the given data set
+   */
+  def retrieveAll(dataSet:Dataset):Option[HashMap[String, AnyRef]]
 
 }
 
@@ -31,6 +58,10 @@ class KeywordsDatabaseImpl extends KeywordsDatabase{
       return None
     }
     return innerMapOption.get.get(keyword)
+  }
+
+  def retrieveAll(dataSet: Dataset): Option[HashMap[String, AnyRef]] = {
+    return map.get(dataSet)
   }
 
   @Validate
--- gds-keyword-sets-composer/src/test/scala/edu/gemini/aspen/gds/keywords/database/KeywordsDatabaseTest.scala
+++ gds-keyword-sets-composer/src/test/scala/edu/gemini/aspen/gds/keywords/database/KeywordsDatabaseTest.scala
@@ -3,6 +3,7 @@ package edu.gemini.aspen.gds.keywords.database
 import org.scalatest.junit.AssertionsForJUnit
 import org.junit.Test
 import edu.gemini.aspen.giapi.data.Dataset
+import collection.mutable.HashMap
 
 
 class KeywordsDatabaseTest extends AssertionsForJUnit {
@@ -18,5 +19,24 @@ class KeywordsDatabaseTest extends AssertionsForJUnit {
     assert(db.retrieve(new Dataset("GS-2011B"),"gpi:keyword").isDefined)
 
     assert(value == db.retrieve(new Dataset("GS-2011B"),"gpi:keyword").get)
+  }
+
+  @Test
+  def testRetrieveAll() = {
+    val db:KeywordsDatabase = new KeywordsDatabaseImpl
+
+
+    val value:AnyRef = (0.1).asInstanceOf[AnyRef]
+
+    db.store(new Dataset("GS-2011B"),"gpi:keyword",value)
+
+    assert(db.retrieveAll(new Dataset("GS-2011B")).isDefined)
+
+    val map = new HashMap[String, AnyRef]()
+    map.put("gpi:keyword",value)
+    val storedMap = db.retrieveAll(new Dataset("GS-2011B")).get
+    for(entry <- storedMap){
+      assert(entry._2 == map.get(entry._1).orNull)
+    }
   }
 }
