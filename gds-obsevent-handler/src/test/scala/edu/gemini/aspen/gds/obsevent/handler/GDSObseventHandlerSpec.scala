3e44d826f025a31028408cd095a6bde171fe0b37
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-04-26 13:18:22 +0000

    Initial skeleton test
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@34057 ee6ba543-07f8-4773-b02c-01d50e9d00ba

---
+++ gds-obsevent-handler/src/test/scala/edu/gemini/aspen/gds/obsevent/handler/GDSObseventHandlerSpec.scala
@@ -0,0 +1,9 @@
+package edu.gemini.aspen.gds.obsevent.handler
+
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.Spec
+
+@RunWith(classOf[JUnitRunner])
+class GDSObseventHandlerSpec extends Spec with ShouldMatchers {
+}
