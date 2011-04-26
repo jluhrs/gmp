21b746bdfd264c789867908bf53e49a16eb5b3e7
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-04-26 12:15:37 +0000

    Skeleton observation event handler
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@34053 ee6ba543-07f8-4773-b02c-01d50e9d00ba

---
+++ gds-obsevent-handler/src/main/scala/edu/gemini/aspen/gds/obsevent/handler/GDSObseventHandler.scala
@@ -0,0 +1,23 @@
+package edu.gemini.aspen.gds.obsevent.handler
+
+import edu.gemini.aspen.giapi.data.ObservationEvent._
+import edu.gemini.aspen.giapi.data.{ObservationEvent, Dataset, ObservationEventHandler}
+import org.apache.felix.ipojo.annotations.{Requires, Provides, Instantiate, Component}
+import edu.gemini.aspen.gds.keywordssets.factory.StartAcquisitionActorsFactory
+import edu.gemini.aspen.gds.keywordssets.{Init, KeywordSetComposer}
+
+@Component
+@Instantiate
+@Provides(specifications = Array(classOf[ObservationEventHandler]))
+class GDSObseventHandler(@Requires observationStartActorsFactory: StartAcquisitionActorsFactory) extends ObservationEventHandler {
+    def onObservationEvent(event: ObservationEvent, dataset: Dataset) {
+        event match {
+            case OBS_START_ACQ => startAcquisition(dataset)
+            case _ => 
+        }
+    }
+
+    private def startAcquisition(dataset: Dataset) {
+        new KeywordSetComposer(observationStartActorsFactory) ! Init(dataset)
+    }
+}
