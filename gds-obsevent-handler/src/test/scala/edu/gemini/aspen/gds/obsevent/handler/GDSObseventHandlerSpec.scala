e7f546c4efb009f832b08f91db17ca22f2a17dbe
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-04-26 13:36:52 +0000

    Fixed missing import
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@34059 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- gds-obsevent-handler/src/test/scala/edu/gemini/aspen/gds/obsevent/handler/GDSObseventHandlerSpec.scala
+++ gds-obsevent-handler/src/test/scala/edu/gemini/aspen/gds/obsevent/handler/GDSObseventHandlerSpec.scala
@@ -3,6 +3,7 @@ package edu.gemini.aspen.gds.obsevent.handler
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.matchers.ShouldMatchers
 import org.scalatest.Spec
+import org.junit.runner.RunWith
 
 @RunWith(classOf[JUnitRunner])
 class GDSObseventHandlerSpec extends Spec with ShouldMatchers {
