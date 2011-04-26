5253540acdfc94c97a0a72a07adcd6883a0e5203
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-04-26 12:16:39 +0000

    sun.misc package is required to run actors in OSGi
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@34054 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- gmp-server/startgmp.sh
+++ gmp-server/startgmp.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-pax-run.sh --keepOriginalUrls --vmo="-Dconf.base=../src/main/etc/conf -Dlogs.dir=logs -Xverify:none" --repositories=http://build.cl.gemini.edu:8081/artifactory/repo1,http://build.cl.gemini.edu:8081/artifactory/ext-releases-local scan-composite:file:$PWD/src/main/etc/conf/module.properties
+pax-run.sh --keepOriginalUrls --vmo="-Dconf.base=../src/main/etc/conf -Dlogs.dir=logs -Xverify:none -Dorg.osgi.framework.system.packages.extra=sun.misc" --repositories=http://build.cl.gemini.edu:8081/artifactory/repo1,http://build.cl.gemini.edu:8081/artifactory/ext-releases-local scan-composite:file:$PWD/src/main/etc/conf/module.properties
