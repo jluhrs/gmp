de5fee0dab11e9d3db1e53f718f456173fcac5d9
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-04-28 21:21:09 +0000

    Refined parser using case classes
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@34143 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- gds-keyword-sets-composer/src/main/scala/edu/gemini/aspen/gds/keywordssets/configuration/GDSConfigurationParser.scala
+++ gds-keyword-sets-composer/src/main/scala/edu/gemini/aspen/gds/keywordssets/configuration/GDSConfigurationParser.scala
@@ -6,17 +6,58 @@ import io.Source
 class GDSConfigurationParser extends RegexParsers {
     override val skipWhitespace = false
 
+    case class Instrument(name:String)
+    case class ObservationEvent(name:String)
+    case class Keyword(name:String)
+    case class DataType(name:String)
+    case class Mandatory(value:Boolean)
+    case class NullValue(value:String)
+    case class Subsystem(value:String)
+    case class Channel(value:String)
+    case class ArrayIndex(value:String)
+    case class Space(length:Int)
+
     def lines = rep(line) ~ EOF 
 
     def line = (comment | configuration | CRLF)
 
-    def configuration = spaces ~ instrument ~ spaces ~ instrument ~ spaces ~ instrument ~ spaces ~ instrument ~ spaces ~ instrument ~ spaces ~ spaces ~ instrument ~ spaces ~ spaces ~ instrument ~ spaces ~ spaces ~ instrument ~ spaces ~ spaces ~ instrument ~ spaces ~ fitscomment
+    def configuration = (spaces ~ instrument
+        ~ spaces ~ observationEvent
+        ~ spaces ~ keyword
+        ~ spaces ~ datatype
+        ~ spaces ~ mandatory
+        ~ spaces ~ nullValue
+        ~ spaces ~ subsystem
+        ~ spaces ~ channelName
+        ~ spaces ~ arrayIndex
+        ~ spaces ~ fitscomment)
 
-    def instrument = """[:\w]*""".r
+    def instrument = """\w*""".r ^^ {x => Instrument(x)}
+
+    def observationEvent = """[\p{Upper}_]*""".r ^^ {x => ObservationEvent(x)}
+
+    def keyword = """[\p{Upper}\d]{1,8}""".r ^^ {x => Keyword(x)}
+
+    def datatype = "DOUBLE" ^^ {x => DataType(x)}
+
+    def mandatory = """[tTfF]""".r ^^ {
+        case "F" => Mandatory(false)
+        case "f" => Mandatory(false)
+        case "T" => Mandatory(true)
+        case "t" => Mandatory(true)
+    }
+
+    def nullValue = """\w*""".r ^^ {x => NullValue(x)}
+
+    def subsystem = """\w*""".r ^^ {x => Subsystem(x)}
+
+    def channelName = """[:\w]*""".r ^^ {x => Channel(x)}
+
+    def arrayIndex = """\w*""".r ^^ {x => ArrayIndex(x)}
 
     def comment: Parser[String] = """#.*""".r
 
-    def fitscomment = "\"" ~ internalComment ~ "\""
+    def fitscomment = "\"" ~> internalComment <~ "\""
 
     def internalComment = """[^"]*""".r
 
