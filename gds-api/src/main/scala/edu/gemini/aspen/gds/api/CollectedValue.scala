ce360c14c88ede96ca542fd93d448baf9e9a0c53
nbarriga <nbarriga@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-05-02 17:34:25 +0000

    Reorganized implicit conversions
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@34204 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- gds-api/src/main/scala/edu/gemini/aspen/gds/api/CollectedValue.scala
+++ gds-api/src/main/scala/edu/gemini/aspen/gds/api/CollectedValue.scala
@@ -2,16 +2,21 @@ package edu.gemini.aspen.gds.api
 
 import edu.gemini.aspen.giapi.data.FitsKeyword
 import edu.gemini.fits.{HeaderItem, DefaultHeaderItem}
+import edu.gemini.aspen.gds.api.Conversions._
 
 /**
  * Message indicating the resulting values
  */
 case class CollectedValue(keyword:FitsKeyword, value:AnyRef, comment:String, index:Int)
 
-//object CollectedValue{
-//  implicit def toHeaderItem(collectedValue:CollectedValue):HeaderItem = DefaultHeaderItem.create(collectedValue.keyword.getName,collectedValue.value.toString,collectedValue.comment)
-////  implicit def fromHeaderItem(headerItem:HeaderItem):CollectedValue = new CollectedValue(new FitsKeyword(headerItem.getKeyword),headerItem.getValue,headerItem.getComment,0)
-////  implicit def fromDefaultHeaderItem(headerItem:DefaultHeaderItem):CollectedValue = new CollectedValue(new FitsKeyword(headerItem.getKeyword),headerItem.getValue,headerItem.getComment,0)
-//  //todo: check value conversion to string
-//}
+/**
+ * Companion object used to place implicit conversions
+ */
+object CollectedValue{
+  implicit def collectedValueToHeaderItem(collectedValue: CollectedValue): HeaderItem = DefaultHeaderItem.create(collectedValue.keyword.getName, collectedValue.value.toString, collectedValue.comment)
+
+  implicit def headerItemToCollectedValue(headerItem: HeaderItem): CollectedValue = new CollectedValue(headerItem.getKeyword, headerItem.getValue, headerItem.getComment, 0)
+
+  //todo: check value conversion to string
+}
 
--- gds-api/src/main/scala/edu/gemini/aspen/gds/api/Conversions.scala
+++ gds-api/src/main/scala/edu/gemini/aspen/gds/api/Conversions.scala
@@ -3,6 +3,10 @@ package edu.gemini.aspen.gds.api
 import edu.gemini.fits.{HeaderItem, DefaultHeaderItem}
 import edu.gemini.aspen.giapi.data.{DataLabel, FitsKeyword}
 
+
+/**
+ * Place to put implicit conversions for Java classes.
+ */
 object Conversions {
   implicit def fitsKeywordToString(fits: FitsKeyword) = fits.getName
 
@@ -11,10 +15,4 @@ object Conversions {
   implicit def dataLabelToString(dataLabel: DataLabel) = dataLabel.getName
 
   implicit def stringToDataLabel(s: String) = new DataLabel(s)
-
-  implicit def collectedValueToHeaderItem(collectedValue: CollectedValue): HeaderItem = DefaultHeaderItem.create(collectedValue.keyword.getName, collectedValue.value.toString, collectedValue.comment)
-
-  implicit def headerItemToCollectedValue(headerItem: HeaderItem): CollectedValue = new CollectedValue(headerItem.getKeyword, headerItem.getValue, headerItem.getComment, 0)
-
-  //todo: check value conversion to string
 }
