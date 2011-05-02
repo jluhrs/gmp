7f3674003657093fdce14a4928a79f0a2705dae0
nbarriga <nbarriga@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-05-02 17:06:54 +0000

    Changed implicit conversion names to lowercase
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@34202 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- gds-api/src/main/scala/edu/gemini/aspen/gds/api/Conversions.scala
+++ gds-api/src/main/scala/edu/gemini/aspen/gds/api/Conversions.scala
@@ -4,17 +4,17 @@ import edu.gemini.fits.{HeaderItem, DefaultHeaderItem}
 import edu.gemini.aspen.giapi.data.{DataLabel, FitsKeyword}
 
 object Conversions {
-  implicit def FitsKeywordToString(fits: FitsKeyword) = fits.getName
+  implicit def fitsKeywordToString(fits: FitsKeyword) = fits.getName
 
-  implicit def StringToFitsKeyword(s: String) = new FitsKeyword(s)
+  implicit def stringToFitsKeyword(s: String) = new FitsKeyword(s)
 
-  implicit def DataLabelToString(dataLabel: DataLabel) = dataLabel.getName
+  implicit def dataLabelToString(dataLabel: DataLabel) = dataLabel.getName
 
-  implicit def StringToDataLabel(s: String) = new DataLabel(s)
+  implicit def stringToDataLabel(s: String) = new DataLabel(s)
 
-  implicit def CollectedValueToHeaderItem(collectedValue: CollectedValue): HeaderItem = DefaultHeaderItem.create(collectedValue.keyword.getName, collectedValue.value.toString, collectedValue.comment)
+  implicit def collectedValueToHeaderItem(collectedValue: CollectedValue): HeaderItem = DefaultHeaderItem.create(collectedValue.keyword.getName, collectedValue.value.toString, collectedValue.comment)
 
-  implicit def HeaderItemToCollectedValue(headerItem: HeaderItem): CollectedValue = new CollectedValue(headerItem.getKeyword, headerItem.getValue, headerItem.getComment, 0)
+  implicit def headerItemToCollectedValue(headerItem: HeaderItem): CollectedValue = new CollectedValue(headerItem.getKeyword, headerItem.getValue, headerItem.getComment, 0)
 
   //todo: check value conversion to string
 }
