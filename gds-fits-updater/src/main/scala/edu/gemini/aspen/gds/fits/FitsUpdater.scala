f050d54a8a5a14e13d8ef8105f325f4a40647f10
anunez <anunez@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-05-03 15:25:28 +0000

    Added structure for the FitsUpdater code
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@34243 ee6ba543-07f8-4773-b02c-01d50e9d00ba

---
+++ gds-fits-updater/src/main/scala/edu/gemini/aspen/gds/fits/FitsUpdater.scala
@@ -0,0 +1,20 @@
+package edu.gemini.aspen.gds.fits
+
+import edu.gemini.aspen.giapi.data.DataLabel
+import edu.gemini.fits.Header
+
+/**
+ *
+ */
+
+class FitsUpdater {
+
+
+  def updateFitsHeaders(dataLabel: DataLabel, headers: List[Header]) {
+
+    //TODO: Carlos will fix this
+
+  }
+
+
+}
