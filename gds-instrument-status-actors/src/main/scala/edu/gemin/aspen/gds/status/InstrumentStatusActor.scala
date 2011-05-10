b124cddaafcf94b62b73f0d4f48ca777935e9aae
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-05-10 15:52:13 +0000

    Cleaned up code and added comments
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@34551 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- gds-instrument-status-actors/src/main/scala/edu/gemin/aspen/gds/status/InstrumentStatusActor.scala
+++ gds-instrument-status-actors/src/main/scala/edu/gemin/aspen/gds/status/InstrumentStatusActor.scala
@@ -5,12 +5,11 @@ import edu.gemini.aspen.gds.api.{GDSConfiguration, CollectedValue}
 import edu.gemini.aspen.giapi.status.{StatusItem, StatusDatabaseService}
 
 /**
- * Very simple actor that can produce as a reply of a Collect request a single value
- * linked to a single fitsKeyword
+ * Actor that can produce as a reply of a Collect request with the value of an StatusItem linked
+ * to a single fitsKeyword
  */
 class InstrumentStatusActor(statusDB: StatusDatabaseService, configuration:GDSConfiguration) extends KeywordValueActor {
     override def collectValues():List[CollectedValue] = {
-
         val (channelName, fitsKeyword, fitsComment, headerIndex) = (
                 configuration.channel.name,
                 configuration.keyword,
@@ -20,17 +19,8 @@ class InstrumentStatusActor(statusDB: StatusDatabaseService, configuration:GDSCo
 
         val statusItem:StatusItem[AnyRef] = statusDB.getStatusItem(channelName)
 
-
-//
-//        if (epicsValue.isInstanceOf[Array[Double]]) {
-//            // TODO: This should be done on the EpicsArray Actor
-//            CollectedValue(fitsKeyword, epicsValue.asInstanceOf[Array[Double]](0).asInstanceOf[AnyRef], fitsComment, headerIndex) :: Nil
-//        } else {
-        statusItem match {
-            // TODO cast to the right type
-            case v:StatusItem[AnyRef] => CollectedValue(fitsKeyword, v.getValue, fitsComment, headerIndex) :: Nil
-        }
-//        }
+        val value = statusItem.getValue
+        CollectedValue(fitsKeyword, value, fitsComment, headerIndex) :: Nil
     }
 
 }
--- gds-instrument-status-actors/src/main/scala/edu/gemin/aspen/gds/status/InstrumentStatusActorsFactory.scala
+++ gds-instrument-status-actors/src/main/scala/edu/gemin/aspen/gds/status/InstrumentStatusActorsFactory.scala
@@ -6,36 +6,40 @@ import org.apache.felix.ipojo.annotations._
 import edu.gemini.aspen.giapi.data.{ObservationEvent, DataLabel}
 import edu.gemin.aspen.gds.status.InstrumentStatusActor
 import edu.gemini.aspen.giapi.status.StatusDatabaseService
+import java.util.logging.Logger
 
+/**
+ * Factory of Actors that can retrieve instrument status
+ */
 @Component
 @Instantiate
 @Provides(specifications = Array(classOf[KeywordActorsFactory]))
 class InstrumentStatusActorsFactory(@Requires statusDB: StatusDatabaseService) extends KeywordActorsFactory {
-    // Use dummy configuration
-    var conf: List[GDSConfiguration] = List()
+    val LOG =  Logger.getLogger(classOf[InstrumentStatusActorsFactory])
+    var actorsConfiguration: List[GDSConfiguration] = List()
 
     override def startAcquisitionActors(dataLabel: DataLabel) = {
-        conf filter {_.event.name == ObservationEvent.OBS_START_ACQ.toString} map {
+        configurationsForEvent(ObservationEvent.OBS_START_ACQ) map {
             case config:GDSConfiguration => new InstrumentStatusActor(statusDB, config)
         }
     }
 
     override def endAcquisitionActors(dataLabel: DataLabel) = {
-        conf filter {_.event.name == ObservationEvent.OBS_END_ACQ.toString} map {
+        configurationsForEvent(ObservationEvent.OBS_END_ACQ) map {
             case config:GDSConfiguration => new InstrumentStatusActor(statusDB, config)
         }
     }
 
     override def configure(configuration:List[GDSConfiguration]) {
-        conf = configuration filter { _.subsystem.name == "STATUS"}
-//        // Bind all required channels
-//        conf map { x =>
-//            epicsReader.bindChannel(x.channel.name)
-//        }
+        actorsConfiguration = configuration filter { _.subsystem.name == "STATUS"}
     }
 
-    @Invalidate
-    def unbindAllChannels() {
-        // TODO remove all the bound channels
+    def configurationsForEvent(e: ObservationEvent) = {
+        actorsConfiguration filter {_.event.name == e.toString}
+    }
+
+    @Validate
+    def start() {
+        LOG.info("InstrumentStatusActorFactory started")
     }
 }
