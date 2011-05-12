e6ab8e3246d864959c9d1a9da10f5a9d85ee4f34
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-05-12 21:28:43 +0000

    Adde module gds-odb-actors
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@34637 ee6ba543-07f8-4773-b02c-01d50e9d00ba

---
+++ gds-odb-actors/src/main/scala/edu/gemini/aspen/gds/odb/ODBServiceTracker.scala
@@ -0,0 +1,45 @@
+package edu.gemini.aspen.gds.odb
+
+import org.apache.felix.ipojo.annotations.{Requires, Instantiate, Component, Validate}
+import org.osgi.service.jini.JiniDriver
+import net.jini.core.lookup.ServiceTemplate
+import edu.gemini.pot.spdb._
+import edu.gemini.pot.sp.{SPProgramID, ISPRemoteNode}
+import edu.gemini.spModel.gemini.obscomp.SPProgram
+
+@Component
+@Instantiate
+class ODBServiceTracker(@Requires jiniDriver: JiniDriver) {
+    @Validate
+    def validate() {
+        val st = new ServiceTemplate(null, Array[Class[_]](classOf[IDBDatabaseService]), null)
+        val templates = Array[ServiceTemplate](st)
+        jiniDriver.setServiceTemplates(templates)
+    }
+
+}
+
+@Component
+@Instantiate
+class DBServiceTracker(@Requires dbService: IDBDatabaseService) {
+    @Validate
+    def validate() {
+        val qr = dbService.getQueryRunner
+        val functor = qr.queryPrograms(new MyFunctor7("GS-2006B-Q-57")).asInstanceOf[MyFunctor7]
+        val piInfo = functor.program.getClientData("DataObject").asInstanceOf[SPProgram].getPIInfo
+        println(piInfo.getFirstName + " " + piInfo.getLastName)
+    }
+}
+
+@serializable class MyFunctor7(programID: String) extends DBAbstractQueryFunctor {
+    var program: ISPRemoteNode = null
+    def execute(p1: IDBDatabase, p2: ISPRemoteNode) {
+        val id = p2.getProgramID()
+
+        // Only look at observations titled "GS-ENG*"
+        if (id != null && id.stringValue().equals(programID)) {
+            println("Found " + p1 + " " + p2.getProgramID.stringValue)
+            program = p2
+        }
+    }
+}
