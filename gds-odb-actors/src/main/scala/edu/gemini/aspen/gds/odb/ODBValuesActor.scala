a03e96b45a2ec29383b3fac5f89bc07592328662
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-05-17 21:27:35 +0000

    Rebuild functor using the dataobject rather than ISPremotenode
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@34838 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- gds-odb-actors/src/main/scala/edu/gemini/aspen/gds/odb/ODBValuesActor.scala
+++ gds-odb-actors/src/main/scala/edu/gemini/aspen/gds/odb/ODBValuesActor.scala
@@ -4,7 +4,7 @@ import edu.gemini.aspen.gds.api.KeywordValueActor
 import edu.gemini.aspen.gds.api.{GDSConfiguration, CollectedValue}
 import edu.gemini.spModel.gemini.obscomp.SPProgram
 import edu.gemini.pot.spdb.{IDBDatabase, DBAbstractQueryFunctor, IDBQueryRunner}
-import edu.gemini.pot.sp.{SPProgramID, ISPRemoteNode}
+import edu.gemini.pot.sp.{ISPProgram, SPProgramID, ISPRemoteNode}
 
 /**
  * Actor that can produce as a reply of a Collect request with the value of an StatusItem linked
@@ -18,6 +18,8 @@ class ODBValuesActor(queryRunner: IDBQueryRunner, configuration: List[GDSConfigu
         "odb:piLastName" -> extractPILastName,
         "odb:piFirstName" -> extractPIFirstName)
 
+    start()
+
     override def collectValues(): List[CollectedValue] = {
         val functor = queryRunner.queryPrograms(GDSProgramQuery("GS-2006B-Q-57")).asInstanceOf[GDSProgramQuery]
         val spProgram = functor.program
@@ -28,13 +30,14 @@ class ODBValuesActor(queryRunner: IDBQueryRunner, configuration: List[GDSConfigu
     }
 
     private def collectValuesFromProgram(program: SPProgram): List[CollectedValue] = {
-        for {c <- configuration
+        val cv =for {c <- configuration
              val (fitsKeyword, fitsComment, headerIndex) = (
                      c.keyword,
                      c.fitsComment.value,
                      c.index.index)
              if extractorFunctions contains c.channel.name
              val r = extractorFunctions.getOrElse(c.channel.name, null)(program)
+
         }
         yield CollectedValue(fitsKeyword, r, fitsComment, headerIndex)
     }
@@ -58,20 +61,26 @@ abstract class GDSProgramQuery(programID: String) extends DBAbstractQueryFunctor
  *
  * Changing the code may be required when changing the functor code which is cached on the ODB
  */
-class GetProgramQuery(programID: String) extends GDSProgramQuery(programID) {
+class GetProgramQuery6(programID: String) extends GDSProgramQuery(programID) {
     var program: Option[SPProgram] = None
 
     def execute(p1: IDBDatabase, p2: ISPRemoteNode) {
-        val programID = p2.getProgramID
+        val currentProgramID = p2.getProgramID
 
-        if (programID != null && programID.stringValue().equals(programID)) {
-            program = Option(p2.asInstanceOf[SPProgram])
+        if (currentProgramID != null && currentProgramID.stringValue().equals(programID) && p2.isInstanceOf[ISPProgram]) {
+            val ispProgram = p2.asInstanceOf[ISPProgram]
+            val dataObject = Option(ispProgram.getDataObject)
+            dataObject match {
+                case Some(spProgram) if spProgram.isInstanceOf[SPProgram] => program = Option(ispProgram.getDataObject.asInstanceOf[SPProgram])
+                case Some(x) =>
+                case None =>
+            }
         }
     }
 }
 
 object GDSProgramQuery {
     def apply(programID: String): GDSProgramQuery = {
-        new GetProgramQuery(programID)
+        new GetProgramQuery6(programID)
     }
 }
