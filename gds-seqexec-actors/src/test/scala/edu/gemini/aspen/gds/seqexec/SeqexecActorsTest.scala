69f6fdc3827bf7467e9ba5f791a4f6b00050d55a
nbarriga <nbarriga@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-05-18 12:49:02 +0000

    Fixed broken test.
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@34885 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- gds-seqexec-actors/src/test/scala/edu/gemini/aspen/gds/seqexec/SeqexecActorsTest.scala
+++ gds-seqexec-actors/src/test/scala/edu/gemini/aspen/gds/seqexec/SeqexecActorsTest.scala
@@ -2,30 +2,30 @@ package edu.gemini.aspen.gds.seqexec
 
 
 import org.scalatest.junit.AssertionsForJUnit
-import org.junit.{Test}
+import org.junit.Test
 import edu.gemini.aspen.gds.api.Conversions._
-import collection.JavaConversions._
-import edu.gemini.aspen.gds.staticheaderreceiver.{StoreKeyword, TemporarySeqexecKeywordsDatabaseImpl}
+import edu.gemini.aspen.gds.staticheaderreceiver.TemporarySeqexecKeywordsDatabaseImpl
 import edu.gemini.aspen.gds.api._
 import edu.gemini.aspen.giapi.data.FitsKeyword
+import edu.gemini.aspen.gds.staticheaderreceiver.TemporarySeqexecKeywordsDatabaseImpl.Store
 
 class SeqexecActorsTest extends AssertionsForJUnit {
 
   @Test
   def testActor() {
     val db = new TemporarySeqexecKeywordsDatabaseImpl
-    db ! StoreKeyword("label","key",1.asInstanceOf[AnyRef])
+    db ! Store("label", "key", 1.asInstanceOf[AnyRef])
 
-    val seqActor = new SeqexecActor(db,"label", new GDSConfiguration("GPI","OBS_START_EVENT","KEY",0,"INT",true,"null","SEQEXEC","KEY","0","my comment"))
+    val seqActor = new SeqexecActor(db, "label", new GDSConfiguration("GPI", "OBS_START_EVENT", "KEY", 0, "INT", true, "null", "SEQEXEC", "KEY", "0", "my comment"))
 
 
-    assert(seqActor.collectValues == List(CollectedValue("KEY",1.asInstanceOf[AnyRef],"my comment",0)))
+    assert(seqActor.collectValues == List(CollectedValue("KEY", 1.asInstanceOf[AnyRef], "my comment", 0)))
   }
 
   @Test
-  def testActorFactory(){
+  def testActorFactory() {
     val db = new TemporarySeqexecKeywordsDatabaseImpl
-    db ! StoreKeyword("label","TEST",1.asInstanceOf[AnyRef])
+    db ! Store("label", "TEST", 1.asInstanceOf[AnyRef])
     val factory = new SeqexecActorsFactory(db)
     factory.configure(List(GDSConfiguration(Instrument("GPI"), GDSEvent("OBS_PREP"), new FitsKeyword("TEST"), HeaderIndex(0), DataType("DOUBLE"), Mandatory(false), NullValue("NONE"), Subsystem("SEQEXEC"), Channel("ws:massAirmass"), ArrayIndex("NULL"), FitsComment("my comment"))))
     assert(factory.buildStartAcquisitionActors("label").isEmpty)
@@ -34,12 +34,12 @@ class SeqexecActorsTest extends AssertionsForJUnit {
     assert(actors.length == 1)
     val values = actors.head.collectValues()
     values.head match {
-      case CollectedValue(fits,value,comment,0) => {
+      case CollectedValue(fits, value, comment, 0) => {
         assert(fits == stringToFitsKeyword("TEST"))
         assert(value == 1)
         assert(comment == "my comment")
       }
-      case x:AnyRef => fail("Wrong answer")
+      case x: AnyRef => fail("Wrong answer")
     }
   }
 }
