c2f35526fb84428b9c4865bb66fe5e3a75fb4692
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-05-19 15:02:38 +0000

    Initial skeleton of GDS Integration Test
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@35003 ee6ba543-07f8-4773-b02c-01d50e9d00ba

---
+++ integration-tests/src/test/java/edu/gemini/aspen/integrationtests/GDSEndToEndIT.java
@@ -0,0 +1,65 @@
+package edu.gemini.aspen.integrationtests;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Inject;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.Configuration;
+import org.ops4j.pax.exam.junit.JUnit4TestRunner;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+
+import java.util.logging.Logger;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+import static org.ops4j.pax.exam.CoreOptions.felix;
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+import static org.ops4j.pax.exam.CoreOptions.options;
+import static org.ops4j.pax.exam.CoreOptions.systemProperty;
+import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.cleanCaches;
+
+@RunWith(JUnit4TestRunner.class)
+public class GDSEndToEndIT {
+    private final Logger LOG = Logger.getLogger(HeartbeatDistributorIT.class.getName());
+    @Inject
+    private BundleContext context;
+
+    @Configuration
+    public static Option[] withJMSProviderConfig() {
+        return options(
+                felix(),
+                cleanCaches(),
+                systemProperty("felix.fileinstall.dir").value(System.getProperty("basedir") + "/src/test/resources/conf/services"),
+                systemProperty("felix.fileinstall.noInitialDelay").value("true"),
+                mavenBundle().artifactId("org.apache.felix.ipojo").groupId("org.apache.felix").versionAsInProject(),
+                mavenBundle().artifactId("com.springsource.javax.jms").groupId("javax.jms").versionAsInProject(),
+                mavenBundle().artifactId("giapi").groupId("edu.gemini.aspen").versionAsInProject(),
+                mavenBundle().artifactId("org.apache.felix.configadmin").groupId("org.apache.felix").versionAsInProject(),
+                mavenBundle().artifactId("org.apache.felix.fileinstall").groupId("org.apache.felix").versionAsInProject(),
+                mavenBundle().artifactId("pax-logging-api").groupId("org.ops4j.pax.logging").versionAsInProject(),
+                mavenBundle().artifactId("pax-logging-service").groupId("org.ops4j.pax.logging").versionAsInProject(),
+                mavenBundle().artifactId("guava").groupId("com.google.guava").versionAsInProject(),
+                mavenBundle().artifactId("file-util").groupId("gemini-nocs").versionAsInProject(),
+                mavenBundle().artifactId("fits-util").groupId("gemini-nocs").versionAsInProject(),
+                mavenBundle().artifactId("scala-library").groupId("com.weiglewilczek.scala-lang-osgi").versionAsInProject(),
+                mavenBundle().artifactId("gds-api").groupId("edu.gemini.aspen.gds").versionAsInProject()
+        );
+    }
+
+    @Test
+    public void bundleExistence() {
+        assertNotNull(getBundle("edu.gemini.aspen.gds.api"));
+    }
+
+    private Bundle getBundle(String symbolicName) {
+        for (Bundle b : context.getBundles()) {
+            if (symbolicName.equals(b.getSymbolicName())) {
+                return b;
+            }
+        }
+        fail();
+        return null;
+    }
+
+}
