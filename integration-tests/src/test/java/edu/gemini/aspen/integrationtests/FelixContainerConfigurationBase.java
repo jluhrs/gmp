d064b0b4ed7aa6ebb0a1251d1c45a069d26f7ccf
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-05-19 16:49:13 +0000

    Moved common functions to base abstract class
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@35007 ee6ba543-07f8-4773-b02c-01d50e9d00ba

---
+++ integration-tests/src/test/java/edu/gemini/aspen/integrationtests/FelixContainerConfigurationBase.java
@@ -0,0 +1,46 @@
+package edu.gemini.aspen.integrationtests;
+
+import org.ops4j.pax.exam.Inject;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.Configuration;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+
+import static org.junit.Assert.fail;
+import static org.ops4j.pax.exam.CoreOptions.felix;
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+import static org.ops4j.pax.exam.CoreOptions.options;
+import static org.ops4j.pax.exam.CoreOptions.systemProperty;
+import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.cleanCaches;
+
+abstract public class FelixContainerConfigurationBase {
+    @Inject
+    private BundleContext context;
+
+    @Configuration
+    public static Option[] baseContainerConfig() {
+        return options(
+                felix(),
+                cleanCaches(),
+                systemProperty("felix.fileinstall.dir").value(System.getProperty("basedir") + "/src/test/resources/conf/services"),
+                systemProperty("felix.fileinstall.noInitialDelay").value("true"),
+                mavenBundle().artifactId("org.apache.felix.ipojo").groupId("org.apache.felix").versionAsInProject(),
+                mavenBundle().artifactId("com.springsource.javax.jms").groupId("javax.jms").versionAsInProject(),
+                mavenBundle().artifactId("giapi").groupId("edu.gemini.aspen").versionAsInProject(),
+                mavenBundle().artifactId("org.apache.felix.configadmin").groupId("org.apache.felix").versionAsInProject(),
+                mavenBundle().artifactId("org.apache.felix.fileinstall").groupId("org.apache.felix").versionAsInProject(),
+                mavenBundle().artifactId("pax-logging-api").groupId("org.ops4j.pax.logging").versionAsInProject(),
+                mavenBundle().artifactId("pax-logging-service").groupId("org.ops4j.pax.logging").versionAsInProject(),
+                mavenBundle().artifactId("guava").groupId("com.google.guava").versionAsInProject());
+    }
+
+    protected Bundle getBundle(String symbolicName) {
+        for (Bundle b : context.getBundles()) {
+            if (symbolicName.equals(b.getSymbolicName())) {
+                return b;
+            }
+        }
+        fail();
+        return null;
+    }
+}
