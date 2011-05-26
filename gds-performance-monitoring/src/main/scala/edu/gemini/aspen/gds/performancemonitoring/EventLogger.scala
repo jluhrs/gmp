d748a2e3cf6f257b1799b741560c45708431bcd3
nbarriga <nbarriga@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-05-26 16:32:27 +0000

    Improved EventLogger and its tests.
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@35153 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- gds-performance-monitoring/src/main/scala/edu/gemini/aspen/gds/performancemonitoring/EventLogger.scala
+++ gds-performance-monitoring/src/main/scala/edu/gemini/aspen/gds/performancemonitoring/EventLogger.scala
@@ -1,38 +1,40 @@
 package edu.gemini.aspen.gds.performancemonitoring
 
 import org.scala_tools.time.Imports._
-import scala.{Some, Option}
 import org.apache.felix.ipojo.annotations._
 import java.util.logging.Logger
 import actors.Actor
+import scala.{Some, Option}
 
 sealed abstract class EventLoggerMsg
 
-case class AddEventSet(set: Any) extends EventLoggerMsg
+case class AddEventSet(set: AnyRef) extends EventLoggerMsg
 
-case class Start(set: Any, evt: Any) extends EventLoggerMsg
+case class Start(set: AnyRef, evt: AnyRef) extends EventLoggerMsg
 
-case class End(set: Any, evt: Any) extends EventLoggerMsg
+case class End(set: AnyRef, evt: AnyRef) extends EventLoggerMsg
 
-case class Dump(set: Any) extends EventLoggerMsg
+case class Dump(set: AnyRef) extends EventLoggerMsg
 
 case class DumpAll() extends EventLoggerMsg
 
-case class Retrieve(set: Any) extends EventLoggerMsg
+case class Retrieve(set: AnyRef) extends EventLoggerMsg
 
 case class RetrieveAll() extends EventLoggerMsg
 
-trait EventLogger extends Actor {
-  def addEventSet(set: Any) {
-    this ! AddEventSet(set)
-  }
-}
+case class RetrieveEventAverage(evt: AnyRef) extends EventLoggerMsg
+
+case class DumpEventAverage(evt: AnyRef) extends EventLoggerMsg
+
+trait EventLogger extends Actor
 
 @Component
 @Instantiate
 @Provides(specifications = Array(classOf[EventLogger]))
 class EventLoggerImpl extends EventLogger {
   private val LOG = Logger.getLogger(this.getClass.getName)
+  //map: eventSet -> (event -> (startTime, endTime))
+  private val map = collection.mutable.Map.empty[AnyRef, collection.mutable.Map[AnyRef, (Option[DateTime], Option[DateTime])]]
 
   @Validate
   def validate() {
@@ -47,38 +49,74 @@ class EventLoggerImpl extends EventLogger {
   override def act() {
     loop {
       react {
-        case AddEventSet(set) => map += set -> collection.mutable.Map.empty[Any, (Option[DateTime], Option[DateTime])]
-        case Start(set, evt) => map.getOrElseUpdate(set, collection.mutable.Map.empty[Any, (Option[DateTime], Option[DateTime])]) += evt -> (Some(DateTime.now), map(set).getOrElse(evt, (None, None))._2)
-        case End(set, evt) => map.getOrElseUpdate(set, collection.mutable.Map.empty[Any, (Option[DateTime], Option[DateTime])]) += evt -> (map(set).getOrElse(evt, (None, None))._1, Some(DateTime.now))
-        case Dump(set) => LOG.info("Timing stats for " + set + ": " +
-          map.getOrElse(set, collection.mutable.Map.empty[Any, (Option[DateTime], Option[DateTime])])
-            .mapValues({
-            case (Some(start), Some(end)) => Some((start to end).toDuration)
-            case _ => None
-          }))
-        case DumpAll() => LOG.info("Timing stats: " + map.mapValues({
-          case m => m.mapValues({
-            case (Some(start), Some(end)) => Some((start to end).toDuration)
-            case _ => None
-          })
-        }))
-        case Retrieve(set) => reply(map.getOrElse(set, collection.mutable.Map.empty[Any, (Option[DateTime], Option[DateTime])])
-          .mapValues({
-          case (Some(start), Some(end)) => Some((start to end).toDuration)
-          case _ => None
-        }))
-        case RetrieveAll() => reply(map.mapValues({
-          case m => m.mapValues({
-            case (Some(start), Some(end)) => Some((start to end).toDuration)
-            case _ => None
-          })
-        }))
+        case AddEventSet(set) => _addEventSet(set)
+        case Start(set, evt) => _start(set, evt)
+        case End(set, evt) => _end(set, evt)
+        case Dump(set) => LOG.info("Timing stats for " + set + ": " + _retrieve(set))
+        case DumpAll() => LOG.info("Timing stats: " + _retrieveAll())
+        case Retrieve(set) => reply(_retrieve(set))
+        case RetrieveAll() => reply(_retrieveAll())
+        case RetrieveEventAverage(evt) => reply(_average(evt))
+        case DumpEventAverage(evt) => LOG.info("Timing average for " + evt + ": " + _average(evt))
       }
     }
   }
 
-  //map: eventset -> (event -> (startTime, endTime))
-  val map = collection.mutable.Map.empty[Any, collection.mutable.Map[Any, (Option[DateTime], Option[DateTime])]]
+  private def _addEventSet(set: AnyRef) {
+    map += set -> collection.mutable.Map.empty[AnyRef, (Option[DateTime], Option[DateTime])]
+  }
 
+  private def _start(set: AnyRef, evt: AnyRef) {
+    map.getOrElseUpdate(set, collection.mutable.Map.empty[AnyRef, (Option[DateTime], Option[DateTime])]) += evt -> (Some(DateTime.now), map(set).getOrElse(evt, (None, None))._2)
+  }
 
+  private def _end(set: AnyRef, evt: AnyRef) {
+    map.getOrElseUpdate(set, collection.mutable.Map.empty[AnyRef, (Option[DateTime], Option[DateTime])]) += evt -> (map(set).getOrElse(evt, (None, None))._1, Some(DateTime.now))
+  }
+
+  private def _retrieve(set: AnyRef) = {
+    map.getOrElse(set, collection.mutable.Map.empty[AnyRef, (Option[DateTime], Option[DateTime])])
+      .mapValues({
+      case (Some(start), Some(end)) => Some((start to end).toDuration)
+      case _ => None
+    })
+  }
+
+  private def _average(evt: AnyRef): Duration = {
+    val values = for {
+      (_, innerMap) <- map
+      (_evt, times) <- innerMap
+      if _evt == evt
+    } yield times
+
+    val durations = values.collect({
+      case (Some(start), Some(end)) => (start to end).toDuration
+    })
+
+    case class Average(sum: Duration, count: Int) {
+      def +(other: Duration): Average = {
+        Average(sum + other, count + 1)
+      }
+
+      def average(): Duration = {
+        count match {
+          case 0 => 0.millis
+          case x => (sum.millis / x).toInt.millis
+        }
+      }
+    }
+    val avg = new Average(Duration.standardSeconds(0), 0)
+    durations.foldLeft(avg) {
+      (currentAvg, currentVal) => currentAvg + currentVal
+    }.average()
+  }
+
+  private def _retrieveAll() = {
+    map.mapValues({
+      case m => m.mapValues({
+        case (Some(start), Some(end)) => Some((start to end).toDuration)
+        case _ => None
+      })
+    })
+  }
 }
--- gds-performance-monitoring/src/test/scala/edu/gemini/aspen/gds/performancemonitoring/EventLoggerTest.scala
+++ gds-performance-monitoring/src/test/scala/edu/gemini/aspen/gds/performancemonitoring/EventLoggerTest.scala
@@ -2,31 +2,56 @@ package edu.gemini.aspen.gds.performancemonitoring
 
 import org.scalatest.junit.AssertionsForJUnit
 import org.junit.Test
-import scala.Some
 import org.scala_tools.time.Imports._
+import scala.Some
 
 class EventLoggerTest extends AssertionsForJUnit {
 
+  private def check(value: Duration, target: Duration, margin: Duration) = {
+    ((value > target) && (value - target) < margin) ||
+      ((value < target) && (target - value) < margin)
+  }
+
   @Test
   def testBasic() {
+    val delay = 500;
     val el = new EventLoggerImpl
     el.validate()
 
     el ! AddEventSet("set")
     el ! Start("set", "hola")
-    Thread.sleep(500)
+    Thread.sleep(delay)
     el ! End("set", "hola")
+
+    el ! AddEventSet("otroset")
+    el ! Start("otroset", "hola")
+    Thread.sleep(delay)
+    el ! End("otroset", "hola")
+
+    el ! AddEventSet("otrosetmas")
+    el ! Start("otrosetmas", "hola")
+    Thread.sleep(delay)
+    el ! End("otrosetmas", "hola")
+
     el ! Start("set", "chao")
     el ! End("set", "Oops")
-    el.addEventSet("otro set")
 
     (el !? (1000, Retrieve("set"))) match {
       case Some(y) => {
-        val x = y.asInstanceOf[scala.collection.Map[Any, Option[Duration]]]
-        assert((x("hola").get - 250.millis) > 0.millis && ((x("hola").get - 750.millis) < 0.millis), "Time is: " + x("hola").get) //+- 50 ms
+        val x = y.asInstanceOf[scala.collection.Map[AnyRef, Option[Duration]]]
+        assert(check(x("hola").get, 500.millis, 200.millis), "Time is: " + x("hola").get) //+- 200 ms
         assert(x("chao").isEmpty) //doesn't end
         assert(x("Oops").isEmpty) //doesn't start
       }
+      case _ => fail()
+    }
+
+    (el !? (1000, RetrieveEventAverage("hola"))) match {
+      case Some(x: Duration) => assert(check(x, 500.millis, 100.millis), "Time is: " + x)
+      case _ => fail()
+    }
+    (el !? (1000, RetrieveEventAverage("chao"))) match {
+      case Some(x: Duration) => assert(x.compare(0.millis) == 0, "Time is: " + x)
       case _ => fail()
     }
   }
