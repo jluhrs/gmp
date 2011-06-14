7fb469031ed6f81efe727126241cc02760a07624
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-06-14 19:03:32 +0000

    Added test to verify that the default values are parsed
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@35575 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- gds-actors-composer/src/test/scala/edu/gemini/aspen/gds/actors/configuration/GDSConfigurationParserSpec.scala
+++ gds-actors-composer/src/test/scala/edu/gemini/aspen/gds/actors/configuration/GDSConfigurationParserSpec.scala
@@ -7,26 +7,25 @@ import org.scalatest.matchers.ShouldMatchers
 import edu.gemini.aspen.giapi.data.FitsKeyword
 import edu.gemini.aspen.gds.api._
 
+/**
+ * Specify how the GDSConfiguration parser should behave
+ */
 @RunWith(classOf[JUnitRunner])
 class GDSConfigurationParserSpec extends Spec with ShouldMatchers {
+    val text = """GPI OBS_END_ACQ AIRMASS 0 DOUBLE F NONE EPICS ws:massAirmass NULL "Mean airmass for the observation""""
+
     describe("A GDSConfigurationParser") {
         it("should parse configuration line") {
             val text = """      GPI          OBS_END_ACQ      AIRMASS       0    DOUBLE          F            NONE        EPICS       ws:massAirmass	              NULL     "Mean airmass for the observation""""
-            val result = new GDSConfigurationParser().parseText(text)
-
-            result.successful should be (true)
-
-            result.get should have length(1)
-            result.get(0) should equal (GDSConfiguration(Instrument("GPI"), GDSEvent("OBS_END_ACQ"), new FitsKeyword("AIRMASS"), HeaderIndex(0), DataType("DOUBLE"), Mandatory(false), NullValue("NONE"), Subsystem("EPICS"), Channel("ws:massAirmass"), ArrayIndex("NULL"), FitsComment("Mean airmass for the observation")))
+            verifyParsedLine(text)
         }
         it("should parse configuration line with minimal spaces") {
-            val text = """GPI OBS_END_ACQ AIRMASS 0 DOUBLE F NONE EPICS ws:massAirmass NULL "Mean airmass for the observation""""
+            verifyParsedLine(text)
+        }
+        it("should read the default value") {
             val result = new GDSConfigurationParser().parseText(text)
 
-            result.successful should be (true)
-
-            result.get should have length(1)
-            result.get(0) should equal (GDSConfiguration(Instrument("GPI"), GDSEvent("OBS_END_ACQ"), new FitsKeyword("AIRMASS"), HeaderIndex(0), DataType("DOUBLE"), Mandatory(false), NullValue("NONE"), Subsystem("EPICS"), Channel("ws:massAirmass"), ArrayIndex("NULL"), FitsComment("Mean airmass for the observation")))
+            result.get(0).asInstanceOf[GDSConfiguration].nullValue should equal (NullValue("NONE"))
         }
         it("should parse comment lines") {
             val text = "#comment"
@@ -37,15 +36,14 @@ class GDSConfigurationParserSpec extends Spec with ShouldMatchers {
             result.get should have length(1)
             result.get(0) should equal (Comment("#comment"))
         }
-//        it("should parse blank lines") {
-//            val text = ""
-//            val parser = new GDSConfigurationParser()
-//            val result = parser.parseText(text)
-//
-//            result.successful should be (true)
-//
-//            result.get should have length(0)
-//            result.get(0) should equal (Space(0))
-//        }
     }
+
+    def verifyParsedLine(text: String): Any = {
+        val result = new GDSConfigurationParser().parseText(text)
+        result.successful should be(true)
+
+        result.get should have length (1)
+        result.get(0) should equal(GDSConfiguration(Instrument("GPI"), GDSEvent("OBS_END_ACQ"), new FitsKeyword("AIRMASS"), HeaderIndex(0), DataType("DOUBLE"), Mandatory(false), NullValue("NONE"), Subsystem("EPICS"), Channel("ws:massAirmass"), ArrayIndex("NULL"), FitsComment("Mean airmass for the observation")))
+    }
+
 }
