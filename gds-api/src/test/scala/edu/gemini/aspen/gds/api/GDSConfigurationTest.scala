79db22fe92aecb93b15ef8de30904284de851584
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-06-15 12:46:28 +0000

    Simplified test using implicit conversions
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@35606 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- gds-api/src/test/scala/edu/gemini/aspen/gds/api/GDSConfigurationTest.scala
+++ gds-api/src/test/scala/edu/gemini/aspen/gds/api/GDSConfigurationTest.scala
@@ -11,9 +11,9 @@ class GDSConfigurationTest extends AssertionsForJUnit {
 
     @Test
     def testIsMandatory() {
-        val mandatoryConfig = GDSConfiguration(Instrument("GPI"), GDSEvent("OBS_START_ACQ"), new FitsKeyword("AIRMASS"), HeaderIndex(0), DataType("DOUBLE"), Mandatory(true), "NONE", Subsystem("EPICS"), Channel("a"), ArrayIndex("NULL"), FitsComment("Mean airmass for the observation"))
+        val mandatoryConfig = GDSConfiguration("GPI", "OBS_START_ACQ", "AIRMASS", 0, "DOUBLE", true, "NONE", "EPICS", "gpi:value", "NULL", "Mean airmass for the observation")
         assertTrue(mandatoryConfig.isMandatory)
-        val nonMandatoryConfig = GDSConfiguration(Instrument("GPI"), GDSEvent("OBS_START_ACQ"), new FitsKeyword("AIRMASS"), HeaderIndex(0), DataType("DOUBLE"), Mandatory(false), "NONE", Subsystem("EPICS"), Channel("a"), ArrayIndex("NULL"), FitsComment("Mean airmass for the observation"))
+        val nonMandatoryConfig = GDSConfiguration("GPI", "OBS_START_ACQ", "AIRMASS", 0, "DOUBLE", false, "NONE", "EPICS", "gpiSvalue", "NULL", "Mean airmass for the observation")
         assertFalse(nonMandatoryConfig.isMandatory)
     }
 
