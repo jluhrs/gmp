96dc2cde93cb17a4f69b7f676e739b86c4aef83c
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-06-20 16:26:13 +0000

    Converted test to junit
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@35693 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- gds-odb-actors/src/test/scala/edu/gemini/aspen/gds/odb/ODBValuesActorSpec.scala
+++ gds-odb-actors/src/test/scala/edu/gemini/aspen/gds/odb/ODBValuesActorSpec.scala
@@ -3,7 +3,7 @@ package edu.gemini.aspen.gds.odb
 import org.scalatest.junit.JUnitRunner
 import org.junit.runner.RunWith
 import org.junit.Assert._
-import org.scalatest.matchers.ShouldMatchers
+import org.junit.Test
 import org.scalatest.Spec
 import edu.gemini.aspen.giapi.data.{FitsKeyword, DataLabel}
 import org.specs2.mock.Mockito
@@ -13,8 +13,7 @@ import edu.gemini.pot.spdb.IDBDatabaseService
 import edu.gemini.pot.sp.{ISPProgram, SPProgramID}
 import edu.gemini.aspen.gds.api._
 
-@RunWith(classOf[JUnitRunner])
-class ODBValuesActorSpec extends Spec with ShouldMatchers with Mockito {
+class ODBValuesActorSpec extends Spec with Mockito {
     val dataLabel = new DataLabel("GS-2011")
     val programIDLabel = "programID"
     val programID = SPProgramID.toProgramID("programID")
@@ -43,134 +42,142 @@ class ODBValuesActorSpec extends Spec with ShouldMatchers with Mockito {
         result().asInstanceOf[List[CollectedValue[_]]]
     }
 
-    describe("An ODBValuesActor") {
-        it("should reply to Collect messages") {
-            val configuration = buildConfigurationItem(firstNameFitsKeyword, firstNameChannel, "PI First Name", true)
+    @Test
+    def testNormalCollection() {
+        val configuration = buildConfigurationItem(firstNameFitsKeyword, firstNameChannel, "PI First Name", true)
 
-            val result = buildActorAndCollect(configuration)
+        val result = buildActorAndCollect(configuration)
 
-            result match {
-                case CollectedValue(keyword, value, comment, 0) :: Nil => {
-                            assertEquals(firstNameFitsKeyword, keyword)
-                            assertEquals(firstName, value)
-                            assertEquals("PI First Name", comment)
-                        }
-                case _ => fail("Should not reply other message ")
+        result match {
+            case CollectedValue(keyword, value, comment, 0) :: Nil => {
+                assertEquals(firstNameFitsKeyword, keyword)
+                assertEquals(firstName, value)
+                assertEquals("PI First Name", comment)
             }
-
-            // verify mock
-            there was databaseService.lookupProgramByID(programID)
+            case _ => fail("Should not reply other message ")
         }
-        it("should ignore unknown channels") {
-            val configuration = buildConfigurationItem(firstNameFitsKeyword, "odb:achannel", "PI Last Name", true)
 
-            val result = buildActorAndCollect(configuration)
+        // verify mock
+        there was databaseService.lookupProgramByID(programID)
+    }
 
-            result match {
-                case ErrorCollectedValue(keyword, error, comment, 0) :: Nil =>  {
-                    assertEquals(firstNameFitsKeyword, keyword)
-                    assertEquals(CollectionError.MandatoryRequired, error)
-                    assertEquals("PI Last Name", comment)
-                }
-                case _ => fail("Should not reply other message ")
+    @Test
+    def testFlagUnknownChannels() {
+        val configuration = buildConfigurationItem(firstNameFitsKeyword, "odb:achannel", "PI Last Name", true)
+
+        val result = buildActorAndCollect(configuration)
+
+        result match {
+            case ErrorCollectedValue(keyword, error, comment, 0) :: Nil => {
+                assertEquals(firstNameFitsKeyword, keyword)
+                assertEquals(CollectionError.MandatoryRequired, error)
+                assertEquals("PI Last Name", comment)
             }
-
-            // verify mock
-            there was databaseService.lookupProgramByID(programID)
+            case _ => fail("Should not reply other message ")
         }
-        it("should reply to Collect messages with multiple configurations") {
-            val lastNameFitsKeyword = new FitsKeyword("PILSTNAM")
 
-            val configuration1 = buildConfigurationItem(lastNameFitsKeyword, lastNameChannel, "PI Last Name", true)
-            val configuration2 = buildConfigurationItem(firstNameFitsKeyword, firstNameChannel, "PI First Name", true)
+        // verify mock
+        there was databaseService.lookupProgramByID(programID)
+    }
 
-            val result = buildActorAndCollect(configuration1 ::: configuration2)
+    @Test
+    def testMultipleItems() {
+        val lastNameFitsKeyword = new FitsKeyword("PILSTNAM")
 
-            result match {
-                case last :: first :: Nil => {
-                    last match {
-                        case CollectedValue(keyword, value, comment, 0) => {
-                            assertEquals(lastNameFitsKeyword, keyword)
-                            assertEquals(lastName, value)
-                            assertEquals("PI Last Name", comment)
-                        }
-                    }
-                    first match {
-                        case CollectedValue(keyword, value, comment, 0) => {
-                            assertEquals(firstNameFitsKeyword, keyword)
-                            assertEquals(firstName, value)
-                            assertEquals("PI First Name", comment)
-                        }
+        val configuration1 = buildConfigurationItem(lastNameFitsKeyword, lastNameChannel, "PI Last Name", true)
+        val configuration2 = buildConfigurationItem(firstNameFitsKeyword, firstNameChannel, "PI First Name", true)
+
+        val result = buildActorAndCollect(configuration1 ::: configuration2)
+
+        result match {
+            case last :: first :: Nil => {
+                last match {
+                    case CollectedValue(keyword, value, comment, 0) => {
+                        assertEquals(lastNameFitsKeyword, keyword)
+                        assertEquals(lastName, value)
+                        assertEquals("PI Last Name", comment)
                     }
                 }
-                case _ => fail("Should not reply other message ")
+                first match {
+                    case CollectedValue(keyword, value, comment, 0) => {
+                        assertEquals(firstNameFitsKeyword, keyword)
+                        assertEquals(firstName, value)
+                        assertEquals("PI First Name", comment)
+                    }
+                }
             }
+            case _ => fail("Should not reply other message ")
+        }
+
+        // verify mock
+        there was databaseService.lookupProgramByID(programID)
+    }
+
+    @Test
+    def testMandatoryItemNotFound() {
+        val piInfo = new SPProgram.PIInfo(null, null, null, null, null)
+        spProgram.setPIInfo(piInfo)
+
+        val configuration = buildConfigurationItem(firstNameFitsKeyword, firstNameChannel, "PI Last Name", true)
+
+        val result = buildActorAndCollect(configuration)
+
+        result match {
+            case ErrorCollectedValue(keyword, error, comment, 0) :: Nil => {
+                assertEquals(firstNameFitsKeyword, keyword)
+                assertEquals(CollectionError.MandatoryRequired, error)
+                assertEquals("PI Last Name", comment)
+            }
+            case _ => fail("Should not reply other message ")
+        }
+        // verify mock
+        there was databaseService.lookupProgramByID(programID)
+    }
+
+    @Test
+    def testNonMandatoryItemNotFound() {
+        val piInfo = new SPProgram.PIInfo(null, null, null, null, null)
+        spProgram.setPIInfo(piInfo)
+        val configuration = buildConfigurationItem(firstNameFitsKeyword, firstNameChannel, "PI Last Name", false)
+
+        val result = buildActorAndCollect(configuration)
+
+        result match {
+            case DefaultCollectedValue(keyword, value, comment, 0) :: Nil => {
+                assertEquals(firstNameFitsKeyword, keyword)
+                assertEquals("NOT FOUND", value)
+                assertEquals("PI Last Name", comment)
+            }
+            case _ => fail("Should not reply other message ")
+
 
             // verify mock
             there was databaseService.lookupProgramByID(programID)
         }
-        it("should produce an error if mandatory and the result is null") {
-            val piInfo = new SPProgram.PIInfo(null, null, null, null, null)
-            spProgram.setPIInfo(piInfo)
+    }
 
-            val configuration = buildConfigurationItem(firstNameFitsKeyword, firstNameChannel, "PI Last Name", true)
+    @Test
+    def testProgramNotFound() {
+        databaseService.lookupProgramByID(programID) returns null
+        val configuration = buildConfigurationItem(firstNameFitsKeyword, firstNameChannel, "PI First Name", true)
 
-            val result = buildActorAndCollect(configuration)
+        val result = buildActorAndCollect(configuration)
 
-            result match {
-                case ErrorCollectedValue(keyword, error, comment, 0) :: Nil =>  {
-                    assertEquals(firstNameFitsKeyword, keyword)
-                    assertEquals(CollectionError.MandatoryRequired, error)
-                    assertEquals("PI Last Name", comment)
-                }
-                case _ => fail("Should not reply other message ")
+        result match {
+            case ErrorCollectedValue(keyword, error, comment, 0) :: Nil => {
+                assertEquals(firstNameFitsKeyword, keyword)
+                assertEquals(CollectionError.MandatoryRequired, error)
+                assertEquals("PI First Name", comment)
             }
-            // verify mock
-            there was databaseService.lookupProgramByID(programID)
+            case _ => fail("Should not reply other message ")
         }
-        it("should give a default value if not mandatory and the result is null") {
-            val piInfo = new SPProgram.PIInfo(null, null, null, null, null)
-            spProgram.setPIInfo(piInfo)
-            val configuration = buildConfigurationItem(firstNameFitsKeyword, firstNameChannel, "PI Last Name", false)
 
-            val result = buildActorAndCollect(configuration)
-
-            result match {
-                case DefaultCollectedValue(keyword, value, comment, 0) :: Nil =>  {
-                    assertEquals(firstNameFitsKeyword, keyword)
-                    assertEquals("NOT FOUND", value)
-                    assertEquals("PI Last Name", comment)
-                }
-                case _ => fail("Should not reply other message ")
-
-
-                // verify mock
-                there was databaseService.lookupProgramByID(programID)
-            }
-        }
-        it("should be empty if there is no program") {
-            databaseService.lookupProgramByID(programID) returns null
-            val configuration = buildConfigurationItem(firstNameFitsKeyword, firstNameChannel, "PI First Name", true)
-
-            val result = buildActorAndCollect(configuration)
-
-            result match {
-                case ErrorCollectedValue(keyword, error, comment, 0) :: Nil =>  {
-                    assertEquals(firstNameFitsKeyword, keyword)
-                    assertEquals(CollectionError.MandatoryRequired, error)
-                    assertEquals("PI First Name", comment)
-                }
-                case _ => fail("Should not reply other message ")
-            }
-
-            // verify mock
-            there was databaseService.lookupProgramByID(programID)
-        }
+        // verify mock
+        there was databaseService.lookupProgramByID(programID)
     }
 
     def buildConfigurationItem(fitsKeyword: FitsKeyword, channelName: String, comment: String, mandatory: Boolean) = {
         List(GDSConfiguration("GPI", "OBS_START_ACQ", fitsKeyword, 0, "DOUBLE", mandatory, "NOT FOUND", "ODB", channelName, 0, comment))
     }
-
 
 }
