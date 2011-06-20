fbfd27cb8c8983bbd6415d52183b402c6526c969
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-06-20 19:58:16 +0000

    Converted test to JUnit
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@35703 ee6ba543-07f8-4773-b02c-01d50e9d00ba

---
+++ gds-instrument-status-actors/src/test/scala/edu/gemin/aspen/gds/status/InstrumentStatusActorTest.scala
@@ -0,0 +1,113 @@
+package edu.gemin.aspen.gds.status
+
+import org.junit.Assert._
+import org.junit.Test
+import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.Spec
+import edu.gemini.aspen.giapi.data.{FitsKeyword, DataLabel}
+import org.specs2.mock.Mockito
+import edu.gemini.aspen.gds.api.Conversions._
+import edu.gemini.aspen.giapi.status.StatusDatabaseService
+import edu.gemini.aspen.giapi.status.impl.BasicStatus
+import org.junit.Assert._
+import edu.gemini.aspen.gds.api._
+
+class InstrumentStatusActorTest extends Mockito {
+    val defaultValue = "DEFAULT"
+    val fitsKeyword = new FitsKeyword("GPISTATUS")
+    val dataLabel = new DataLabel("GS-2011")
+    val statusDB = mock[StatusDatabaseService]
+    val statusItemName = "gpi:status1"
+
+    @Test
+    def testReplyToCollect {
+            val configuration = buildConfiguration(statusItemName, false)
+
+            val referenceValue = "ok"
+            // mock return value
+            val statusItem = new BasicStatus[String](statusItemName, referenceValue)
+            statusDB.getStatusItem(statusItemName) returns statusItem
+
+            val instrumentStatusActor = new InstrumentStatusActor(statusDB, configuration)
+
+            // Send a Collect message and wait the response
+            val result = instrumentStatusActor !! Collect
+
+
+            result() match {
+                case CollectedValue(keyword, value, comment, 0) :: Nil => {
+                    assertEquals(fitsKeyword, keyword)
+                    assertEquals(referenceValue, value)
+                    assertEquals("Current global status of the instrument", comment)
+                }
+                case _ => fail("Should not reply other message ")
+            }
+
+            // verify mock
+            there was one(statusDB).getStatusItem(statusItemName)
+        }
+
+    @Test
+    def testReplyToCollectIfStatusUnknown {
+            val configuration = buildConfiguration(statusItemName, false)
+
+            // mock return value
+            statusDB.getStatusItem(statusItemName) returns null
+
+            val instrumentStatusActor = new InstrumentStatusActor(statusDB, configuration)
+
+            // Send a Collect message and wait the response
+            val result = instrumentStatusActor !! Collect
+
+            result() match {
+                case CollectedValue(keyword, value, comment, 0) :: Nil  => {
+                    assertEquals(fitsKeyword, keyword)
+                    assertEquals(defaultValue, value)
+                    assertEquals("Current global status of the instrument", comment)
+                }
+                case _ => fail("Should not reply other message ")
+            }
+
+            // verify mock
+            there was one(statusDB).getStatusItem(statusItemName)
+        }
+    @Test
+    def testErrorMandatoryItemAndStatusUnknown {
+            val configuration = buildConfiguration(statusItemName, true)
+
+            // mock return value
+            statusDB.getStatusItem(statusItemName) returns null
+
+            val instrumentStatusActor = new InstrumentStatusActor(statusDB, configuration)
+
+            // Send a Collect message and wait the response
+            val result = instrumentStatusActor !! Collect
+
+            result() match {
+                case ErrorCollectedValue(keyword, error, comment, 0) :: Nil => {
+                    assertEquals(fitsKeyword, keyword)
+                    assertEquals(CollectionError.MandatoryRequired, error)
+                    assertEquals("Current global status of the instrument", comment)
+                }
+                case _ => fail("Should not reply other message ")
+            }
+
+            // verify mock
+            there was one(statusDB).getStatusItem(statusItemName)
+    }
+
+    def buildConfiguration(statusItem: String, mandatory: Boolean): GDSConfiguration = {
+        GDSConfiguration("GPI",
+            "OBS_START_ACQ",
+            fitsKeyword,
+            0,
+            "STRING",
+            mandatory,
+            defaultValue,
+            "STATUS",
+            statusItem,
+            0,
+            "Current global status of the instrument")
+    }
+}
+
