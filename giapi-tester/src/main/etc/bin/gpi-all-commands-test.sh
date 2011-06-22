ec6e15f547dfbdc0dec6f05d529e01f9eaf708d0
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-06-22 13:16:21 +0000

    Changed path to the right location of giapi-tester
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@35739 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- giapi-tester/src/main/etc/bin/gpi-all-commands-test.sh
+++ giapi-tester/src/main/etc/bin/gpi-all-commands-test.sh
@@ -3,7 +3,7 @@
 DEFAULT_TIMEOUT=50000
 
 function giapi-tester {
-   java -jar target/giapi-tester-0.1.0-jar-with-dependencies.jar $@
+   java -jar giapi-tester-0.1.0-jar-with-dependencies.jar $@
    RETVAL=$?
    [ $RETVAL -eq 0 ] && echo "Command $2 OK"   
    [ $RETVAL -ne 0 ] && echo "Command $2 ERROR" && exit 1 
