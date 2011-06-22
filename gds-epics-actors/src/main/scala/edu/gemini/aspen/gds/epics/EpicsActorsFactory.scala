c1c17f12abfa75d1b9568b6520063d0eb5a9c159
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-06-22 20:25:33 +0000

    Added calls to unbind in the right place
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@35749 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- gds-epics-actors/src/main/scala/edu/gemini/aspen/gds/epics/EpicsActorsFactory.scala
+++ gds-epics-actors/src/main/scala/edu/gemini/aspen/gds/epics/EpicsActorsFactory.scala
@@ -10,30 +10,31 @@ import edu.gemini.aspen.giapi.data.{ObservationEvent, DataLabel}
 @Instantiate
 @Provides(specifications = Array(classOf[KeywordActorsFactory]))
 class EpicsActorsFactory(@Requires epicsReader: EpicsReader) extends KeywordActorsFactory {
-  // Use dummy configuration
-  var conf: List[GDSConfiguration] = List()
+    // Use dummy configuration
+    var conf: List[GDSConfiguration] = List()
 
-  override def buildActors(obsEvent: ObservationEvent, dataLabel: DataLabel) = {
-    conf filter {
-      _.event.name == obsEvent.name
-    } map {
-      case config: GDSConfiguration => new EpicsValuesActor(epicsReader, config)
+    override def buildActors(obsEvent: ObservationEvent, dataLabel: DataLabel) = {
+        conf filter {
+            _.event.name == obsEvent.name
+        } map {
+            c => {
+                epicsReader.bindChannel(c.channel.name)
+                new EpicsValuesActor(epicsReader, c)
+            }
+        }
     }
-  }
 
-  override def configure(configuration: List[GDSConfiguration]) {
-    conf = configuration filter {
-      _.subsystem.name == "EPICS"
+    override def configure(configuration: List[GDSConfiguration]) {
+        conf = configuration filter {
+            _.subsystem.name == "EPICS"
+        }
     }
-    // Bind all required channels
-    conf map {
-      x =>
-        epicsReader.bindChannel(x.channel.name)
-    }
-  }
 
-  @Invalidate
-  def unbindAllChannels() {
-    // TODO remove all the bound channels
-  }
+    @Invalidate
+    def unbindAllChannels() {
+        // Unbind all required channels
+        conf map {
+            c => epicsReader.unbindChannel(c.channel.name)
+        }
+    }
 }
