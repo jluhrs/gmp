3c62323c79b406617edb86d85b6005ffc4bbc40b
nbarriga <nbarriga@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-06-24 13:38:40 +0000

    Added comments
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@35777 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- gds-obsevent-handler/src/main/scala/edu/gemini/aspen/gds/obsevent/handler/ObsEventBookKeeping.scala
+++ gds-obsevent-handler/src/main/scala/edu/gemini/aspen/gds/obsevent/handler/ObsEventBookKeeping.scala
@@ -3,36 +3,60 @@ package edu.gemini.aspen.gds.obsevent.handler
 import java.util.EnumSet
 import edu.gemini.aspen.giapi.data.{DataLabel, ObservationEvent}
 
+/**
+ * Keep track of which observation events have arrived and which data collection are done
+ */
 class ObsEventBookKeeping {
     private val obsEventsMap = collection.mutable.Map.empty[DataLabel, EnumSet[ObservationEvent]]
     private val repliesMap = collection.mutable.Map.empty[DataLabel, EnumSet[ObservationEvent]]
 
+    /**
+     * Ask if all previous observation events have arrived
+     */
     def previousArrived(obsEvent: ObservationEvent, dataLabel: DataLabel) = {
         EnumSet.complementOf(EnumSet.range(obsEvent, ObservationEvent.values().last)).equals(obsEventsMap.getOrElse(dataLabel, EnumSet.noneOf(classOf[ObservationEvent])))
     }
 
+    /**
+     * Ask if a given observation event has arrived
+     */
     def obsArrived(obsEvent: ObservationEvent, dataLabel: DataLabel) = {
         obsEventsMap.getOrElse(dataLabel, EnumSet.noneOf(classOf[ObservationEvent])).contains(obsEvent)
     }
 
+    /**
+     * Ask if a reply for a given obs event has arrived
+     */
     def replyArrived(obsEvent: ObservationEvent, dataLabel: DataLabel) = {
         repliesMap.getOrElse(dataLabel, EnumSet.noneOf(classOf[ObservationEvent])).contains(obsEvent)
     }
 
+    /**
+     * Ask if replies for data collection for all observation events have arrived
+     */
     def allRepliesArrived(dataLabel: DataLabel) = {
         repliesMap.getOrElse(dataLabel, EnumSet.noneOf(classOf[ObservationEvent])).containsAll(EnumSet.allOf(classOf[ObservationEvent]))
     }
 
+    /**
+     * Indicate that data collection for a given obs event is done
+     */
     def addReply(obsEvent: ObservationEvent, dataLabel: DataLabel) {
         val repliesSet = repliesMap.getOrElseUpdate(dataLabel, EnumSet.noneOf(classOf[ObservationEvent]))
         repliesSet.add(obsEvent)
     }
 
+    /**
+     * Indicate that an obs event has arrived
+     */
     def addObs(obsEvent: ObservationEvent, dataLabel: DataLabel) {
         val obsSet = obsEventsMap.getOrElseUpdate(dataLabel, EnumSet.noneOf(classOf[ObservationEvent]))
         obsSet.add(obsEvent)
     }
 
+    /**
+     * Delete all data for a given DataLabel
+     */
     def clean(dataLabel: DataLabel) {
         repliesMap -= dataLabel
         obsEventsMap -= dataLabel
