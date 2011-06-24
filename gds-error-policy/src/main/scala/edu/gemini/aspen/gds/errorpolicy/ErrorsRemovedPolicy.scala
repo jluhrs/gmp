c054535f9fd02695d1c85ede31c2460010929ab9
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-06-24 19:08:18 +0000

    Added implementation and test of an ErrorPolicy
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@35786 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- gds-error-policy/src/main/scala/edu/gemini/aspen/gds/errorpolicy/ErrorsRemovedPolicy.scala
+++ gds-error-policy/src/main/scala/edu/gemini/aspen/gds/errorpolicy/ErrorsRemovedPolicy.scala
@@ -1,8 +1,18 @@
 package edu.gemini.aspen.gds.errorpolicy
 
 import org.apache.felix.ipojo.annotations.{Provides, Component}
-import edu.gemini.aspen.gds.api.{DefaultErrorPolicy, ErrorPolicy}
+import edu.gemini.aspen.giapi.data.DataLabel
+import edu.gemini.aspen.gds.api.{ErrorCollectedValue, CollectedValue, DefaultErrorPolicy, ErrorPolicy}
 
 @Component
 @Provides
-class ErrorsRemovedPolicy extends DefaultErrorPolicy
+class ErrorsRemovedPolicy extends DefaultErrorPolicy {
+    override def applyPolicy(dataLabel: DataLabel, headers: Option[List[CollectedValue[_]]]): Option[List[CollectedValue[_]]] = headers map {
+        _ filter {
+            _ match {
+                case c:ErrorCollectedValue => false
+                case c => true
+            }
+        }
+    }
+}
--- gds-error-policy/src/test/scala/edu/gemini/aspen/gds/errorpolicy/ErrorsRemovedPolicyTest.scala
+++ gds-error-policy/src/test/scala/edu/gemini/aspen/gds/errorpolicy/ErrorsRemovedPolicyTest.scala
@@ -1,10 +1,29 @@
 package edu.gemini.aspen.gds.errorpolicy
 
 import org.junit.Test
+import org.junit.Assert._
+import edu.gemini.aspen.giapi.data.DataLabel
+import edu.gemini.aspen.gds.api.Conversions._
+import scala.Option
+import edu.gemini.aspen.gds.api.{CollectionError, ErrorCollectedValue, CollectedValue}
 
 class ErrorsRemovedPolicyTest {
+    val dataLabel = new DataLabel("some name")
+
     @Test
     def testNonErrors() {
-        
+        val ep = new ErrorsRemovedPolicy()
+        val collectedValues = Option(CollectedValue[Double]("KEY1", 1.0, "comment", 0) :: Nil)
+
+        assertEquals(collectedValues, ep.applyPolicy(dataLabel, collectedValues))
+    }
+
+    @Test
+    def testWithOneError() {
+        val ep = new ErrorsRemovedPolicy()
+        val collectedValues = Option(CollectedValue[Double]("KEY1", 1.0, "comment", 0) :: ErrorCollectedValue("KEY2", CollectionError.GenericError, "comment", 0) :: Nil)
+        val filteredValues = Option(CollectedValue[Double]("KEY1", 1.0, "comment", 0) :: Nil)
+
+        assertEquals(filteredValues, ep.applyPolicy(dataLabel, collectedValues))
     }
 }
