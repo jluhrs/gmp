9e35b9a07a9620c7f6be9716284755be9bff2da8
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-06-24 20:11:14 +0000

    Initial implementatio of the CompositeErrorPolicy and integration test
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@35792 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- gds-api/src/main/scala/edu/gemini/aspen/gds/api/CompositeErrorPolicy.scala
+++ gds-api/src/main/scala/edu/gemini/aspen/gds/api/CompositeErrorPolicy.scala
@@ -1,6 +1,9 @@
 package edu.gemini.aspen.gds.api
 
-import org.apache.felix.ipojo.annotations.{Provides, Instantiate, Component}
+import org.apache.felix.ipojo.annotations._
+import edu.gemini.aspen.giapi.data.DataLabel
+import org.osgi.framework.PackagePermission
+import javax.management.remote.rmi._RMIConnection_Stub
 
 /**
  * Interface for an ErrorPolicy that uses OSGi services implementing error policies
@@ -13,4 +16,26 @@ trait CompositeErrorPolicy extends ErrorPolicy
 @Component
 @Instantiate
 @Provides(specifications = Array(classOf[CompositeErrorPolicy]))
-class CompositeErrorPolicyImpl extends DefaultErrorPolicy with CompositeErrorPolicy
+class CompositeErrorPolicyImpl extends DefaultErrorPolicy with CompositeErrorPolicy {
+    var policies: List[ErrorPolicy] = List()
+
+    // Let all the original headers to be applied
+    override def applyPolicy(dataLabel: DataLabel, headers: Option[List[CollectedValue[_]]]): Option[List[CollectedValue[_]]] = {
+        val p = for {ep <- policies
+        } yield (ep.applyPolicy(dataLabel, headers))
+        println(p)
+        Option(p flatMap {
+            _.get
+        })
+    }
+
+    @Bind(optional = true)
+    def bindPolicy(ep: ErrorPolicy) {
+        policies = ep :: policies
+    }
+
+    @Validate
+    def validate() {
+        println("Validated")
+    }
+}
---
+++ integration-tests/src/test/java/edu/gemini/aspen/integrationtests/GDSWithODBAndEliminateErrorsPolicyIT.java
@@ -0,0 +1,92 @@
+package edu.gemini.aspen.integrationtests;
+
+import edu.gemini.aspen.gds.keywords.database.ProgramIdDatabase;
+import edu.gemini.aspen.giapi.data.DataLabel;
+import edu.gemini.aspen.giapi.data.ObservationEventHandler;
+import edu.gemini.fits.FitsParseException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.Configuration;
+import org.ops4j.pax.exam.junit.JUnit4TestRunner;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+import static org.ops4j.pax.exam.CoreOptions.options;
+import static org.ops4j.pax.exam.CoreOptions.systemProperty;
+
+@RunWith(JUnit4TestRunner.class)
+public class GDSWithODBAndEliminateErrorsPolicyIT extends GDSIntegrationBase {
+
+    @Configuration
+    public static Option[] gdsODBBundles() {
+        return options(
+                systemProperty("jini.lus.import.hosts").value("sbfswgdev01.cl.gemini.edu"),
+                systemProperty("org.osgi.framework.system.packages.extra").value("sun.misc,sun.security.action,sun.rmi.runtime,edu.gemini.rmi.server"),
+                systemProperty("org.osgi.framework.bootdelegation").value("java.rmi.server"),
+                systemProperty("jini.lus.import.groups").value("swg-test"),
+                systemProperty("org.osgi.service.http.port").value("8888"),
+                mavenBundle().artifactId("pax-web-jetty-bundle").groupId("org.ops4j.pax.web").versionAsInProject(),
+                mavenBundle().artifactId("pax-web-spi").groupId("org.ops4j.pax.web").versionAsInProject(),
+                mavenBundle().artifactId("org.osgi.compendium").groupId("org.osgi").versionAsInProject(),
+                mavenBundle().artifactId("jini-driver").groupId("gemini-nocs").versionAsInProject(),
+                mavenBundle().artifactId("spdb-activator").groupId("gemini-nocs").versionAsInProject(),
+                mavenBundle().artifactId("rr").groupId("gemini-nocs").versionAsInProject(),
+                mavenBundle().artifactId("gds-odb-actors").groupId("edu.gemini.aspen.gds").versionAsInProject(),
+                mavenBundle().artifactId("gds-error-policy").groupId("edu.gemini.aspen.gds").versionAsInProject()
+        );
+    }
+
+    @Override
+    protected String confDir() {
+        return "/src/test/resources/conf/gds_with_error_policy";
+    }
+
+    //@Test
+    public void bundleExistence() throws InterruptedException {
+        TimeUnit.MILLISECONDS.sleep(400);
+        assertNotNull(getBundle("edu.gemini.aspen.gds.odb"));
+    }
+
+    @Test
+    public void sendObsEvents() throws InterruptedException, URISyntaxException, IOException, FitsParseException {
+        TimeUnit.MILLISECONDS.sleep(400);
+        ObservationEventHandler eventHandler = (ObservationEventHandler) context.getService(context.getServiceReference(ObservationEventHandler.class.getName()));
+        assertNotNull(eventHandler);
+
+        postProgramIDToDataLabelLink();
+
+        copyInitialFile();
+
+        Set<String> originalKeywords = readOriginalKeywords();
+
+        assertFalse(originalKeywords.contains("PIFSTNAM"));
+
+        sendObservationEvents(eventHandler);
+
+        File finalFile = new File(FINAL_FITS_FILE);
+        assertTrue(finalFile.exists());
+
+        Set<String> afterProcessingKeywords = readFinalKeywords();
+
+        assertTrue(afterProcessingKeywords.containsAll(originalKeywords));
+        assertTrue(afterProcessingKeywords.contains("PIFSTNAM"));
+        assertFalse(afterProcessingKeywords.contains("EPIC"));
+    }
+
+    private void postProgramIDToDataLabelLink() {
+        ProgramIdDatabase programIdDatabase = (ProgramIdDatabase) context.getService(context.getServiceReference(ProgramIdDatabase.class.getName()));
+        assertNotNull(programIdDatabase);
+        DataLabel dataLabel = new DataLabel("S20110427-01");
+        programIdDatabase.store(dataLabel, "GS-2006B-Q-57");
+    }
+
+}
