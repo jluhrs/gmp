dcbe32f82766409ff9ffb0ac300dbc2e3546c953
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-07-03 18:37:52 +0000

    Added column definition class
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@35898 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- gds-web-ui-keywords/src/main/scala/edu/gemini/aspen/gds/web/ui/keywords/KeywordsTableModule.scala
+++ gds-web-ui-keywords/src/main/scala/edu/gemini/aspen/gds/web/ui/keywords/KeywordsTableModule.scala
@@ -3,45 +3,69 @@ package edu.gemini.aspen.gds.web.ui.keywords
 import edu.gemini.aspen.gds.web.ui.api.GDSWebModule
 import edu.gemini.aspen.gds.api.configuration.GDSConfigurationService
 import org.apache.felix.ipojo.annotations.{Requires, Provides, Instantiate, Component}
-import com.vaadin.ui.{Panel, Table}
 import scala.Predef._
-import edu.gemini.aspen.gds.api.{ArrayIndex, Mandatory, GDSConfiguration, Instrument}
+import edu.gemini.aspen.gds.api.{Mandatory, GDSConfiguration}
+import com.vaadin.ui.{CheckBox, Table}
+
+abstract class ColumnDefinition(clazz: Class[_], val columnType:Class[_]) {
+  def title = clazz.getSimpleName
+
+  def buildComponent(config: GDSConfiguration):Any
+}
+
+class DefaultColumnDefinition(clazz: Class[_]) extends ColumnDefinition(clazz, classOf[String]) {
+  def buildComponent(config: GDSConfiguration) = title
+}
+
+class MandatoryColumnDefinition() extends ColumnDefinition(classOf[Mandatory], classOf[CheckBox]) {
+  def buildComponent(config: GDSConfiguration) = title
+}
 
 @Component
 @Instantiate
 @Provides(specifications = Array(classOf[GDSWebModule]))
 class KeywordsTableModule(@Requires configService: GDSConfigurationService) extends GDSWebModule {
-    val title = "Keyword Configuration"
-    val order = 0
-    val columns = Map[Class[_], Any](classOf[Mandatory] -> classOf[Boolean], classOf[ArrayIndex] -> classOf[String])
+  val title = "Keyword Configuration"
+  val order = 0
+  // Contains the non default column definitions
+  val columnsDefinitions = Map[Class[_], ColumnDefinition](classOf[Mandatory] -> new MandatoryColumnDefinition())
 
-    override def buildTabContent = {
-        val table = new Table("")
-        table.setSizeFull
-        table.setSelectable(true)
-        table.setColumnCollapsingAllowed(true)
+  def defaultColumnDefinition(clazz: Class[_]) = new DefaultColumnDefinition(clazz)
 
-        classOf[GDSConfiguration].getDeclaredFields foreach {
-            c => table.addContainerProperty(c.getType.getSimpleName, "".getClass, "")
-        }
-        configService.getConfiguration foreach {
-            v => table.addItem(configToItem(v), v)
-        }
-        table
+  override def buildTabContent = {
+    val table = new Table("")
+    table.setSizeFull
+    table.setSelectable(true)
+    table.setColumnCollapsingAllowed(true)
+
+    classOf[GDSConfiguration].getDeclaredFields foreach {
+      c => {
+        val cd = columnsDefinitions.getOrElse(c.getType, defaultColumnDefinition(c.getType))
+        table.addContainerProperty(cd.title, cd.columnType, "")
+      }
     }
-
-    def configToItem(config: GDSConfiguration) = {
-        Array[Object](config.instrument.name.toString,
-            config.event.name.toString,
-            config.keyword.getName.toString,
-            config.index.index.toString,
-            config.dataType.name.toString,
-            config.mandatory.mandatory.toString,
-            config.nullValue.value.toString,
-            config.subsystem.name.toString,
-            config.channel.name.toString,
-            config.arrayIndex.value.toString,
-            config.fitsComment.value.toString
-        )
+    configService.getConfiguration foreach {
+      v => table.addItem(configToItem(v), v)
     }
+    table
+  }
+
+  def configToItem(config: GDSConfiguration):Array[Object] = {
+    /*for {f <- classOf[GDSConfiguration].getDeclaredFields
+        val cd = columnsDefinitions.getOrElse(f.getType, defaultColumnDefinition(f.getType))
+        table.addContainerProperty(cd.title, cd.columnType, "")
+    } yield cd.buildComponent(config)*/
+    Array[Object](config.instrument.name.toString,
+      config.event.name.toString,
+      config.keyword.getName.toString,
+      config.index.index.toString,
+      config.dataType.name.toString,
+      config.mandatory.mandatory.toString,
+      config.nullValue.value.toString,
+      config.subsystem.name.toString,
+      config.channel.name.toString,
+      config.arrayIndex.value.toString,
+      config.fitsComment.value.toString
+    )
+  }
 }
---
+++ gds-web-ui-keywords/src/test/scala/edu/gemini/aspen/gds/web/ui/keywords/ColumnDefinitionTest.scala
@@ -0,0 +1,15 @@
+package edu.gemini.aspen.gds.web.ui.keywords
+
+import org.junit.Test
+import org.junit.Assert._
+import com.vaadin.ui.CheckBox
+
+class ColumnDefinitionTest {
+  @Test
+  def testMandatoryColumnDefinition {
+    val definition = new MandatoryColumnDefinition
+    assertEquals("Mandatory", definition.title)
+    assertEquals(classOf[CheckBox], definition.columnType)
+    assertEquals(classOf[CheckBox], definition.buildComponent())
+  }
+}
