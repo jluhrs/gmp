14044aef30dcff64e629c7d0e4f1d6516ef399a9
nbarriga <nbarriga@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-07-06 19:23:14 +0000

    Added support for other datatypes to ConstantActor. Added tests.
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@35962 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- gds-constant-actors/src/main/scala/edu/gemini/aspen/gds/constant/ConstantActor.scala
+++ gds-constant-actors/src/main/scala/edu/gemini/aspen/gds/constant/ConstantActor.scala
@@ -8,7 +8,29 @@ import edu.gemini.aspen.gds.api._
  */
 class ConstantActor(configurations: List[GDSConfiguration]) extends KeywordValueActor {
     override def collectValues(): List[CollectedValue[_]] = {
-        for {config <- configurations} yield CollectedValue(config.keyword, config.nullValue.value, config.fitsComment.value, config.index.index)
+        for {config <- configurations} yield getCollectedValue(config: GDSConfiguration)
     }
 
+    /**
+     * Method to convert a value read from a given source to the type requested in the configuration
+     *
+     */
+    private def getCollectedValue(config: GDSConfiguration): CollectedValue[_] = {
+        try {
+            config.dataType match {
+                // Anything can be converted to a string
+                case DataType("STRING") => CollectedValue(config.keyword, config.nullValue.value, config.fitsComment.value, config.index.index)
+                // Any number can be converted to a double
+                case DataType("DOUBLE") => CollectedValue(config.keyword, config.nullValue.value.toDouble, config.fitsComment.value, config.index.index)
+                // Any number can be converted to a int
+                case DataType("INT") => CollectedValue(config.keyword, config.nullValue.value.toInt, config.fitsComment.value, config.index.index)
+                // this should not happen
+                case _ => ErrorCollectedValue(config.keyword, CollectionError.TypeMismatch, config.fitsComment.value, config.index.index)
+            }
+        } catch {
+            //The string was not parseable as a number
+            case ex: NumberFormatException => ErrorCollectedValue(config.keyword, CollectionError.TypeMismatch, config.fitsComment.value, config.index.index)
+            case _ => ErrorCollectedValue(config.keyword, CollectionError.GenericError, config.fitsComment.value, config.index.index)
+        }
+    }
 }
--- gds-constant-actors/src/test/scala/edu/gemini/aspen/gds/constant/ConstantActorTest.scala
+++ gds-constant-actors/src/test/scala/edu/gemini/aspen/gds/constant/ConstantActorTest.scala
@@ -3,7 +3,8 @@ package edu.gemini.aspen.gds.constant
 import edu.gemini.aspen.gds.api.Conversions._
 import org.junit.Test
 import org.junit.Assert._
-import edu.gemini.aspen.gds.api.{CollectedValue, GDSConfiguration}
+import edu.gemini.aspen.giapi.data.ObservationEvent
+import edu.gemini.aspen.gds.api.{ErrorCollectedValue, CollectionError, CollectedValue, GDSConfiguration}
 
 class ConstantActorTest {
 
@@ -11,6 +12,43 @@ class ConstantActorTest {
     def testActor() {
         val constActor = new ConstantActor(buildConfiguration("key1", "val1") :: buildConfiguration("key2", "val2") :: Nil)
         assertEquals(CollectedValue("key1", "val1", "", 0) :: CollectedValue("key2", "val2", "", 0) :: Nil, constActor.collectValues())
+    }
+
+    @Test
+    def testActorWrongType() {
+        val constActor = new ConstantActor(GDSConfiguration("GPI",
+            "OBS_START_ACQ",
+            "key1",
+            0,
+            "INT",
+            false,
+            "val1",
+            "CONSTANT",
+            "",
+            0,
+            "") :: Nil)
+        assertEquals(ErrorCollectedValue("key1", CollectionError.TypeMismatch, "", 0) :: Nil, constActor.collectValues())
+    }
+
+    @Test
+    def testActorFactory() {
+        val factory = new ConstantActorsFactory
+        factory.configure(List(GDSConfiguration("GPI", "OBS_PREP", "TEST", 0, "DOUBLE", false, "1.0", "CONSTANT", "ws:massAirmass", 0, "my comment")))
+
+        assertEquals(1, factory.buildActors(ObservationEvent.OBS_START_ACQ, "label").length)
+
+        val actors = factory.buildActors(ObservationEvent.OBS_PREP, "label")
+        assertEquals(1, actors.length)
+
+        val values = actors.head.collectValues()
+        values.head match {
+            case CollectedValue(fits, value, comment, 0) => {
+                assertEquals(stringToFitsKeyword("TEST"), fits)
+                assertEquals(1.0, value)
+                assertEquals("my comment", comment)
+            }
+            case _ => fail("Wrong answer")
+        }
     }
 
     private def buildConfiguration(keyword: String, value: String): GDSConfiguration = {
