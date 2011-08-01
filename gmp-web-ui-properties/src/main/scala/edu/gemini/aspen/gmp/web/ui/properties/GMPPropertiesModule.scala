b4f1b2e364361d8f1aa2350311e512efb3f3459a
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-08-01 20:47:50 +0000

    Added temporal implementation of a web panel to show the GMP Properties
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@36359 ee6ba543-07f8-4773-b02c-01d50e9d00ba

---
+++ gmp-web-ui-properties/src/main/scala/edu/gemini/aspen/gmp/web/ui/properties/GMPPropertiesModule.scala
@@ -0,0 +1,34 @@
+package edu.gemini.aspen.gmp.web.ui.properties
+
+import _root_.edu.gemini.aspen.gds.web.ui.api.GDSWebModule
+import com.vaadin.Application
+import edu.gemini.aspen.gmp.services.{GMPProperties, GMPServices, PropertyHolder}
+import com.vaadin.ui._
+
+/**
+ * Module for the table to edit the keywords
+ */
+class GMPPropertiesModule(propertyHolder: PropertyHolder) extends GDSWebModule {
+  val title = "GMP Properties"
+  val order = 4
+
+  val layout = new FormLayout
+
+  override def buildTabContent(app: Application) = {
+    val topLabel = new Label()
+    topLabel.setCaption("Property name")
+    topLabel.setValue("Property value")
+    layout.addComponent(topLabel)
+
+    GMPProperties.values() foreach {
+      p =>
+          val label = new Label()
+        label.setCaption(p.name)
+        label.setValue(propertyHolder.getProperty(p.name))
+        layout.addComponent(label)
+    }
+
+    layout
+  }
+
+}
