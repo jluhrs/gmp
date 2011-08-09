0808cda72002ab8058bd890cf328cc1ed89f88fb
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-08-09 15:18:03 +0000

    Added expiration to items in the ObservationState database
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@36485 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- gds-observation-state-publisher/src/main/scala/edu/gemini/aspen/gds/observationstate/impl/ObservationStateImpl.scala
+++ gds-observation-state-publisher/src/main/scala/edu/gemini/aspen/gds/observationstate/impl/ObservationStateImpl.scala
@@ -2,73 +2,81 @@ package edu.gemini.aspen.gds.observationstate.impl
 
 import org.apache.felix.ipojo.annotations.{Requires, Provides, Instantiate, Component}
 import edu.gemini.aspen.giapi.data.{FitsKeyword, DataLabel}
-import edu.gemini.aspen.gds.api.CollectionError
 import org.scala_tools.time.Imports._
 import edu.gemini.aspen.gds.observationstate.{ObservationStatePublisher, ObservationStateProvider, ObservationStateRegistrar}
 import collection.mutable.{SynchronizedMap, HashMap, SynchronizedSet, HashSet, Set}
 import java.util.concurrent.atomic.AtomicReference
+import java.util.concurrent.TimeUnit._
+import collection.mutable.ConcurrentMap
+import scala.collection.JavaConversions._
+import com.google.common.collect.MapMaker
+import edu.gemini.aspen.gds.api.{CollectedValue, CollectionError}
 
 @Component
 @Instantiate
 @Provides(specifications = Array(classOf[ObservationStateRegistrar], classOf[ObservationStateProvider]))
 class ObservationStateImpl(@Requires obsStatePubl: ObservationStatePublisher) extends ObservationStateRegistrar with ObservationStateProvider {
+  // expiration of 1 day by default but tests can override it
+  def expirationMillis = 24 * 60 * 60 * 1000
 
-    class ObservationInfo {
-        val missingKeywords: Set[FitsKeyword] = new HashSet[FitsKeyword] with SynchronizedSet[FitsKeyword]
-        val errorKeywords: Set[(FitsKeyword, CollectionError.CollectionError)] = new HashSet[(FitsKeyword, CollectionError.CollectionError)] with SynchronizedSet[(FitsKeyword, CollectionError.CollectionError)]
-        val times: Set[(AnyRef, Option[Duration])] = new HashSet[(AnyRef, Option[Duration])] with SynchronizedSet[(AnyRef, Option[Duration])] //todo: think which is the correct type here
-        var started = false
-        var ended = false
-    }
+  class ObservationInfo {
+    val missingKeywords: Set[FitsKeyword] = new HashSet[FitsKeyword] with SynchronizedSet[FitsKeyword]
+    val errorKeywords: Set[(FitsKeyword, CollectionError.CollectionError)] = new HashSet[(FitsKeyword, CollectionError.CollectionError)] with SynchronizedSet[(FitsKeyword, CollectionError.CollectionError)]
+    val times: Set[(AnyRef, Option[Duration])] = new HashSet[(AnyRef, Option[Duration])] with SynchronizedSet[(AnyRef, Option[Duration])] //todo: think which is the correct type here
+    var started = false
+    var ended = false
+  }
 
-    //todo: this Map is never cleaned. We need a general strategy on when/who to clean. Maybe a periodic thread sends clean directives to all DBs
-    val obsInfoMap = new HashMap[DataLabel, ObservationInfo] with SynchronizedMap[DataLabel, ObservationInfo]
-    var lastDataLabel = new AtomicReference[Option[DataLabel]](None)
+  val obsInfoMap: ConcurrentMap[DataLabel, ObservationInfo] = new MapMaker().
+    expireAfterWrite(expirationMillis, MILLISECONDS)
+    .makeMap[DataLabel, ObservationInfo]()
 
-    override def registerMissingKeyword(label: DataLabel, keywords: Traversable[FitsKeyword]) {
-        obsInfoMap.getOrElseUpdate(label, new ObservationInfo).missingKeywords ++= keywords
-    }
+  val lastDataLabel = new AtomicReference[Option[DataLabel]](None)
 
-    override def registerCollectionError(label: DataLabel, errors: Traversable[(FitsKeyword, CollectionError.CollectionError)]) {
-        obsInfoMap.getOrElseUpdate(label, new ObservationInfo).errorKeywords ++= errors
-    }
+  override def registerMissingKeyword(label: DataLabel, keywords: Traversable[FitsKeyword]) {
+    obsInfoMap.getOrElseUpdate(label, new ObservationInfo).missingKeywords ++= keywords
+  }
 
-    override def registerTimes(label: DataLabel, times: Traversable[(AnyRef, Option[Duration])]) {
-        obsInfoMap.getOrElseUpdate(label, new ObservationInfo).times ++= times
-    }
+  override def registerCollectionError(label: DataLabel, errors: Traversable[(FitsKeyword, CollectionError.CollectionError)]) {
+    obsInfoMap.getOrElseUpdate(label, new ObservationInfo).errorKeywords ++= errors
+  }
 
-    override def endObservation(label: DataLabel) {
-        obsInfoMap.getOrElseUpdate(label, new ObservationInfo).ended = true
-        lastDataLabel.set(Some(label))
-        obsStatePubl.publishEndObservation(label, getMissingKeywords(label), getKeywordsInError(label))
-    }
+  override def registerTimes(label: DataLabel, times: Traversable[(AnyRef, Option[Duration])]) {
+    obsInfoMap.getOrElseUpdate(label, new ObservationInfo).times ++= times
+  }
 
-    override def startObservation(label: DataLabel) {
-        obsInfoMap.getOrElseUpdate(label, new ObservationInfo).started = true
-        obsStatePubl.publishStartObservation(label)
-    }
+  override def endObservation(label: DataLabel) {
+    obsInfoMap.getOrElseUpdate(label, new ObservationInfo).ended = true
+    lastDataLabel.set(Some(label))
+    obsStatePubl.publishEndObservation(label, getMissingKeywords(label), getKeywordsInError(label))
+  }
 
-    //-----------------------------------------------------------------------
+  override def startObservation(label: DataLabel) {
+    obsInfoMap.getOrElseUpdate(label, new ObservationInfo).started = true
+    obsStatePubl.publishStartObservation(label)
+  }
 
-    override def getTimes(label: DataLabel): Traversable[(AnyRef, Option[Duration])] = {
-        obsInfoMap.getOrElse(label, new ObservationInfo).times
-    }
+  //-----------------------------------------------------------------------
 
-    override def getMissingKeywords(label: DataLabel): Traversable[FitsKeyword] = {
-        obsInfoMap.getOrElse(label, new ObservationInfo).missingKeywords
-    }
+  override def getTimes(label: DataLabel): Traversable[(AnyRef, Option[Duration])] = {
+    obsInfoMap.getOrElse(label, new ObservationInfo).times
+  }
 
-    override def getKeywordsInError(label: DataLabel): Traversable[(FitsKeyword, CollectionError.CollectionError)] = {
-        obsInfoMap.getOrElse(label, new ObservationInfo).errorKeywords
-    }
+  override def getMissingKeywords(label: DataLabel): Traversable[FitsKeyword] = {
+    obsInfoMap.getOrElse(label, new ObservationInfo).missingKeywords
+  }
 
-    override def getObservationsInProgress: Traversable[DataLabel] = {
-        obsInfoMap filter {
-            case (key, value) => (value.started && !value.ended)
-        } keySet
-    }
+  override def getKeywordsInError(label: DataLabel): Traversable[(FitsKeyword, CollectionError.CollectionError)] = {
+    obsInfoMap.getOrElse(label, new ObservationInfo).errorKeywords
+  }
 
-    override def getLastDataLabel: Option[DataLabel] = {
-        lastDataLabel.get()
-    }
+  override def getObservationsInProgress: Traversable[DataLabel] = {
+    obsInfoMap filter {
+      case (key, value) => (value.started && !value.ended)
+    } keySet
+  }
+
+  override def getLastDataLabel: Option[DataLabel] = {
+    lastDataLabel.get()
+  }
 }
--- gds-observation-state-publisher/src/test/scala/edu/gemini/aspen/gds/observationstate/impl/ObservationStateImplTest.scala
+++ gds-observation-state-publisher/src/test/scala/edu/gemini/aspen/gds/observationstate/impl/ObservationStateImplTest.scala
@@ -8,51 +8,70 @@ import edu.gemini.aspen.gds.observationstate.ObservationStatePublisher
 import edu.gemini.aspen.giapi.data.{FitsKeyword, DataLabel}
 import edu.gemini.aspen.gds.api.CollectionError
 import collection.immutable.Set.Set2
+import java.util.concurrent.TimeUnit
 
 class ObservationStateImplTest {
 
-    @Test
-    def testObsInProgress() {
-        val obsState: ObservationStateImpl = new ObservationStateImpl(mock(classOf[ObservationStatePublisher]))
-        assertTrue(obsState.getObservationsInProgress.isEmpty)
+  @Test
+  def testObsInProgress() {
+    val obsState: ObservationStateImpl = new ObservationStateImpl(mock(classOf[ObservationStatePublisher]))
+    assertTrue(obsState.getObservationsInProgress.isEmpty)
 
-        obsState.startObservation("label1")
-        assertEquals(new DataLabel("label1"), obsState.getObservationsInProgress.head)
-        assertEquals(obsState.getObservationsInProgress.size, 1)
+    obsState.startObservation("label1")
+    assertEquals(new DataLabel("label1"), obsState.getObservationsInProgress.head)
+    assertEquals(obsState.getObservationsInProgress.size, 1)
 
-        obsState.startObservation("label2")
-        obsState.endObservation("label2")
-        assertEquals(new DataLabel("label1"), obsState.getObservationsInProgress.head)
-        assertEquals(obsState.getObservationsInProgress.size, 1)
+    obsState.startObservation("label2")
+    obsState.endObservation("label2")
+    assertEquals(new DataLabel("label1"), obsState.getObservationsInProgress.head)
+    assertEquals(obsState.getObservationsInProgress.size, 1)
+  }
+
+  @Test
+  def testLastDataLabel() {
+    val obsState: ObservationStateImpl = new ObservationStateImpl(mock(classOf[ObservationStatePublisher]))
+    obsState.startObservation("label1")
+    obsState.startObservation("label2")
+    obsState.endObservation("label2")
+    assertEquals(Some(new DataLabel("label2")), obsState.getLastDataLabel)
+
+    obsState.endObservation("label1")
+    assertEquals(Some(new DataLabel("label1")), obsState.getLastDataLabel)
+  }
+
+  @Test
+  def testMissing() {
+    val obsState: ObservationStateImpl = new ObservationStateImpl(mock(classOf[ObservationStatePublisher]))
+    obsState.startObservation("label1")
+    obsState.registerMissingKeyword("label1", new FitsKeyword("a") :: Nil)
+    obsState.registerMissingKeyword("label1", new FitsKeyword("b") :: Nil)
+    assertEquals((new FitsKeyword("b") :: new FitsKeyword("a") :: Nil).toSet, obsState.getMissingKeywords("label1").toSet)
+  }
+
+  @Test
+  def testError() {
+    val obsState: ObservationStateImpl = new ObservationStateImpl(mock(classOf[ObservationStatePublisher]))
+    obsState.startObservation("label1")
+    obsState.registerCollectionError("label1", List((new FitsKeyword("a"), CollectionError.GenericError)))
+    obsState.registerCollectionError("label1", List((new FitsKeyword("b"), CollectionError.GenericError)))
+    assertEquals(new Set2((new FitsKeyword("b"), CollectionError.GenericError), (new FitsKeyword("a"), CollectionError.GenericError)), obsState.getKeywordsInError("label1").toSet)
+  }
+
+  @Test
+  def testExpiration() {
+    val obsState: ObservationStateImpl = new ObservationStateImpl(mock(classOf[ObservationStatePublisher])) {
+      override def expirationMillis = 5
     }
+    obsState.startObservation("label1")
+    obsState.registerCollectionError("label1", List((new FitsKeyword("a"), CollectionError.GenericError)))
+    obsState.registerCollectionError("label1", List((new FitsKeyword("b"), CollectionError.GenericError)))
 
-    @Test
-    def testLastDataLabel() {
-        val obsState: ObservationStateImpl = new ObservationStateImpl(mock(classOf[ObservationStatePublisher]))
-        obsState.startObservation("label1")
-        obsState.startObservation("label2")
-        obsState.endObservation("label2")
-        assertEquals(Some(new DataLabel("label2")), obsState.getLastDataLabel)
+    // Verify it is there
+    assertEquals(new Set2((new FitsKeyword("b"), CollectionError.GenericError), (new FitsKeyword("a"), CollectionError.GenericError)), obsState.getKeywordsInError("label1").toSet)
 
-        obsState.endObservation("label1")
-        assertEquals(Some(new DataLabel("label1")), obsState.getLastDataLabel)
-    }
+    TimeUnit.MILLISECONDS.sleep(5)
 
-    @Test
-    def testMissing() {
-        val obsState: ObservationStateImpl = new ObservationStateImpl(mock(classOf[ObservationStatePublisher]))
-        obsState.startObservation("label1")
-        obsState.registerMissingKeyword("label1", new FitsKeyword("a") :: Nil)
-        obsState.registerMissingKeyword("label1", new FitsKeyword("b") :: Nil)
-        assertEquals((new FitsKeyword("b") :: new FitsKeyword("a") :: Nil).toSet, obsState.getMissingKeywords("label1").toSet)
-    }
-
-    @Test
-    def testError() {
-        val obsState: ObservationStateImpl = new ObservationStateImpl(mock(classOf[ObservationStatePublisher]))
-        obsState.startObservation("label1")
-        obsState.registerCollectionError("label1", List((new FitsKeyword("a"), CollectionError.GenericError)))
-        obsState.registerCollectionError("label1", List((new FitsKeyword("b"), CollectionError.GenericError)))
-        assertEquals(new Set2((new FitsKeyword("b"), CollectionError.GenericError), (new FitsKeyword("a"), CollectionError.GenericError)), obsState.getKeywordsInError("label1").toSet)
-    }
+    // Now it is gone
+    assertEquals(Set(), obsState.getKeywordsInError("label1").toSet)
+  }
 }
