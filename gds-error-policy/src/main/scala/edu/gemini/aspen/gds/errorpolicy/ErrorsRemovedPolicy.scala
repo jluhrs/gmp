937c2753aa37c2f2ad68d0ebae5d131cb4debff8
nbarriga <nbarriga@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-08-09 18:34:41 +0000

    Added comments, changed priority.
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@36496 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- gds-error-policy/src/main/scala/edu/gemini/aspen/gds/errorpolicy/ErrorsRemovedPolicy.scala
+++ gds-error-policy/src/main/scala/edu/gemini/aspen/gds/errorpolicy/ErrorsRemovedPolicy.scala
@@ -1,16 +1,22 @@
 package edu.gemini.aspen.gds.errorpolicy
 
-import org.apache.felix.ipojo.annotations.{Provides, Component}
 import edu.gemini.aspen.giapi.data.DataLabel
-import edu.gemini.aspen.gds.api.{ErrorCollectedValue, CollectedValue, DefaultErrorPolicy}
+import edu.gemini.aspen.gds.api.{ErrorPolicy, ErrorCollectedValue, CollectedValue, DefaultErrorPolicy}
+import org.apache.felix.ipojo.annotations.{Instantiate, Provides, Component}
 
+/**
+ * This policy removes ErrorCollectedValues. It should probably be the last policy applied.
+ */
 @Component
-@Provides
+@Instantiate
+@Provides(specifications = Array(classOf[ErrorPolicy]))
 class ErrorsRemovedPolicy extends DefaultErrorPolicy {
-    override def applyPolicy(dataLabel: DataLabel, headers: List[CollectedValue[_]]): List[CollectedValue[_]] = headers filter {
-        _ match {
-            case c: ErrorCollectedValue => false
-            case c => true
-        }
+  override val priority = 10
+
+  override def applyPolicy(dataLabel: DataLabel, headers: List[CollectedValue[_]]): List[CollectedValue[_]] = headers filter {
+    _ match {
+      case c: ErrorCollectedValue => false
+      case c => true
     }
+  }
 }
--- gds-observation-state-publisher/src/main/scala/edu/gemini/aspen/gds/observationstate/impl/InspectPolicy.scala
+++ gds-observation-state-publisher/src/main/scala/edu/gemini/aspen/gds/observationstate/impl/InspectPolicy.scala
@@ -6,36 +6,40 @@ import edu.gemini.aspen.gds.observationstate.ObservationStateRegistrar
 import edu.gemini.aspen.gds.api.configuration.GDSConfigurationService
 import edu.gemini.aspen.gds.api.{KeywordSource, ErrorCollectedValue, CollectedValue, ErrorPolicy}
 
+/**
+ * This policy checks for errors and missing values and adds them to the ObservationStateRegistrar.
+ * It should be the first policy applied.
+ */
 @Component
 @Instantiate
 @Provides(specifications = Array(classOf[ErrorPolicy]))
 class InspectPolicy(@Requires configService: GDSConfigurationService, @Requires obsState: ObservationStateRegistrar) extends ErrorPolicy {
-    override val priority = 0
+  override val priority = 0
 
-    override def applyPolicy(dataLabel: DataLabel, headers: List[CollectedValue[_]]): List[CollectedValue[_]] = {
-        checkErrors(dataLabel, headers)
-        checkMissing(dataLabel, headers)
-        headers
-    }
+  override def applyPolicy(dataLabel: DataLabel, headers: List[CollectedValue[_]]): List[CollectedValue[_]] = {
+    checkErrors(dataLabel, headers)
+    checkMissing(dataLabel, headers)
+    headers
+  }
 
-    private def checkErrors(label: DataLabel, headers: List[CollectedValue[_]]) {
-        obsState.registerCollectionError(label, headers collect {
-            case collected: ErrorCollectedValue => collected
-        } collect {
-            case c => (c.keyword, c.error)
-        })
-    }
+  private def checkErrors(label: DataLabel, headers: List[CollectedValue[_]]) {
+    obsState.registerCollectionError(label, headers collect {
+      case collected: ErrorCollectedValue => collected
+    } collect {
+      case c => (c.keyword, c.error)
+    })
+  }
 
-    private def checkMissing(label: DataLabel, headers: List[CollectedValue[_]]) {
-        val configList = configService.getConfiguration filterNot {
-            _.subsystem.name == KeywordSource.IFS //IFS keywords are not written by us, they are already in the file
-        }
-        obsState.registerMissingKeyword(label, configList filterNot {
-            config => headers exists {
-                collected => collected.keyword == config.keyword
-            }
-        } collect {
-            case config => config.keyword
-        })
+  private def checkMissing(label: DataLabel, headers: List[CollectedValue[_]]) {
+    val configList = configService.getConfiguration filterNot {
+      _.subsystem.name == KeywordSource.IFS //IFS keywords are not written by us, they are already in the file
     }
+    obsState.registerMissingKeyword(label, configList filterNot {
+      config => headers exists {
+        collected => collected.keyword == config.keyword
+      }
+    } collect {
+      case config => config.keyword
+    })
+  }
 }
