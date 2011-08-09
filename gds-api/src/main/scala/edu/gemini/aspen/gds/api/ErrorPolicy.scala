0628f82930b3320d1cca41bcf1ca27efebfdfa7a
nbarriga <nbarriga@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-08-09 18:33:35 +0000

    Added comments and a Logger for derived classes to use.
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@36495 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- gds-api/src/main/scala/edu/gemini/aspen/gds/api/ErrorPolicy.scala
+++ gds-api/src/main/scala/edu/gemini/aspen/gds/api/ErrorPolicy.scala
@@ -1,30 +1,34 @@
 package edu.gemini.aspen.gds.api
 
 import edu.gemini.aspen.giapi.data.DataLabel
+import java.util.logging.Logger
 
 /**
  * Defines an error policy
  */
 trait ErrorPolicy {
-    /**
-     * Lower priority policies are applied first
-     */
-    val priority = 5
+  /**
+   * Lower priority policies are applied first (0 - 10)
+   */
+  val priority = 5
+  //todo: maybe change to an enum or Byte, to have an appropriate range of possible values.
 
-    /**
-     * Applies the policy taking a set of proposed headers and returning the set of headers allowed
-     *
-     * @param dataLabel The data label being processed
-     * @param headers Proposed set of headers
-     * @return A list of headers to be written to the fits file
-     */
-    def applyPolicy(dataLabel: DataLabel, headers: List[CollectedValue[_]]): List[CollectedValue[_]]
+  /**
+   * Applies the policy taking a set of proposed headers and returning the set of headers allowed
+   *
+   * @param dataLabel The data label being processed
+   * @param headers Proposed set of headers
+   * @return A list of headers to be written to the fits file
+   */
+  def applyPolicy(dataLabel: DataLabel, headers: List[CollectedValue[_]]): List[CollectedValue[_]]
 }
 
 /**
  * The default error policy
  */
 class DefaultErrorPolicy extends ErrorPolicy {
-    // Let all the original headers to be applied
-    override def applyPolicy(dataLabel: DataLabel, headers: List[CollectedValue[_]]): List[CollectedValue[_]] = headers
+  protected val LOG = Logger.getLogger(this.getClass.getName)
+
+  // Let all the original headers to be applied
+  override def applyPolicy(dataLabel: DataLabel, headers: List[CollectedValue[_]]): List[CollectedValue[_]] = headers
 }
