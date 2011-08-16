56934a5435074417ee362371b7a848add2a0066b
nbarriga <nbarriga@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-08-16 15:55:43 +0000

    Split big list mappings into a few functions for readability.
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@36637 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- gds-error-policy/src/main/scala/edu/gemini/aspen/gds/errorpolicy/EnforceMandatoryPolicy.scala
+++ gds-error-policy/src/main/scala/edu/gemini/aspen/gds/errorpolicy/EnforceMandatoryPolicy.scala
@@ -3,8 +3,7 @@ package edu.gemini.aspen.gds.errorpolicy
 import edu.gemini.aspen.giapi.data.DataLabel
 import org.apache.felix.ipojo.annotations.{Requires, Instantiate, Provides, Component}
 import edu.gemini.aspen.gds.api.configuration.GDSConfigurationService
-import edu.gemini.aspen.gds.api.{DefaultCollectedValue, ErrorPolicy, CollectionError, ErrorCollectedValue, CollectedValue, DefaultErrorPolicy}
-
+import edu.gemini.aspen.gds.api.{GDSConfiguration, DefaultCollectedValue, ErrorPolicy, CollectionError, ErrorCollectedValue, CollectedValue, DefaultErrorPolicy}
 
 /**
  * This policy adds missing items(i.e. that are in the configuration but not among the CollectedValues) and then
@@ -18,19 +17,27 @@ class EnforceMandatoryPolicy(@Requires configService: GDSConfigurationService) e
   override val priority = 2
 
   override def applyPolicy(dataLabel: DataLabel, headers: List[CollectedValue[_]]): List[CollectedValue[_]] = {
-    headers ++ (configService.getConfiguration filterNot {
+    headers ++ constructValuesForMissing(getMissing(headers)) map {
+      case ErrorCollectedValue(keyword, CollectionError.MandatoryRequired, comment, index) => new CollectedValue(keyword, "", comment, index)
+      case c => c
+    }
+  }
+
+  private def getMissing(headers: List[CollectedValue[_]]): List[GDSConfiguration] = {
+    configService.getConfiguration filterNot {
       config => headers exists {
         collected => collected.keyword == config.keyword
       }
-    } map {
+    }
+  }
+
+  private def constructValuesForMissing(configurations: List[GDSConfiguration]): List[CollectedValue[_]] = {
+    configurations map {
       case config => if (config.isMandatory) {
-        new CollectedValue(config.keyword, "", config.fitsComment.value, config.index.index)
+        new CollectedValue[String](config.keyword, "", config.fitsComment.value, config.index.index)
       } else {
         new DefaultCollectedValue(config.keyword, config.nullValue.value, config.fitsComment.value, config.index.index)
       }
-    }) map {
-      case ErrorCollectedValue(keyword, CollectionError.MandatoryRequired, comment, index) => new CollectedValue(keyword, "", comment, index)
-      case c => c
     }
   }
 }
