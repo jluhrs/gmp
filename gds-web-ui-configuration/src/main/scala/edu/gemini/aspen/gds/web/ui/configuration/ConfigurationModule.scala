3caec020d6dff622b4eb5ca4979e7a8a06b25f8b
nbarriga <nbarriga@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-08-31 20:32:41 +0000

    GIAPI-495 GIAPI-696: Initial working implementation of changing a property.
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@37001 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- gds-web-ui-configuration/src/main/scala/edu/gemini/aspen/gds/web/ui/configuration/ConfigurationModule.scala
+++ gds-web-ui-configuration/src/main/scala/edu/gemini/aspen/gds/web/ui/configuration/ConfigurationModule.scala
@@ -1,22 +1,63 @@
 package edu.gemini.aspen.gds.web.ui.configuration
 
 import edu.gemini.aspen.gds.web.ui.api.GDSWebModule
-import com.vaadin.ui.{Panel, Component}
 import com.vaadin.Application
+import com.vaadin.data.util.ObjectProperty
+import edu.gemini.aspen.gmp.services.PropertyHolder
+import org.osgi.service.cm.ConfigurationAdmin
+import java.util.{Hashtable, Dictionary}
+import org.osgi.service.cm.Configuration
+import edu.gemini.aspen.gds.web.ui.api.Preamble._
+import com.vaadin.ui.{TextField, GridLayout, Button, Label, FormLayout, Panel, Component}
 
-class ConfigurationModule extends GDSWebModule {
+class ConfigurationModule(propHolder: PropertyHolder, configAdmin: ConfigurationAdmin) extends GDSWebModule {
   val title: String = "System Configuration"
   val order: Int = 5
 
+  import ConfigurationModule._
+
+  val gmp_host_name = new TextField()
+  val gmp_host_nameProp = new ObjectProperty(defaultGmp_host_name)
+  gmp_host_name.setPropertyDataSource(gmp_host_nameProp)
+
   override def buildTabContent(app: Application): Component = {
-    new Panel()
+    val layout = new GridLayout(3, 1)
+
+    layout.addComponent(new Label("GMP_HOST_NAME"))
+    layout.addComponent(gmp_host_name)
+    val okButton = new Button("Apply")
+    okButton.addListener((e: Button#ClickEvent) => {
+      setProperty("GMP_HOST_NAME", gmp_host_name.getValue.toString)
+    })
+
+
+    layout.addComponent(okButton)
+    layout
   }
 
   override def refresh() {
+    gmp_host_nameProp.setValue(getProperty("GMP_HOST_NAME"))
   }
 
+  private def getProperty(propName: String) = {
+    propHolder.getProperty(propName)
+  }
+
+  private def setProperty(propName: String, propValue: String) {
+    var config: Configuration = configAdmin.listConfigurations("(service.factorypid=edu.gemini.aspen.gmp.services.properties.SimplePropertyHolder)")(0)
+
+    var props: Dictionary[String, String] = config.getProperties.asInstanceOf[Dictionary[String, String]]
+
+    if (props == null) {
+      props = new Hashtable[String, String]()
+    }
+
+    props.put(propName, propValue)
+    config.update(props)
+  }
 }
 
-object ConfigurationModule {
 
+object ConfigurationModule {
+  val defaultGmp_host_name = "INVALID"
 }
--- gds-web-ui-configuration/src/main/scala/edu/gemini/aspen/gds/web/ui/configuration/ConfigurationModuleFactory.scala
+++ gds-web-ui-configuration/src/main/scala/edu/gemini/aspen/gds/web/ui/configuration/ConfigurationModuleFactory.scala
@@ -1,14 +1,16 @@
 package edu.gemini.aspen.gds.web.ui.configuration
 
-import org.apache.felix.ipojo.annotations.{Provides, Instantiate, Component}
 import edu.gemini.aspen.gds.web.ui.api.GDSWebModuleFactory
+import org.apache.felix.ipojo.annotations.{Requires, Provides, Instantiate, Component}
+import org.osgi.service.cm.ConfigurationAdmin
+import edu.gemini.aspen.gmp.services.PropertyHolder
 
 
 @Component
 @Instantiate
 @Provides(specifications = Array(classOf[GDSWebModuleFactory]))
-class ConfigurationModuleFactory extends GDSWebModuleFactory {
-  override def buildWebModule = new ConfigurationModule
+class ConfigurationModuleFactory(@Requires propHolder: PropertyHolder, @Requires configAdmin: ConfigurationAdmin) extends GDSWebModuleFactory {
+  override def buildWebModule = new ConfigurationModule(propHolder, configAdmin)
 
   override protected def canEqual(other: Any): Boolean = other.isInstanceOf[ConfigurationModuleFactory]
 }
