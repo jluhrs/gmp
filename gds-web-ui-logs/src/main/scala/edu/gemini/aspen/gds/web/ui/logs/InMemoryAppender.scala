4caf6f31ff591eff78ee2c851669fe6688d2a712
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-08-31 19:23:09 +0000

    Added logEvents method and test
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@36993 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- gds-web-ui-logs/src/main/scala/edu/gemini/aspen/gds/web/ui/logs/InMemoryAppender.scala
+++ gds-web-ui-logs/src/main/scala/edu/gemini/aspen/gds/web/ui/logs/InMemoryAppender.scala
@@ -2,21 +2,41 @@ package edu.gemini.aspen.gds.web.ui.logs
 
 import org.apache.felix.ipojo.annotations._
 import org.ops4j.pax.logging.spi.{PaxLoggingEvent, PaxAppender}
+import collection.mutable.ConcurrentMap
+import scala.collection.JavaConversions._
+import com.google.common.collect.MapMaker
+import java.util.concurrent.TimeUnit._
+import java.util.concurrent.atomic.AtomicInteger
 
 /**
- * A PaxAppender service thatwill get log events from pax-logging */
+ * A PaxAppender service that will get log events from pax-logging */
 @Component
 @Instantiate
 @Provides(specifications = Array(classOf[PaxAppender]))
 class InMemoryAppender extends PaxAppender {
+  val MAXSIZE = 10000
   /** Service property that matches the configuration on the org.ops4j.pax.logging.cfg file */
   @ServiceProperty(name = "org.ops4j.pax.logging.appender.name", value = "GeminiAppender")
   val name = "GeminiAppender"
+
+  // expiration of 1 day by default but tests can override it
+  def expirationMillis = 24 * 60 * 60 * 1000
+
+  // We index with an artificial value to avoid collisions with timestamps
+  val index = new AtomicInteger(0)
+  val logEventsMap: ConcurrentMap[Int, PaxLoggingEvent] = new MapMaker()
+    .expireAfterWrite(expirationMillis, MILLISECONDS)
+    .maximumSize(MAXSIZE)
+    .makeMap[Int, PaxLoggingEvent]()
 
   @Validate
   def initLogListener() {
   }
 
-  def doAppend(event: PaxLoggingEvent) {
+  override def doAppend(event: PaxLoggingEvent) {
+    val i = index.incrementAndGet()
+    logEventsMap += i -> event
   }
+
+  def logEvents = logEventsMap.values
 }
---
+++ gds-web-ui-logs/src/test/scala/edu/gemini/aspen/gds/web/ui/logs/InMemoryAppenderTest.scala
@@ -0,0 +1,35 @@
+package edu.gemini.aspen.gds.web.ui.logs
+
+import org.junit.Test
+import org.junit.Assert._
+import java.security.SecureRandom
+import org.ops4j.pax.logging.service.internal.PaxLoggingEventImpl
+import org.apache.log4j.spi.LoggingEvent
+import org.apache.log4j.{Level, Logger}
+
+/**
+ * Tests for the InMemoryAppender class
+ */
+class InMemoryAppenderTest {
+  @Test
+  def testBuild {
+    val appender = new InMemoryAppender()
+    assertNotNull(appender)
+  }
+
+  @Test
+  def testAppend {
+    val appender = new InMemoryAppender()
+    val event = newLoggingEvent
+    appender.doAppend(event)
+    assertEquals(1, appender.logEvents.size)
+  }
+
+  val rnd = new SecureRandom()
+  val category = Logger.getLogger("Logger")
+  val level = Level.INFO
+  def newLoggingEvent = {
+    val e = new LoggingEvent("myclass", category, level, "msg", null)
+    new PaxLoggingEventImpl(e)
+  }
+}
