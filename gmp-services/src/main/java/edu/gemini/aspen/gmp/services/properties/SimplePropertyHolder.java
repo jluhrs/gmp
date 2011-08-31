8f1b36a6cfb7dff872e24c8fc757b940dfaec044
nbarriga <nbarriga@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-08-31 15:41:10 +0000

    GIAPI-495 GIAPI-695: Created PropertyHolder service that gets its properties via FileInstall and can be configured using ConfigAdmin.
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@36980 ee6ba543-07f8-4773-b02c-01d50e9d00ba

---
+++ gmp-services/src/main/java/edu/gemini/aspen/gmp/services/properties/SimplePropertyHolder.java
@@ -0,0 +1,73 @@
+package edu.gemini.aspen.gmp.services.properties;
+
+import edu.gemini.aspen.gmp.services.PropertyHolder;
+import org.apache.felix.ipojo.annotations.*;
+import org.osgi.service.cm.ConfigurationException;
+import org.osgi.service.cm.ManagedService;
+
+import java.util.Dictionary;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * A property holder that will get the properties
+ * directly from OSGi bundle configuration.
+ */
+@Component
+@Provides
+public class SimplePropertyHolder implements PropertyHolder, ManagedService {
+
+    @Property(name = "GMP_HOST_NAME", value = "NO_VALID", mandatory = true)
+    public void setGMP_HOST_NAME(String GMP_HOST_NAME) {
+        _properties.put("GMP_HOST_NAME", GMP_HOST_NAME);
+    }
+
+    @Property(name = "DHS_ANCILLARY_DATA_PATH", value = "/NO_VALID", mandatory = true)
+    public void setDHS_ANCILLARY_DATA_PATH(String DHS_ANCILLARY_DATA_PATH) {
+        _properties.put("DHS_ANCILLARY_DATA_PATH", DHS_ANCILLARY_DATA_PATH);
+    }
+
+    @Property(name = "DHS_SCIENCE_DATA_PATH", value = "/NO_VALID", mandatory = true)
+    public void setDHS_SCIENCE_DATA_PATH(String DHS_SCIENCE_DATA_PATH) {
+        _properties.put("DHS_SCIENCE_DATA_PATH", DHS_SCIENCE_DATA_PATH);
+    }
+
+    @Property(name = "DHS_INTERMEDIATE_DATA_PATH", value = "/NO_VALID", mandatory = true)
+    public void setDHS_INTERMEDIATE_DATA_PATH(String DHS_INTERMEDIATE_DATA_PATH) {
+        _properties.put("DHS_INTERMEDIATE_DATA_PATH", DHS_INTERMEDIATE_DATA_PATH);
+    }
+
+    @Property(name = "DEFAULT", value = "NO_VALID", mandatory = true)
+    public void setDEFAULT(String DEFAULT) {
+        _properties.put("DEFAULT", DEFAULT);
+    }
+
+
+    private Map<String, String> _properties = new HashMap<String, String>();
+
+
+    @Validate
+    public void validate() {
+
+    }
+
+    @Invalidate
+    public void inValidate() {
+
+    }
+
+    @Override
+    public void updated(Dictionary dictionary) throws ConfigurationException {
+        Dictionary<String, String> dict = dictionary;
+        Enumeration<String> elems = dict.keys();
+        for (String property = elems.nextElement(); elems.hasMoreElements(); property = elems.nextElement()) {
+            _properties.put(property, dict.get(property));
+        }
+    }
+
+    @Override
+    public String getProperty(String key) {
+        return _properties.get(key);
+    }
+}
---
+++ integration-tests/src/test/java/edu/gemini/aspen/integrationtests/SimplePropertyHolderIT.java
@@ -0,0 +1,95 @@
+package edu.gemini.aspen.integrationtests;
+
+import edu.gemini.aspen.gmp.services.PropertyHolder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.Configuration;
+import org.ops4j.pax.exam.junit.JUnit4TestRunner;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.InvalidSyntaxException;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.cm.ConfigurationAdmin;
+import org.osgi.service.cm.ManagedService;
+
+import java.io.IOException;
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+import static org.junit.Assert.*;
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+import static org.ops4j.pax.exam.CoreOptions.options;
+import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.vmOption;
+
+/**
+ * Class SimplePropertyHolderIT
+ *
+ * @author Nicolas A. Barriga
+ *         Date: 8/31/11
+ */
+@RunWith(JUnit4TestRunner.class)
+public class SimplePropertyHolderIT extends FelixContainerConfigurationBase {
+    @Configuration
+    public static Option[] withJMSProviderConfig() {
+        return options(
+                vmOption("-Xverify:none "),
+                mavenBundle().artifactId("giapi").groupId("edu.gemini.aspen").versionAsInProject(),
+                mavenBundle().artifactId("jms-api").groupId("edu.gemini.jms").versionAsInProject(),
+                mavenBundle().artifactId("com.springsource.org.dom4j").groupId("org.dom4j").versionAsInProject(),
+                mavenBundle().artifactId("com.springsource.javax.xml.stream").groupId("javax.xml.stream").versionAsInProject(),
+                mavenBundle().artifactId("gmp-services").groupId("edu.gemini.aspen.gmp").update().versionAsInProject()
+        );
+    }
+
+    @Override
+    protected String confDir() {
+        return "/src/test/resources/conf/propertyholder";
+    }
+
+    @Test
+    public void bundleExistence() {
+        assertNotNull(getBundle("edu.gemini.aspen.giapi"));
+        assertNotNull(getBundle("com.springsource.org.dom4j"));
+        assertNotNull(getBundle("com.springsource.javax.xml.stream"));
+        assertNotNull(getBundle("com.springsource.javax.jms"));
+        assertNotNull(getBundle("edu.gemini.jms-api"));
+        assertNotNull(getBundle("edu.gemini.aspen.gmp.services"));
+    }
+
+
+    @Test
+    public void testService() {
+        Bundle bundle = getBundle("edu.gemini.aspen.gmp.services");
+        assertNotNull(context.getServiceReference(PropertyHolder.class.getName()));
+        assertNotNull(context.getServiceReference(ManagedService.class.getName()));
+    }
+
+    @Test
+    public void testProperties() {
+        assertEquals("localhost", ((PropertyHolder) context.getService(context.getServiceReference(PropertyHolder.class.getName()))).getProperty("GMP_HOST_NAME"));
+        assertEquals("/tmp", ((PropertyHolder) context.getService(context.getServiceReference(PropertyHolder.class.getName()))).getProperty("DHS_ANCILLARY_DATA_PATH"));
+        assertEquals("/tmp", ((PropertyHolder) context.getService(context.getServiceReference(PropertyHolder.class.getName()))).getProperty("DHS_SCIENCE_DATA_PATH"));
+        assertEquals("/tmp", ((PropertyHolder) context.getService(context.getServiceReference(PropertyHolder.class.getName()))).getProperty("DHS_INTERMEDIATE_DATA_PATH"));
+        assertEquals("", ((PropertyHolder) context.getService(context.getServiceReference(PropertyHolder.class.getName()))).getProperty("DEFAULT"));
+    }
+
+    @Test
+    public void testUpdateProperties() throws IOException, InvalidSyntaxException, InterruptedException {
+        //use config admin to change properties and then check
+        ConfigurationAdmin ca = (ConfigurationAdmin) context.getService(context.getServiceReference(ConfigurationAdmin.class.getName()));
+        org.osgi.service.cm.Configuration config = ca.listConfigurations("(service.factorypid=edu.gemini.aspen.gmp.services.properties.SimplePropertyHolder)")[0];
+
+        Dictionary props = config.getProperties();
+        if (props == null) {
+            props = new Hashtable();
+        }
+        // configure the Dictionary
+        props.put("GMP_HOST_NAME", "bla");
+        //push the configuration dictionary to the SmsService
+        config.update(props);
+
+        Thread.sleep(1000);
+        assertEquals("bla", ((PropertyHolder) context.getService(context.getServiceReference(PropertyHolder.class.getName()))).getProperty("GMP_HOST_NAME"));
+
+    }
+}
