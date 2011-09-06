d9cd3600696d350c4974ccedc5e8cde2f521acd5
nbarriga <nbarriga@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-09-06 20:23:22 +0000

    GIAPI-689: Added test where factory generates no actors.
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@37165 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- gds-actors-composer/src/test/scala/edu/gemini/aspen/gds/actors/KeywordSetComposerTest.scala
+++ gds-actors-composer/src/test/scala/edu/gemini/aspen/gds/actors/KeywordSetComposerTest.scala
@@ -3,11 +3,15 @@ package edu.gemini.aspen.gds.actors
 import org.junit.Test
 import org.junit.Assert._
 import edu.gemini.aspen.gds.keywords.database.impl.KeywordsDatabaseImpl
-import edu.gemini.aspen.giapi.data.{ObservationEvent, DataLabel}
+import org.mockito.Mockito._
+import org.mockito.Matchers._
+import edu.gemini.aspen.gds.api.{CollectedValue, KeywordValueActor, KeywordActorsFactory}
+import edu.gemini.aspen.gds.api.CollectedValue.apply
+import edu.gemini.aspen.giapi.data.{FitsKeyword, ObservationEvent, DataLabel}
 
 class KeywordSetComposerTest {
   @Test
-  def startAcqMessage {
+  def startAcqMessage() {
     val (dataLabel, composer) = createFixture
 
     // Send an init message
@@ -23,7 +27,7 @@ class KeywordSetComposerTest {
   }
 
   @Test
-  def endAcqMessage {
+  def endAcqMessage() {
     val (dataLabel, composer) = createFixture
 
     // Send an init message
@@ -33,6 +37,30 @@ class KeywordSetComposerTest {
       case AcquisitionRequestReply(obsEvent, replyDataSet) => {
         assertEquals(replyDataSet, dataLabel)
         assertEquals(obsEvent, ObservationEvent.OBS_END_ACQ)
+      }
+      case _ => fail("Should not reply other message")
+    }
+  }
+
+  @Test
+  def testNoActors() {
+    // Generate dataset
+    val dataLabel = new DataLabel("GS-2011")
+
+    val noActorsFactory = mock(classOf[KeywordActorsFactory])
+    when(noActorsFactory.buildActors(ObservationEvent.OBS_END_READOUT, dataLabel)).thenReturn(Nil)
+
+    val keywordsDatabase = new KeywordsDatabaseImpl()
+    // Create composer
+    val composer = new KeywordSetComposer(noActorsFactory, keywordsDatabase)
+
+    // Send an init message
+    val result = composer !! AcquisitionRequest(ObservationEvent.OBS_END_READOUT, dataLabel)
+
+    result() match {
+      case AcquisitionRequestReply(obsEvent, replyDataSet) => {
+        assertEquals(replyDataSet, dataLabel)
+        assertEquals(obsEvent, ObservationEvent.OBS_END_READOUT)
       }
       case _ => fail("Should not reply other message")
     }
