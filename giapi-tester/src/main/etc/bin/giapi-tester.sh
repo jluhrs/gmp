84ccfbb7ec38bba9bd59858228a0f605d4e90119
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-11-08 13:26:40 +0000

    Improved pom, startup script and usage text. The jar with dependencies is now renamed to giapi-tester.jar to be consistent with the help command
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@38452 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- giapi-tester/src/main/etc/bin/giapi-tester.sh
+++ giapi-tester/src/main/etc/bin/giapi-tester.sh
@@ -23,9 +23,7 @@ ABSPATH=$(cd ${0%/*} && echo $PWD/${0##*/})
 # to get the path only - not the script name - add
 SCRIPT_PATH=`dirname "$ABSPATH"`
 
-GIAPI_TESTER_VERSION=0.1.0
-
-GIAPI_TESTER_JAR=$SCRIPT_PATH/giapi-tester-$GIAPI_TESTER_VERSION-jar-with-dependencies.jar 
+GIAPI_TESTER_JAR=$SCRIPT_PATH/giapi-tester.jar
 
 if ! [ -e $GIAPI_TESTER_JAR ]; then
     echo "Giapi tester jar file not found, review your installation"
