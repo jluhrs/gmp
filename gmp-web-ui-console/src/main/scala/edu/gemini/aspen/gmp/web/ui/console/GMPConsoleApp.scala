2e1a28aaa35e58c0ed4b23103248a1b528623537
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-11-16 18:29:40 +0000

    Added link to gogo runtime
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@38747 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- gmp-web-ui-console/src/main/scala/edu/gemini/aspen/gmp/web/ui/console/GMPConsoleApp.scala
+++ gmp-web-ui-console/src/main/scala/edu/gemini/aspen/gmp/web/ui/console/GMPConsoleApp.scala
@@ -3,45 +3,26 @@ package edu.gemini.aspen.gmp.web.ui.console
 import com.vaadin.Application;
 import java.util.logging.Logger
 
-import com.vaadin.ui.themes.BaseTheme
-import com.vaadin.ui.TabSheet.SelectedTabChangeListener
-import java.util.{Calendar, Date}
 import scala.collection.JavaConversions._
-import collection.mutable.LinkedHashSet
-import java.io.Serializable
-import com.vaadin.terminal.{Sizeable, ClassResource}
-import org.apache.felix.ipojo.annotations.{Invalidate, Requires, Bind, Unbind}
-import com.vaadin.data.util.{HierarchicalContainer, ObjectProperty}
-import com.vaadin.ui._
-import java.lang.{String, Boolean}
+import com.vaadin.terminal.Sizeable
 import com.vaadin.ui.Button.ClickListener
-import com.vaadin.data.Item
 import org.vaadin.console.Console
+import org.apache.felix.service.command.CommandProcessor
+import java.io.{PrintStream, ByteArrayOutputStream, ByteArrayInputStream}
+import org.apache.felix.gogo.runtime.CommandProcessorImpl
+import java.util.{Set, Date}
+import org.vaadin.console.Console.{Command, Handler}
+import org.apache.felix.ipojo.annotations.{Bind, Component, Requires, Invalidate}
+import org.osgi.framework.ServiceReference
+import com.vaadin.ui._
 
 /**
  * Main page of the GDS web UI
  */
-@org.apache.felix.ipojo.annotations.Component(name = "GMPConsoleApp")
-class GMPConsoleApp extends Application {
+@Component(name = "GMPConsoleApp")
+class GMPConsoleApp(@Requires commandProcessor:CommandProcessor) extends Application {
   private val LOG = Logger.getLogger(this.getClass.getName)
-  val STREHL = "Strehl"
-  val R0 = "R0"
-  val NAME_PROPERTY = "NAME"
-  val VALUE_PROPERTY = "VALUE"
-  val GRAPH_PROPERTY = "GRAPH"
-
-  val AOC_STATS = "AOC Statistics"
-
   val mainWindow = new Window("GMP Console")
-    // Add Table columns
-    val dataSource = new HierarchicalContainer
-
-    dataSource.addContainerProperty(NAME_PROPERTY, classOf[String], "")
-    dataSource.addContainerProperty(VALUE_PROPERTY, classOf[String], "")
-    dataSource.addContainerProperty(GRAPH_PROPERTY, classOf[CheckBox], null)
-    val aocStatistics = dataSource.addItem(AOC_STATS)
-    val aocStatR0 = dataSource.addItem(R0)
-    val aocStatStrehl = dataSource.addItem(STREHL)
 
   /**
    * Called by Vaadin when the application needs to start
@@ -50,83 +31,61 @@ class GMPConsoleApp extends Application {
    */
   override def init() {
     LOG.info("GMPConsoleApp init> ")
-    val dataPanelLayout = new VerticalLayout
-
-    val mainLayout = new HorizontalSplitPanel
-    mainLayout.setSplitPosition(25, Sizeable.UNITS_PERCENTAGE)
-
-    val treetable = new TreeTable()
-    treetable.setSizeFull()
-    aocStatStrehl.getItemProperty(VALUE_PROPERTY).setValue("0.00")
-    aocStatStrehl.getItemProperty(NAME_PROPERTY).setValue(STREHL)
-    val strehlCheck = new CheckBox("Shown")
-    strehlCheck.setValue(true)
-    strehlCheck.addListener(new ClickListener {
-      def buttonClick(e: Button#ClickEvent) {
-        if (strehlCheck.getValue == false) {
-          strehlCheck.setCaption("Hidden")
-        } else {
-          strehlCheck.setCaption("Shown")
-        }
-      }
-    })
-    aocStatStrehl.getItemProperty(GRAPH_PROPERTY).setValue(strehlCheck)
-
-    aocStatR0.getItemProperty(VALUE_PROPERTY).setValue("0.00")
-    aocStatR0.getItemProperty(NAME_PROPERTY).setValue(R0)
-    val r0Check = new CheckBox("Hidden")
-    r0Check.setValue(false)
-    r0Check.addListener(new ClickListener {
-      def buttonClick(e: Button#ClickEvent) {
-        if (r0Check.getValue == false) {
-          r0Check.setCaption("Hidden")
-        } else {
-          r0Check.setCaption("Shown")
-        }
-      }
-    })
-
-    aocStatR0.getItemProperty(GRAPH_PROPERTY).setValue(r0Check)
-
-    aocStatistics.getItemProperty(NAME_PROPERTY).setValue(AOC_STATS)
-
-    dataSource.setParent(STREHL, AOC_STATS)
-    dataSource.setParent(R0, AOC_STATS)
-    dataSource.setChildrenAllowed(STREHL, false)
-    dataSource.setChildrenAllowed(R0, false)
-    
-    treetable.setContainerDataSource(dataSource)
-
-    dataPanelLayout.addComponent(treetable)
-    treetable.setCollapsed(AOC_STATS, false)
-
-    mainLayout.setFirstComponent(dataPanelLayout)
     val console = new Console
-    console.setSizeFull()
-    console.setPs("gmp >")
     console.setGreeting("Welcome to the GMP/Felix console")
-    val dateCommand = new Console.Command() {
+    console.setPs("gmp> ")
+    val in =  new ByteArrayInputStream(Array[Byte]())
+    val bout = new ByteArrayOutputStream
+    val out = new PrintStream(bout)
+    val err =  new PrintStream(new ByteArrayOutputStream)
+    val cp = commandProcessor.createSession(in, out, err)
+    val handler = new Handler {
+      def commandNotFound(console: Console, commands: Array[String]) {println("brt found")}
 
-                override def getUsage(console:Console, argv:Array[String]) = {
-                    argv(0) + ": prints the current date and time."
-                }
+      def inputReceived(console: Console, text: String) = {
+        println("execute " + text)
+        cp.execute(text)
+        console.print(new String(bout.toByteArray))
+        bout.reset()
+        console.newLine()
+        console.prompt()
+        console.scrollToEnd()
+        true
+      }
 
-                override def execute(console:Console, argv:Array[String]) {
-                    return ""+new Date()
-                }
-            }
+      def handleException(p1: Console, p2: Exception, p3: Command, p4: Array[String]) {println("exc")}
 
-    mainLayout.setSecondComponent(console)
+      def getSuggestions(p1: Console, p2: String): Set[String] = null
+    }
+    //console.setHandler(handler)
+    //console.reset()
+    console.focus()
+    console.setWrap(true)
+    console.setSizeFull()
+    console.setRows(40)
 
-    mainLayout.setSizeFull
+    val mainLayout = new VerticalLayout
+    val label = new Label("GMP/Felix Console")
+    val help = new Button("Help")
+    val mainPanel = new Panel
+    //mainPanel.addComponent(mainLayout)
+    //mainLayout.addComponent(label)
+    //mainLayout.addComponent(console)
+    //mainLayout.setExpandRatio(console, 1.0f)
 
-    mainWindow.setContent(mainLayout)
-    mainWindow.setCaption("AO Performance")
+    mainPanel.setSizeFull()
+    mainLayout.setSizeFull()
+    mainLayout.setMargin(true)
+
+    mainWindow.addComponent(label)
+    mainWindow.addComponent(console)
+    mainWindow.addComponent(help)
+    mainWindow.setCaption("GMP Console")
     setMainWindow(mainWindow)
-    //splineThread.start()
   }
 
-
+  @Bind(optional = true, aggregate = true)
+  def bindServiceCommand() {println("o.p")}
 
   @Invalidate
   def stopListening() {
