94d93ce5489d755f2e2009847b052eb259f5045c
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-11-18 20:47:18 +0000

    Added tests for all unit conversions
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@38819 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- giapi-vaadin-common/src/test/scala/edu/gemini/aspen/giapi/web/ui/vaadin/UnitsTest.scala
+++ giapi-vaadin-common/src/test/scala/edu/gemini/aspen/giapi/web/ui/vaadin/UnitsTest.scala
@@ -9,5 +9,39 @@ import org.scalatest.junit.JUnitRunner
 class UnitsTest extends FunSuite {
   test("Percent conversions") {
     assertEquals("100%", 100 percent)
+    assertEquals("100%", 100 pct)
   }
+
+  test("Pixel conversions") {
+    assertEquals("100px", 100 px)
+  }
+
+  test("em conversions") {
+    assertEquals("100em", 100 em)
+  }
+
+  test("ex conversions") {
+    assertEquals("100ex", 100 ex)
+  }
+
+  test("inch conversions") {
+    assertEquals("100in", 100 in)
+  }
+
+  test("cm conversions") {
+    assertEquals("100cm", 100 cm)
+  }
+
+  test("mm conversions") {
+    assertEquals("100mm", 100 mm)
+  }
+
+  test("pt conversions") {
+    assertEquals("100pt", 100 pt)
+  }
+
+  test("pc conversions") {
+    assertEquals("100pc", 100 pc)
+  }
+
 }
