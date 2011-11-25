64d4e16d31cf60997be4eecbfec4ceee684afed2
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-11-25 22:31:02 +0000

    Fixed guava dependecy for integration test
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@39405 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- jms-activemq-provider/src/test/java/edu/gemini/jms/activemq/provider/ActiveMQJMSProviderIT.java
+++ jms-activemq-provider/src/test/java/edu/gemini/jms/activemq/provider/ActiveMQJMSProviderIT.java
@@ -53,7 +53,7 @@ public class ActiveMQJMSProviderIT {
                 mavenBundle().artifactId("org.apache.felix.fileinstall").groupId("org.apache.felix").version("3.1.10"),
                 mavenBundle().artifactId("pax-logging-api").groupId("org.ops4j.pax.logging").version("1.6.0"),
                 mavenBundle().artifactId("pax-logging-service").groupId("org.ops4j.pax.logging").version("1.6.0"),
-                mavenBundle().artifactId("guava").groupId("com.google.guava").version("8.0.0"),
+                mavenBundle().artifactId("guava-osgi").groupId("com.googlecode.guava-osgi").versionAsInProject(),
                 mavenBundle().artifactId("jms-activemq-provider").groupId("edu.gemini.jms").version("1.1.0").update()
         );
     }
