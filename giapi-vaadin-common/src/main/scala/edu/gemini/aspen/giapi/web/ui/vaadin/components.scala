7bb70e6b81199005d74176de709e506804bb8940
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-11-25 21:09:45 +0000

    Added wrapper for Button class
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@39397 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- giapi-vaadin-common/src/main/scala/edu/gemini/aspen/giapi/web/ui/vaadin/components.scala
+++ giapi-vaadin-common/src/main/scala/edu/gemini/aspen/giapi/web/ui/vaadin/components.scala
@@ -1,15 +1,30 @@
 package edu.gemini.aspen.giapi.web.ui.vaadin
 
 import com.vaadin.data.Property
+import com.vaadin.terminal.Resource
 
 package components {
 
-  class Label(content: String = null, caption: String = null, contentMode: Int = com.vaadin.ui.Label.CONTENT_DEFAULT, style: String = null, property: Property = null) extends com.vaadin.ui.Label(content, contentMode) {
-    setStyleName(style)
-    setCaption(caption)
-    if (Option(property).isDefined) {
-      setPropertyDataSource(property)
-    }
+/**
+ * Scala wrapper for com.vaadin.ui.Label */
+class Label(content: String = null, caption: String = null, contentMode: Int = com.vaadin.ui.Label.CONTENT_DEFAULT, style: String = null, property: Property = null) extends com.vaadin.ui.Label(content, contentMode) {
+  setStyleName(style)
+  setCaption(caption)
+  Option(property) map setPropertyDataSource
+}
+
+class Button(caption: String = null, action: com.vaadin.ui.Button#ClickEvent => Unit = null, icon: Resource = null, style: String = null, enabled: Boolean = true) extends com.vaadin.ui.Button(caption) {
+  setIcon(icon)
+  setStyleName(style)
+  setEnabled(enabled)
+
+  Option(action) map addListener
+
+  protected[vaadin] class ButtonClickListener(action: com.vaadin.ui.Button#ClickEvent => Unit) extends com.vaadin.ui.Button.ClickListener {
+    def buttonClick(event: com.vaadin.ui.Button#ClickEvent) = action(event)
   }
+
+  def addListener(action: com.vaadin.ui.Button#ClickEvent => Unit): Unit = addListener(new ButtonClickListener(action))
+}
 
 }
--- giapi-vaadin-common/src/test/scala/edu/gemini/aspen/giapi/web/ui/vaadin/ScalaComponentsTest.scala
+++ giapi-vaadin-common/src/test/scala/edu/gemini/aspen/giapi/web/ui/vaadin/ScalaComponentsTest.scala
@@ -6,20 +6,45 @@ import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import com.vaadin.data.util.ObjectProperty
 import edu.gemini.aspen.giapi.web.ui.vaadin.components._
+import com.vaadin.terminal.ThemeResource
+import com.vaadin.event.MouseEvents.ClickEvent
 
 @RunWith(classOf[JUnitRunner])
 class ScalaComponentsTest extends FunSuite {
   test("label component construction") {
-    var l = new Label(caption="Caption")
+    var l = new Label(caption = "Caption")
     assertEquals("Caption", l.getCaption)
 
-    l = new Label(style="style")
+    l = new Label(style = "style")
     assertEquals("style", l.getStyleName)
 
-    l = new Label(content="Content")
+    l = new Label(content = "Content")
     assertEquals("Content", l.getPropertyDataSource.getValue)
 
-    l = new Label(property=new ObjectProperty("Content"))
+    l = new Label(property = new ObjectProperty("Content"))
     assertEquals("Content", l.getPropertyDataSource.getValue)
+  }
+
+  test("button component construction") {
+    var b = new Button(caption = "Caption")
+    assertEquals("Caption", b.getCaption)
+
+    b = new Button(style = "style")
+    assertEquals("style", b.getStyleName)
+
+    b = new Button(enabled = false)
+    assertFalse(b.isEnabled)
+
+    val icon = new ThemeResource("a.png")
+    b = new Button(icon = icon)
+    assertEquals(icon, b.getIcon)
+  }
+
+  test("button component action") {
+    var b = new Button(action = _ => {
+    })
+    println(b.getListeners(classOf[Button#ButtonClickListener]))
+    println(b.getListeners(classOf[ClickEvent]))
+   // assertEquals(1, b.getListeners(classOf[Button#ButtonClickListener]).size)
   }
 }
