1103ee8199ca5da8b77de62394af329609a136d9
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-12-02 22:22:40 +0000

    Small cleanups
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@39550 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- gds-web-ui-logs/src/main/scala/edu/gemini/aspen/gds/web/ui/logs/model/LoggingEventBeanQuery.scala
+++ gds-web-ui-logs/src/main/scala/edu/gemini/aspen/gds/web/ui/logs/model/LoggingEventBeanQuery.scala
@@ -13,38 +13,23 @@ import org.joda.time.DateTimeZone
  * The BeanQuery in this case is read only */
 class LoggingEventBeanQuery(queryDefinition0: QueryDefinition, queryConfiguration: java.util.Map[String, Object], sortPropertyIds: Array[Object], sortStates: Array[Boolean]) extends AbstractBeanQuery[LogEventWrapper](queryDefinition0, queryConfiguration, sortPropertyIds, sortStates) {
   val LOG = Logger.getLogger(this.getClass.getName)
-  val sortingFunctions = Map[String, (LogEventWrapper) => String](
-    "timeStamp" -> {
-      _.timeStamp
-    },
-    "level" -> {
-      _.level.toString
-    },
-    "loggerName" -> {
-      _.loggerName
-    },
-    "message" -> {
-      _.message
-    }
-  )
 
-  val queryDefinition = queryDefinition0 match {
-    case q: LogSourceQueryDefinition => q
+  val logSource = queryDefinition0 match {
+    case q: LogSourceQueryDefinition => q.logSource
     case _ => sys.error("Should not happen")
   }
-  val logSource = queryDefinition.logSource
 
   override def saveBeans(p1: List[LogEventWrapper], p2: List[LogEventWrapper], p3: List[LogEventWrapper]) {
     throw new UnsupportedOperationException()
   }
 
   override def loadBeans(startIndex: Int, count: Int) = {
-    val result = filteredLogs.drop(startIndex - 1).take(count)
+    val result = filteredLogs drop(startIndex - 1) take(count) toList
 
-    val sortableCol = if (!sortPropertyIds.isEmpty) sortPropertyIds(0).toString else "timeStamp"
-    val ascending = if (!sortStates.isEmpty) sortStates(0) else true
+    val sortProperties = sortPropertyIds.headOption.getOrElse("timeStamp").toString
+    val ascending = sortStates.headOption.getOrElse(true)
 
-    val sortedLog = result.toList sortBy sortingFunctions(sortableCol)
+    val sortedLog = result sortBy LoggingEventBeanQuery.sortingFunctions(sortProperties)
 
     if (ascending) {
       sortedLog
@@ -63,13 +48,28 @@ class LoggingEventBeanQuery(queryDefinition0: QueryDefinition, queryConfiguratio
 object LoggingEventBeanQuery {
   val timeStampFormatter = ISODateTimeFormat.dateTime().withZone(DateTimeZone.UTC)
   val MAX_MESSAGE_LENGTH = 150
+  /**Functions used to sort the logs based on different properties */
+  val sortingFunctions = Map[String, (LogEventWrapper) => String](
+    "timeStamp" -> {
+      _.timeStamp
+    },
+    "level" -> {
+      _.level.toString
+    },
+    "loggerName" -> {
+      _.loggerName
+    },
+    "message" -> {
+      _.message
+    }
+  )
 
-  /** Formats the timestamp output */
+  /**Formats the timestamp output */
   def formatTimeStamp(timeStamp: Long) = timeStampFormatter.print(timeStamp)
 
-  /** Formats the output of the logger name */
+  /**Formats the output of the logger name */
   def formatLoggerName(loggerName: String) = if (loggerName.contains(".")) loggerName.substring(loggerName.lastIndexOf(".") + 1, loggerName.size) else loggerName
 
-  /** Formats the log message */
+  /**Formats the log message */
   def formatMessage(message: String) = if (message.size > MAX_MESSAGE_LENGTH) message.substring(0, MAX_MESSAGE_LENGTH) + "..." else message
 }
