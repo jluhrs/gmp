d4d55d94cffd7e62e6bbe51bca15272e03626b7a
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-12-09 21:09:04 +0000

    Added test for startJMSProvider
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@39684 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- gmp-status-simulator/src/main/java/edu/gemini/aspen/gmp/status/simulator/StatusSimulator.java
+++ gmp-status-simulator/src/main/java/edu/gemini/aspen/gmp/status/simulator/StatusSimulator.java
@@ -1,10 +1,41 @@
 package edu.gemini.aspen.gmp.status.simulator;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
+import edu.gemini.aspen.giapi.util.jms.status.StatusSetter;
+import edu.gemini.aspen.gmp.status.simulator.generated.StatusType;
+import edu.gemini.jms.api.JmsArtifact;
+import edu.gemini.jms.api.JmsProvider;
+
+import javax.jms.JMSException;
+import java.util.List;
+
 /**
  * Class StatusSimulator creates loops to simulate the value of status items
  */
-public class StatusSimulator {
+public class StatusSimulator implements JmsArtifact {
+    private final List<StatusSetter> statusSetters;
+
     public StatusSimulator(SimulatorConfiguration simulatorConfiguration) {
-        //To change body of created methods use File | Settings | File Templates.
+        List<StatusType> statuses = simulatorConfiguration.getStatuses();
+        List<StatusSetter> setters = Lists.newArrayList();
+        for (StatusType s:statuses) {
+            setters.add(new StatusSetter("StatusSimulator-" + s.getName(), s.getName()));
+        }
+        statusSetters = ImmutableList.copyOf(setters);
+    }
+
+    @Override
+    public void startJms(JmsProvider provider) throws JMSException {
+        for (StatusSetter s:statusSetters) {
+            s.startJms(provider);
+        }
+    }
+
+    @Override
+    public void stopJms() {
+        for (StatusSetter s:statusSetters) {
+            s.stopJms();
+        }
     }
 }
--- gmp-status-simulator/src/test/java/edu/gemini/aspen/gmp/status/simulator/StatusSimulatorTest.java
+++ gmp-status-simulator/src/test/java/edu/gemini/aspen/gmp/status/simulator/StatusSimulatorTest.java
@@ -1,25 +1,77 @@
 package edu.gemini.aspen.gmp.status.simulator;
 
+import edu.gemini.jms.api.JmsProvider;
 import org.junit.Test;
+import org.mockito.Matchers;
 
+import javax.jms.*;
 import javax.xml.bind.JAXBException;
 
 import static org.junit.Assert.assertNotNull;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.any;
 
 public class StatusSimulatorTest {
     private StatusSimulator component;
+    protected ConnectionFactory connectionFactory;
+    protected Session session;
+    protected JmsProvider provider;
+    protected MessageProducer producer;
+    protected MessageConsumer consumer;
+    protected MapMessage mapMessage;
 
     @Test
     public void testCreation() throws InterruptedException, JAXBException {
         component = new StatusSimulator(new SimulatorConfiguration(getClass().getResourceAsStream("status-simulator.xml")));
         assertNotNull(component);
-//        component = new EpicsSimulatorComponent(registrar, configFile.getAbsolutePath());
-//        component.readConfiguration();
-//        component.startSimulation();
-//
-//        // Wait for one update
-//        TimeUnit.MILLISECONDS.sleep(1100L);
-//
-//        verify(registrar, atLeastOnce()).processEpicsUpdate(Matchers.<EpicsUpdate<?>>anyObject());
     }
+
+    @Test
+    public void testStartJMSProvider() throws InterruptedException, JAXBException, JMSException {
+        provider = mock(JmsProvider.class);
+        mockSessionProducerAndConsumer();
+
+        component = new StatusSimulator(new SimulatorConfiguration(getClass().getResourceAsStream("status-simulator.xml")));
+        component.startJms(provider);
+        verify(session.createProducer(any(Destination.class)), times(2));
+    }
+
+    protected void mockSessionProducerAndConsumer() throws JMSException {
+        session = mockSessionCreation();
+
+        producer = mock(MessageProducer.class);
+        when(session.createProducer(Matchers.<Destination>anyObject())).thenReturn(producer);
+        consumer = mock(MessageConsumer.class);
+        when(session.createConsumer(Matchers.<Destination>anyObject())).thenReturn(consumer);
+
+        Queue queue = mock(Queue.class);
+        when(session.createQueue(anyString())).thenReturn(queue);
+
+        Topic topic = mock(Topic.class);
+        when(session.createTopic(anyString())).thenReturn(topic);
+
+        mapMessage = mock(MapMessage.class);
+        when(session.createMapMessage()).thenReturn(mapMessage);
+
+        when(provider.getConnectionFactory()).thenReturn(connectionFactory);
+    }
+
+    private Session mockSessionCreation() throws JMSException {
+        Session session = mock(Session.class);
+        // Mock connection factory
+        connectionFactory = mock(ConnectionFactory.class);
+
+        // Mock connection
+        Connection connection = mock(Connection.class);
+        when(connectionFactory.createConnection()).thenReturn(connection);
+
+        // Mock session
+        when(connection.createSession(Matchers.anyBoolean(), Matchers.anyInt())).thenReturn(session);
+        return session;
+    }
+
 }
