3d99af03ea6f3b671a005006f065bd9b27ede5c1
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-12-09 20:11:12 +0000

    Added test for verifying a channel content
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@39674 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- gmp-status-simulator/src/test/java/edu/gemini/aspen/gmp/status/simulator/SimulatorConfigurationTest.java
+++ gmp-status-simulator/src/test/java/edu/gemini/aspen/gmp/status/simulator/SimulatorConfigurationTest.java
@@ -7,6 +7,7 @@ import javax.xml.bind.JAXBException;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class SimulatorConfigurationTest {
     @Test
@@ -14,5 +15,20 @@ public class SimulatorConfigurationTest {
         SimulatorConfiguration configuration = new SimulatorConfiguration(getClass().getResourceAsStream("status-simulator.xml"));
         List<StatusType> statuses = configuration.getStatuses();
         assertEquals(2, statuses.size());
+    }
+
+    @Test
+    public void testRandomChannel() throws JAXBException {
+        SimulatorConfiguration configuration = new SimulatorConfiguration(getClass().getResourceAsStream("status-simulator.xml"));
+        List<StatusType> statuses = configuration.getStatuses();
+        boolean verified = false;
+        for (StatusType s:statuses) {
+            if (s.getName().equals("test:cc.status1")) {
+                assertEquals("random", s.getMode());
+                assertEquals(0, s.getParameters().getStart().intValue());
+                verified = true;
+            }
+        }
+        assertTrue("Must have found the tested status", verified);
     }
 }
