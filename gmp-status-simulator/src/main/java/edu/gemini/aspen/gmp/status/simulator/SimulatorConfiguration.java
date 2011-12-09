e485020da632e166fedad58c61d0f09175919ad5
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-12-09 20:04:41 +0000

    Added unmarshalling code
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@39673 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- gmp-status-simulator/src/main/java/edu/gemini/aspen/gmp/status/simulator/SimulatorConfiguration.java
+++ gmp-status-simulator/src/main/java/edu/gemini/aspen/gmp/status/simulator/SimulatorConfiguration.java
@@ -1,18 +1,29 @@
 package edu.gemini.aspen.gmp.status.simulator;
 
+import edu.gemini.aspen.gmp.status.simulator.generated.SimulatedStatusesType;
+import edu.gemini.aspen.gmp.status.simulator.generated.StatusType;
+
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
+import javax.xml.transform.stream.StreamSource;
 import java.io.InputStream;
+import java.util.List;
 
 /**
  * Class that defines the configuration of the status simulator
  */
 public class SimulatorConfiguration {
+    private final SimulatedStatusesType simulatedStatuses;
+
     public SimulatorConfiguration(InputStream resourceAsStream) throws JAXBException {
         JAXBContext jaxbContext = JAXBContext.
                 newInstance("edu.gemini.aspen.gmp.status.simulator.generated");
         Unmarshaller u = jaxbContext.createUnmarshaller();
-        System.out.println(u.unmarshal(resourceAsStream));
+        simulatedStatuses = u.unmarshal(new StreamSource(resourceAsStream), SimulatedStatusesType.class).getValue();
+    }
+
+    public List<StatusType> getStatuses() {
+        return simulatedStatuses.getStatus();
     }
 }
--- gmp-status-simulator/src/test/java/edu/gemini/aspen/gmp/status/simulator/SimulatorConfigurationTest.java
+++ gmp-status-simulator/src/test/java/edu/gemini/aspen/gmp/status/simulator/SimulatorConfigurationTest.java
@@ -1,19 +1,18 @@
 package edu.gemini.aspen.gmp.status.simulator;
 
+import edu.gemini.aspen.gmp.status.simulator.generated.StatusType;
 import org.junit.Test;
 
 import javax.xml.bind.JAXBException;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 public class SimulatorConfigurationTest {
     @Test
     public void testSimpleConfiguration() throws JAXBException {
-        System.out.println(getClass().getResourceAsStream("status-simulator.xml"));
         SimulatorConfiguration configuration = new SimulatorConfiguration(getClass().getResourceAsStream("status-simulator.xml"));
-        /*Set<? extends SimulatedEpicsChannel> simulatedChannels = configuration.getSimulatedChannels();
-        assertNotNull(simulatedChannels);
-        assertEquals(2, simulatedChannels.size());
-
-        assertTrue(simulatedChannels.contains(SimulatedEpicsChannel.buildSimulatedEpicsChannel("ws:wsFilter.VALL", 10, DataType.DOUBLE, 1000L)));
-        assertTrue(simulatedChannels.contains(SimulatedEpicsChannel.buildSimulatedEpicsChannel("ws:cpWf", 20, DataType.BYTE, 7000L)));*/
+        List<StatusType> statuses = configuration.getStatuses();
+        assertEquals(2, statuses.size());
     }
 }
