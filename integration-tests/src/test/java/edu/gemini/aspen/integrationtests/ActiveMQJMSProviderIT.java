9b82a5c6cfe3364feca99f13de762de30bb032f1
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2012-02-08 14:30:47 +0000

    Maved integration test to integration-test module
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@41465 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- integration-tests/src/test/java/edu/gemini/aspen/integrationtests/ActiveMQJMSProviderIT.java
+++ integration-tests/src/test/java/edu/gemini/aspen/integrationtests/ActiveMQJMSProviderIT.java
@@ -1,4 +1,4 @@
-package edu.gemini.jms.activemq.provider;
+package edu.gemini.aspen.integrationtests;
 
 import edu.gemini.jms.api.JmsProvider;
 import org.apache.activemq.ActiveMQConnectionFactory;
@@ -16,8 +16,8 @@ import org.osgi.framework.ServiceReference;
 import java.io.IOException;
 
 import static org.junit.Assert.*;
-import static org.ops4j.pax.exam.CoreOptions.*;
-import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.cleanCaches;
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+import static org.ops4j.pax.exam.CoreOptions.options;
 
 /**
  * Basic integration test for JMS Provider basically verifying that the service starts and can be configured
@@ -29,34 +29,25 @@ import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.cleanCaches;
  * @author cquiroz
  */
 @RunWith(JUnit4TestRunner.class)
-public class ActiveMQJMSProviderIT {
+public class ActiveMQJMSProviderIT extends FelixContainerConfigurationBase {
     @Inject
     private BundleContext context;
 
     @Configuration
     public static Option[] baseConfig() {
         return options(
-                felix(),
-                cleanCaches(),
-                systemProperty("felix.fileinstall.dir").value(System.getProperty("basedir") + "/src/test/resources/conf/services"),
-                systemProperty("felix.fileinstall.noInitialDelay").value("true"),
-                mavenBundle().artifactId("org.apache.felix.ipojo").groupId("org.apache.felix").versionAsInProject(),
-                mavenBundle().artifactId("com.springsource.javax.jms").groupId("javax.jms").versionAsInProject(),
-                mavenBundle().artifactId("org.osgi.compendium").groupId("org.osgi").version("4.2.0"),
-                mavenBundle().artifactId("activemq-core").groupId("org.apache.activemq").version("5.4.2"),
-                mavenBundle().artifactId("geronimo-j2ee-management_1.1_spec").groupId("org.apache.geronimo.specs").version("1.0.1"),
-                mavenBundle().artifactId("kahadb").groupId("org.apache.activemq").version("5.4.2"),
-                mavenBundle().artifactId("geronimo-annotation_1.0_spec").groupId("org.apache.geronimo.specs").version("1.1.1"),
-                mavenBundle().artifactId("com.springsource.org.apache.commons.logging").groupId("org.apache.commons").version("1.1.1"),
+                mavenBundle().artifactId("activemq-core").groupId("org.apache.activemq").versionAsInProject(),
+                mavenBundle().artifactId("geronimo-j2ee-management_1.1_spec").groupId("org.apache.geronimo.specs").versionAsInProject(),
+                mavenBundle().artifactId("kahadb").groupId("org.apache.activemq").versionAsInProject(),
+                mavenBundle().artifactId("geronimo-annotation_1.0_spec").groupId("org.apache.geronimo.specs").versionAsInProject(),
                 mavenBundle().artifactId("jms-api").groupId("edu.gemini.jms").versionAsInProject(),
-                mavenBundle().artifactId("org.apache.felix.configadmin").groupId("org.apache.felix").version("1.2.8"),
-                mavenBundle().artifactId("org.apache.felix.fileinstall").groupId("org.apache.felix").version("3.1.10"),
-                mavenBundle().artifactId("pax-logging-api").groupId("org.ops4j.pax.logging").version("1.6.0"),
-                mavenBundle().artifactId("pax-logging-service").groupId("org.ops4j.pax.logging").version("1.6.0"),
-                mavenBundle().artifactId("guava-osgi").groupId("com.googlecode.guava-osgi").versionAsInProject(),
                 mavenBundle().artifactId("eproperties").groupId("edu.gemini.external.osgi.net.jmatrix.eproperties").versionAsInProject(),
                 mavenBundle().artifactId("jms-activemq-provider").groupId("edu.gemini.jms").versionAsInProject()
         );
+    }
+
+    protected String confDir() {
+        return "/src/test/resources/conf/jms-provider";
     }
 
     @Test
