716266a02b710fa9c55e3c95836470974d63e504
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2012-02-27 20:23:37 +0000

    Removed classes depending on log4j api
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@42159 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- gds-static-header-receiver/src/main/scala/edu/gemini/aspen/gds/staticheaderreceiver/HeaderReceiver.scala
+++ gds-static-header-receiver/src/main/scala/edu/gemini/aspen/gds/staticheaderreceiver/HeaderReceiver.scala
@@ -1,42 +1,6 @@
 package edu.gemini.aspen.gds.staticheaderreceiver
 
-import org.apache.felix.ipojo.annotations._
-import edu.gemini.aspen.gds.keywords.database.impl.ProgramIdDatabaseImpl
-import edu.gemini.aspen.gds.keywords.database.ProgramIdDatabase
-
 /**
  * Needed by iPojo
  */
 trait HeaderReceiver
-
-/**
- * Component that starts the XMLRPC server and starts the actor that forwards messages to the appropriate database
- */
-@Component
-//@Instantiate
-@Provides(specifications = Array(classOf[HeaderReceiver]))
-class SeqexecHeaderReceiver(@Requires keywordsDatabase: TemporarySeqexecKeywordsDatabase, @Requires programIdDB: ProgramIdDatabase) extends HeaderReceiver {
-  private val webServer = XmlRpcServerFactory.newServer("HeaderReceiver", classOf[XmlRpcReceiver], 12345)
-
-  @Validate
-  def start() {
-    webServer.start();
-  }
-
-  @Invalidate
-  def shutdown() {
-    webServer.shutdown()
-  }
-
-}
-
-/**
- * Temporary test app
- */
-object TestApp extends App {
-  org.apache.log4j.BasicConfigurator.configure();
-  val seq = new SeqexecHeaderReceiver(new TemporarySeqexecKeywordsDatabaseImpl, new ProgramIdDatabaseImpl)
-  seq.start()
-  Thread.sleep(1000000)
-}
-
