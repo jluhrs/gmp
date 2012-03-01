62d933a9141f4798343ec52c2a42aa61ae9921c0
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2012-03-01 14:07:29 +0000

    GIAPI-904 Minimal working distribution using epics-api
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@42351 ee6ba543-07f8-4773-b02c-01d50e9d00ba

---
+++ distribution/src/main/etc/bin/start.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#==========================
+# GMP start script
+#==========================
+#
+# Usage:
+#   ./start.sh 
+
+# Var setup
+# Don't allow non initialized vars
+set -o nounset
+
+# Exit on failure
+set -e
+
+# Verify dependencies
+#====================
+# Check that java is available
+which java > /dev/null || { echo "Need java in PATH to run"; exit 1; }
+
+# Confirm that jps is available
+which jps > /dev/null || { echo "Need jps in PATH to run"; exit 1; }
+
+# Check no other instance is running
+#===================================
+
+# Check if gmp is already running
+RUNNING=`jps -l | grep "org.apache.felix.main.Main" | wc -l`
+
+if ! [ $RUNNING = "0" ]; then
+    echo "GMP is already running"
+    exit 1
+fi
+
+# Find path to script
+#==========================
+ABSPATH=$(cd ${0%/*} && echo $PWD/${0##*/})
+
+# to get the path only - not the script name - add
+SCRIPT_PATH=`dirname "$ABSPATH"`
+
+echo "Starting GMP version: ${gmp.version}"
+# Will start pax-runner reading the configuration from the file bin/runner.args
+exec java -jar $SCRIPT_PATH/pax-runner-${pax-runner.version}.jar
