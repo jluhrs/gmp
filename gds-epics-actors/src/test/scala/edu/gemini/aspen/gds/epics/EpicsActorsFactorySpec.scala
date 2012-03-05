1c6cc2029d98f7f5f6b286a3404564658dea2233
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2012-03-05 17:53:22 +0000

    Use FunSuite rather than Spec for tests
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@42500 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- gds-epics-actors/src/test/scala/edu/gemini/aspen/gds/epics/EpicsActorsFactorySpec.scala
+++ gds-epics-actors/src/test/scala/edu/gemini/aspen/gds/epics/EpicsActorsFactorySpec.scala
@@ -1,18 +1,18 @@
 package edu.gemini.aspen.gds.epics
 
 import org.junit.runner.RunWith
+import org.junit.Assert._
 import org.scalatest.junit.JUnitRunner
-import org.scalatest.matchers.ShouldMatchers
-import org.scalatest.Spec
 import org.mockito.Mockito._
 import edu.gemini.aspen.gds.api._
 import edu.gemini.aspen.gds.api.Conversions._
 import edu.gemini.aspen.giapi.data.{ObservationEvent, DataLabel}
 import edu.gemini.epics.{ReadOnlyClientEpicsChannel, EpicsReader}
 import org.scalatest.mock.MockitoSugar
+import org.scalatest.FunSuite
 
 @RunWith(classOf[JUnitRunner])
-class EpicsActorsFactorySpec extends Spec with ShouldMatchers with MockitoSugar {
+class EpicsActorsFactorySpec extends FunSuite with MockitoSugar {
   val epicsReader = mock[EpicsReader]
   val channel = mock[ReadOnlyClientEpicsChannel[Double]]
   val channel2 = mock[ReadOnlyClientEpicsChannel[Double]]
@@ -31,65 +31,63 @@ class EpicsActorsFactorySpec extends Spec with ShouldMatchers with MockitoSugar 
     GDSConfiguration("GPI", "OBS_START_ACQ", "AIRMASS", 0, "DOUBLE", false, "NONE", "EPICS", "gpi:value", 0, "Mean airmass for the observation")
   }
 
-  describe("An EpicsActorsFactory") {
-    it("should return an empty list of Actors when not configured") {
-      val (dataLabel, epicsActorsFactory) = createFixture
+  test("should return an empty list of Actors when not configured") {
+    val (dataLabel, epicsActorsFactory) = createFixture
 
-      val actors = epicsActorsFactory.buildActors(ObservationEvent.OBS_START_ACQ, dataLabel)
-      actors should be('empty)
-    }
-    it("should be configurable with one item") {
-      val (dataLabel, epicsActorsFactory) = createFixture
-      val configuration = List(
-        buildOneConfiguration
-      )
-      epicsActorsFactory.configure(configuration)
+    val actors = epicsActorsFactory.buildActors(ObservationEvent.OBS_START_ACQ, dataLabel)
+    assertTrue(actors.isEmpty)
+  }
+  test("should be configurable with one item") {
+    val (dataLabel, epicsActorsFactory) = createFixture
+    val configuration = List(
+      buildOneConfiguration
+    )
+    epicsActorsFactory.configure(configuration)
 
-      val actors = epicsActorsFactory.buildActors(ObservationEvent.OBS_START_ACQ, dataLabel)
-      actors should have length (1)
-    }
-    it("should be configurable with two item for diferent channel") {
-      val (dataLabel, epicsActorsFactory) = createFixture
-      val configuration = List(
-        GDSConfiguration("GPI", "OBS_START_ACQ", "AIRMASS", 0, "DOUBLE", true, "NONE", "EPICS", "gpi:value", 0, "Mean airmass for the observation"),
-        GDSConfiguration("GPI", "OBS_START_ACQ", "AIRMASS2", 0, "DOUBLE", true, "NONE", "EPICS", "gpi:value2", 0, "Mean airmass for the observation"))
-      epicsActorsFactory.configure(configuration)
+    val actors = epicsActorsFactory.buildActors(ObservationEvent.OBS_START_ACQ, dataLabel)
+    assertEquals(1, actors.size)
+  }
+  test("should be configurable with two item for diferent channel") {
+    val (dataLabel, epicsActorsFactory) = createFixture
+    val configuration = List(
+      GDSConfiguration("GPI", "OBS_START_ACQ", "AIRMASS", 0, "DOUBLE", true, "NONE", "EPICS", "gpi:value", 0, "Mean airmass for the observation"),
+      GDSConfiguration("GPI", "OBS_START_ACQ", "AIRMASS2", 0, "DOUBLE", true, "NONE", "EPICS", "gpi:value2", 0, "Mean airmass for the observation"))
+    epicsActorsFactory.configure(configuration)
 
-      val actors = epicsActorsFactory.buildActors(ObservationEvent.OBS_START_ACQ, dataLabel)
-      actors should have length (2)
-    }
-    it("should be configurable with two item for same channel") {
-      val (dataLabel, epicsActorsFactory) = createFixture
-      val configuration = List(
-        GDSConfiguration("GPI", "OBS_START_ACQ", "AIRMASS", 0, "DOUBLE", true, "NONE", "EPICS", "gpi:value", 0, "Mean airmass for the observation"),
-        GDSConfiguration("GPI", "OBS_START_ACQ", "AIRMASS2", 0, "DOUBLE", true, "NONE", "EPICS", "gpi:value", 0, "Mean airmass for the observation"))
-      epicsActorsFactory.configure(configuration)
+    val actors = epicsActorsFactory.buildActors(ObservationEvent.OBS_START_ACQ, dataLabel)
+    assertEquals(2, actors.size)
+  }
+  test("should be configurable with two item for same channel") {
+    val (dataLabel, epicsActorsFactory) = createFixture
+    val configuration = List(
+      GDSConfiguration("GPI", "OBS_START_ACQ", "AIRMASS", 0, "DOUBLE", true, "NONE", "EPICS", "gpi:value", 0, "Mean airmass for the observation"),
+      GDSConfiguration("GPI", "OBS_START_ACQ", "AIRMASS2", 0, "DOUBLE", true, "NONE", "EPICS", "gpi:value", 0, "Mean airmass for the observation"))
+    epicsActorsFactory.configure(configuration)
 
-      val actors = epicsActorsFactory.buildActors(ObservationEvent.OBS_START_ACQ, dataLabel)
-      actors should have length (1)
-    }
-    it("should be configurable with one item for start and one item for end") {
-      val (dataLabel, epicsActorsFactory) = createFixture
-      val configuration = List(
-        GDSConfiguration("GPI", "OBS_START_ACQ", "AIRMASS", 0, "DOUBLE", true, "NONE", "EPICS", "gpi:value", 0, "Mean airmass for the observation"),
-        GDSConfiguration("GPI", "OBS_END_ACQ", "AIRMASS2", 0, "DOUBLE", true, "NONE", "EPICS", "gpi:value", 0, "Mean airmass for the observation"))
-      epicsActorsFactory.configure(configuration)
+    val actors = epicsActorsFactory.buildActors(ObservationEvent.OBS_START_ACQ, dataLabel)
+    assertEquals(1, actors.size)
+  }
+  test("should be configurable with one item for start and one item for end") {
+    val (dataLabel, epicsActorsFactory) = createFixture
+    val configuration = List(
+      GDSConfiguration("GPI", "OBS_START_ACQ", "AIRMASS", 0, "DOUBLE", true, "NONE", "EPICS", "gpi:value", 0, "Mean airmass for the observation"),
+      GDSConfiguration("GPI", "OBS_END_ACQ", "AIRMASS2", 0, "DOUBLE", true, "NONE", "EPICS", "gpi:value", 0, "Mean airmass for the observation"))
+    epicsActorsFactory.configure(configuration)
 
-      val startActors = epicsActorsFactory.buildActors(ObservationEvent.OBS_START_ACQ, dataLabel)
-      startActors should have length (1)
-      val endActors = epicsActorsFactory.buildActors(ObservationEvent.OBS_END_ACQ, dataLabel)
-      endActors should have length (1)
-    }
-    it("should only pick EPICS subsystems") {
-      val (dataLabel, epicsActorsFactory) = createFixture
-      val configuration = List(
-        GDSConfiguration("GPI", "OBS_START_ACQ", "AIRMASS", 0, "DOUBLE", true, "NONE", "EPICS", "gpi:value", 0, "Mean airmass for the observation"),
-        GDSConfiguration("GPI", "OBS_START_ACQ", "AIRMASS2", 0, "DOUBLE", true, "NONE", "STATUS", "gpi:value", 0, "Mean airmass for the observation"))
-      epicsActorsFactory.configure(configuration)
+    val startActors = epicsActorsFactory.buildActors(ObservationEvent.OBS_START_ACQ, dataLabel)
+    assertEquals(1, startActors.size)
+    val endActors = epicsActorsFactory.buildActors(ObservationEvent.OBS_END_ACQ, dataLabel)
+    assertEquals(1, endActors.size)
+  }
+  test("should only pick EPICS subsystems") {
+    val (dataLabel, epicsActorsFactory) = createFixture
+    val configuration = List(
+      GDSConfiguration("GPI", "OBS_START_ACQ", "AIRMASS", 0, "DOUBLE", true, "NONE", "EPICS", "gpi:value", 0, "Mean airmass for the observation"),
+      GDSConfiguration("GPI", "OBS_START_ACQ", "AIRMASS2", 0, "DOUBLE", true, "NONE", "STATUS", "gpi:value", 0, "Mean airmass for the observation"))
+    epicsActorsFactory.configure(configuration)
 
-      val actors = epicsActorsFactory.buildActors(ObservationEvent.OBS_START_ACQ, dataLabel)
-      actors should have length (1)
-    }
+    val actors = epicsActorsFactory.buildActors(ObservationEvent.OBS_START_ACQ, dataLabel)
+    assertEquals(1, actors.size)
   }
 
 }
