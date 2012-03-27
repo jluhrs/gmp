f69d98e4f14c5738116d3359e021b8aaff7d5be6
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2012-03-27 15:45:44 +0000

    GIAPI-731 Now if a tab throws an error a nicer error message is presented #time 30m
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@43644 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- gds-web-ui/src/main/scala/edu/gemini/aspen/gds/web/ui/vaadin/VaadinServlet.scala
+++ gds-web-ui/src/main/scala/edu/gemini/aspen/gds/web/ui/vaadin/VaadinServlet.scala
@@ -13,6 +13,10 @@ import org.apache.felix.ipojo.annotations._
 import java.util.Properties
 import java.util.logging.{Level, Logger}
 import org.ops4j.pax.web.service.WebContainer
+import com.vaadin.ui.Window
+import edu.gemini.aspen.giapi.web.ui.vaadin.layouts.VerticalLayout
+import edu.gemini.aspen.giapi.web.ui.vaadin.components.Label
+import com.vaadin.ui.Window.Notification
 
 /**
  * A Generic Vaadin servlet that can delegate to an iPojo factory the creation of a Vaadin applicat
@@ -20,8 +24,8 @@ import org.ops4j.pax.web.service.WebContainer
  * TODO Abstract away the
  */
 @Component
-@Provides(specifications = Array[Class[_]](classOf[Servlet]))
 @Instantiate
+@Provides(specifications = Array[Class[_]](classOf[Servlet]))
 class VaadinServlet(@Requires(from = "VaadinAppFactory") val vaadinAppFactory: Factory, @Requires val webContainer: WebContainer) extends AbstractApplicationServlet {
   private val LOG = Logger.getLogger(this.getClass.getName)
   @ServiceProperty(name = "alias", value = "/gds")
@@ -37,29 +41,37 @@ class VaadinServlet(@Requires(from = "VaadinAppFactory") val vaadinAppFactory: F
   }
 
   override protected def getNewApplication(request: HttpServletRequest): Application = {
-    val app = try {
+    val app: Either[String, Application] = try {
       val instance = vaadinAppFactory.createComponentInstance(new Properties())
 
       if (instance.getState() == ComponentInstance.VALID) {
         val application = instance.asInstanceOf[InstanceManager].getPojoObject.asInstanceOf[GDSCoreVaadinApp]
         sessions = VaadinSession(request.getSession(), application) :: sessions
-        Option(application)
+        Right(application)
       } else {
-        LOG.severe("Cannot get an implementation object from an invalid instance");
-        None
+        LOG.severe("Cannot get an implementation object from an invalid instance")
+        Left("The requested iPojo instance GDSCoreVaadinApp is invalid, contact the administrator")
       }
     } catch {
       case e: Exception => {
-        LOG.log(Level.SEVERE, "Cannot get an implementation object from an invalid instance", e);
-        None
+        LOG.log(Level.SEVERE, "Cannot get an implementation object from an invalid instance", e)
+        Left("There is an error when burding the GDS Web UT, contact the administrator\nerror: " + e.getMessage + "\n" + e.getStackTraceString)
       }
     }
-    app.get
+    app match {
+      case Right(app) => app
+      case Left(msg) => new Application {
+        def init() {
+          setMainWindow(new LoadingErrorWindow(msg))
+          getMainWindow.showNotification("Error while starting the app", Notification.TYPE_ERROR_MESSAGE)
+        }
+      }
+    }
   }
 
   @Validate
   def validated() {
-    LOG.info("Registered servlet under path " + label);
+    LOG.info("Registered servlet under path " + label)
   }
 
   case class VaadinSession(session: HttpSession, application: Application) {
@@ -67,9 +79,15 @@ class VaadinServlet(@Requires(from = "VaadinAppFactory") val vaadinAppFactory: F
     require(application != null)
 
     def dispose() {
-      application.close();
-      session.invalidate();
+      application.close()
+      session.invalidate()
     }
   }
 
+}
+
+class LoadingErrorWindow(msg:String) extends Window {
+  setContent(new VerticalLayout(sizeFull = true) {
+    add(new Label(msg, contentMode = com.vaadin.ui.Label.CONTENT_PREFORMATTED), ratio = 1f)
+  })
 }
