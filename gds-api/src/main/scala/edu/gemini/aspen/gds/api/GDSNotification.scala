ec3087a35ed7b7fb117d1a08ac53a40934c8aa71
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2012-04-02 13:35:18 +0000

    Rolled back type changes
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@43900 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- gds-api/src/main/scala/edu/gemini/aspen/gds/api/GDSNotification.scala
+++ gds-api/src/main/scala/edu/gemini/aspen/gds/api/GDSNotification.scala
@@ -32,6 +32,5 @@ case class GDSObservationError(dataLabel: DataLabel, msg: String) extends GDSNot
  * Register how long observation stage have taken
  * @param dataLabel The data label of the observation
  * @param times List of the duration of each observation stage
- * @tparam A Type of the observation, has to be a subclass of Traversable
  */
-case class GDSObservationTimes[A <: Traversable[(ObservationEvent, Option[Duration])]](dataLabel: DataLabel, times: A) extends GDSNotification
+case class GDSObservationTimes(dataLabel: DataLabel, times: Traversable[(ObservationEvent, Option[Duration])]) extends GDSNotification
