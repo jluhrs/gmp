2ffab45625e2829eb76f418a9ab294b194cd7f44
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2012-04-02 13:20:34 +0000

    GIAPI-957 Slight change to GDSObservationTimes signature #time 10m
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@43898 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- gds-api/src/main/scala/edu/gemini/aspen/gds/api/GDSNotification.scala
+++ gds-api/src/main/scala/edu/gemini/aspen/gds/api/GDSNotification.scala
@@ -7,13 +7,31 @@ import org.joda.time.Duration
 /**
  * Parent trait of case classes defining notifications or events produced by the GDS */
 sealed trait GDSNotification {
-  val dataLabel:DataLabel
+  val dataLabel: DataLabel
 }
 
-case class GDSStartObservation(dataLabel:DataLabel) extends GDSNotification
+/**
+ * Notification that an observation has started
+ * @param dataLabel The data label of the observation
+ */
+case class GDSStartObservation(dataLabel: DataLabel) extends GDSNotification
 
-case class GDSEndObservation(dataLabel:DataLabel) extends GDSNotification
+/**
+ * Notification that an observation has completed
+ * @param dataLabel The data label of the observation
+ */
+case class GDSEndObservation(dataLabel: DataLabel) extends GDSNotification
 
-case class GDSObservationError(dataLabel:DataLabel, msg:String) extends GDSNotification
+/**
+ * Notification that an observation has stopped with an error
+ * @param dataLabel The data label of the observation
+ */
+case class GDSObservationError(dataLabel: DataLabel, msg: String) extends GDSNotification
 
-case class GDSObservationTimes(dataLabel:DataLabel, times:Traversable[(ObservationEvent, Option[Duration])]) extends GDSNotification
+/**
+ * Register how long observation stage have taken
+ * @param dataLabel The data label of the observation
+ * @param times List of the duration of each observation stage
+ * @tparam A Type of the observation, has to be a subclass of Traversable
+ */
+case class GDSObservationTimes[A <: Traversable[(ObservationEvent, Option[Duration])]](dataLabel: DataLabel, times: A) extends GDSNotification
