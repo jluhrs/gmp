602a8fc1147ddb09f33441b1d86d2b10e0a848f1
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2012-04-03 18:21:58 +0000

    GIAPI-964 Added a simple filter to the GDS Web UI #time 3h
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@43981 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- gds-web-ui-logs/src/main/scala/edu/gemini/aspen/gds/web/ui/logs/LogsModule.scala
+++ gds-web-ui-logs/src/main/scala/edu/gemini/aspen/gds/web/ui/logs/LogsModule.scala
@@ -3,11 +3,16 @@ package edu.gemini.aspen.gds.web.ui.logs
 import java.util.logging.Logger
 import model.{LogsContainer, LogSourceQueryDefinition, LoggingEventBeanQuery}
 import edu.gemini.aspen.gds.web.ui.api.GDSWebModule
+import edu.gemini.aspen.giapi.web.ui.vaadin._
 import edu.gemini.aspen.giapi.web.ui.vaadin.layouts._
 import edu.gemini.aspen.giapi.web.ui.vaadin.selects._
 import com.vaadin.Application
 import org.vaadin.addons.lazyquerycontainer._
-import com.vaadin.ui.Component
+import org.vaadin.henrik.drawer.Drawer
+import com.vaadin.ui.{Alignment, Component}
+import com.vaadin.data.Container
+import com.vaadin.data.util.filter.Compare
+import edu.gemini.aspen.giapi.web.ui.vaadin.components.Label
 
 /**
  * The LogsModule class shows the content of GDS related log files */
@@ -26,9 +31,20 @@ class LogsModule(logSource: LogSource) extends GDSWebModule {
     sortAscending = true,
     sortPropertyId = "timeStamp",
     cellStyleGenerator = styleGenerator)
-
+  val levelCombo = new NativeSelect(nullSelectionAllowed = false, action = changeLevel, immediate = true)
+  levelCombo.addItem("INFO")
+  levelCombo.addItem("WARN")
+  levelCombo.addItem("ERROR")
+  levelCombo.setValue("INFO")
 
   override def buildTabContent(app: Application): Component = new VerticalLayout(sizeFull = true) {
+    val filters = new HorizontalLayout(spacing = true) {
+      add(new Label("Level:"))
+      add(levelCombo)
+    }
+    val drawer = new Drawer("Filters", filters)
+    drawer.setWidth(100 percent)
+    add(drawer)
     add(logTable, ratio = 1.0f)
   }
 
@@ -53,5 +69,11 @@ class LogsModule(logSource: LogSource) extends GDSWebModule {
    * Define a custom cell style based on the content of the cell */
   private def styleGenerator(itemId: AnyRef, propertyId: AnyRef): String = {
     STYLES.getOrElse(dataContainer.getItem(itemId).getItemProperty("level").toString, "")
+  }
+
+  def changeLevel(e:com.vaadin.data.Property.ValueChangeEvent) {
+    dataContainer.removeAllContainerFilters()
+    dataContainer.addContainerFilter(new Compare.Equal("level", e.getProperty.getValue))
+    dataContainer.refresh()
   }
 }
