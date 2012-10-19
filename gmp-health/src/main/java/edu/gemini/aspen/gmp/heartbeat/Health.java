fc45e3e73257ed275fc0d33bbfd92eff7834b0ff
Carlos Quiroz <cquiroz@ketalo.com>
2012-10-19 15:56:14 -0300

    Initial module for gmp-health

---
+++ gmp-health/src/main/java/edu/gemini/aspen/gmp/heartbeat/Health.java
@@ -0,0 +1,35 @@
+package edu.gemini.aspen.gmp.heartbeat;
+
+import edu.gemini.aspen.gmp.top.Top;
+import org.apache.felix.ipojo.annotations.Component;
+import org.apache.felix.ipojo.annotations.Property;
+import org.apache.felix.ipojo.annotations.Provides;
+import org.apache.felix.ipojo.annotations.Requires;
+
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.logging.Logger;
+
+
+/**
+ * Class Health evaluates the health of the GMP
+ */
+@Component
+@Provides
+public class Health {
+    private static final Logger LOG = Logger.getLogger(Health.class.getName());    //private final IStatusSetter heartbeatSetter;
+    private final Top top;
+    private final String heartbeatName;
+
+
+    private ScheduledThreadPoolExecutor executor;
+    private ScheduledFuture future;
+
+    public Health(@Property(name = "healthName", value = "INVALID", mandatory = true) String heartbeatName,
+            @Requires Top top) {
+        LOG.info("Heartbeat Constructor");
+        this.top = top;
+        this.heartbeatName = heartbeatName;
+    }
+
+}
---
+++ gmp-health/src/test/java/edu/gemini/aspen/gmp/heartbeat/HealthTest.java
@@ -0,0 +1,13 @@
+package edu.gemini.aspen.gmp.heartbeat;
+
+import org.junit.Test;
+
+/**
+ * Tests for Health
+ */
+public class HealthTest {
+
+    @Test
+    public void testBasic() throws Exception {
+    }
+}
