62f66b3327f669a6375e487f88fcebe48fb7460f
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2013-10-21 15:09:18 +0000

    GIAPI-1027 Updated version numbers of gds-api, gds-fits-updater and gds-obsevent-handler #time 30m
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@54800 ee6ba543-07f8-4773-b02c-01d50e9d00ba

---
+++ gds-postprocessing-policy/src/test/scala/edu/gemini/aspen/gds/postprocessingpolicy/DeleteOriginalPolicyTest.scala
@@ -0,0 +1,12 @@
+package edu.gemini.aspen.gds.postprocessingpolicy
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: cquiroz
+ * Date: 10/21/13
+ * Time: 11:57 AM
+ * To change this template use File | Settings | File Templates.
+ */
+class DeleteOriginalPolicyTest {
+
+}
