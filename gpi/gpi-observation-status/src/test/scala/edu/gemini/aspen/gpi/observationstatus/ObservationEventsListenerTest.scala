abc228a504942ea10a38ef886f8b71e5b5068b7c
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2014-04-05 15:17:50 +0000

    GPI-217 Created a module to export the label status for GPI #time 1h
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@57838 ee6ba543-07f8-4773-b02c-01d50e9d00ba

---
+++ gpi/gpi-observation-status/src/test/scala/edu/gemini/aspen/gpi/observationstatus/ObservationEventsListenerTest.scala
@@ -0,0 +1,60 @@
+package edu.gemini.aspen.gpi.observationstatus
+
+import org.junit.runner.RunWith
+import org.junit.Assert._
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.FunSuite
+import org.scalatest.mock.MockitoSugar
+import edu.gemini.aspen.gmp.top.Top
+import edu.gemini.aspen.giapi.util.jms.status.IStatusSetter
+import edu.gemini.aspen.giapi.data.DataLabel
+import org.mockito.Mockito._
+import edu.gemini.aspen.gds.api.GDSStartObservation
+import org.mockito.ArgumentCaptor
+import edu.gemini.aspen.giapi.status.StatusItem
+
+@RunWith(classOf[JUnitRunner])
+class ObservationEventsListenerTest extends FunSuite with MockitoSugar {
+  test("export datalabel") {
+    val top = mock[Top]
+    when(top.buildStatusItemName("observationDataLabel")).thenReturn("gpi:observationDataLabel")
+    val setter = mock[IStatusSetter]
+
+    val listener = new ObservationEventsListener(top, setter)
+    listener.gdsEvent(GDSStartObservation(new DataLabel("NEW_LABEL")))
+
+    val argument = ArgumentCaptor.forClass(classOf[StatusItem[String]])
+
+    verify(setter).setStatusItem(argument.capture())
+    assertEquals(argument.getValue.getName, "gpi:observationDataLabel")
+    assertEquals(argument.getValue.getValue, "NEW_LABEL")
+  }
+  test("export datalabel verify label name") {
+    val top = mock[Top]
+    when(top.buildStatusItemName("observationDataLabel")).thenReturn("gpi:observationDataLabel")
+    val setter = mock[IStatusSetter]
+
+    val listener = new ObservationEventsListener(top, setter)
+    listener.gdsEvent(GDSStartObservation(new DataLabel("ANOTHER_LABEL")))
+
+    val argument = ArgumentCaptor.forClass(classOf[StatusItem[String]])
+
+    verify(setter).setStatusItem(argument.capture())
+    assertEquals(argument.getValue.getName, "gpi:observationDataLabel")
+    assertEquals(argument.getValue.getValue, "ANOTHER_LABEL")
+  }
+  test("export datalabel verify channel name") {
+    val top = mock[Top]
+    when(top.buildStatusItemName("observationDataLabel")).thenReturn("gmp:observationDataLabel")
+    val setter = mock[IStatusSetter]
+
+    val listener = new ObservationEventsListener(top, setter)
+    listener.gdsEvent(GDSStartObservation(new DataLabel("ANOTHER_LABEL")))
+
+    val argument = ArgumentCaptor.forClass(classOf[StatusItem[String]])
+
+    verify(setter).setStatusItem(argument.capture())
+    assertEquals(argument.getValue.getName, "gmp:observationDataLabel")
+    assertEquals(argument.getValue.getValue, "ANOTHER_LABEL")
+  }
+}
