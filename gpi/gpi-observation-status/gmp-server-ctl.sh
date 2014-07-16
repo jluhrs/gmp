ebdd462e7091d2e6649276fa35a9200536d84242
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2014-07-16 12:43:05 +0000

    Added GPI specific startup script
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-osgi/trunk@59777 ee6ba543-07f8-4773-b02c-01d50e9d00ba

---
+++ gpi/gpi-observation-status/gmp-server-ctl.sh
@@ -0,0 +1,163 @@
+#!/bin/bash
+#
+# gmp-server control script
+#
+# usage gmp-server-ctl.sh {start|stop|status|kill}
+#
+set -e # stop on errors
+set -u # Don't allow using non defined variables
+
+# Vars that are set at build time
+
+PAX_RUNNER_VERSION=1.8.5
+GMP_VERSION=14.3.9-SNAPSHOT
+
+# Confirm that java is available
+which java > /dev/null || { echo "Need java in PATH to run"; exit 1; }
+
+# Verify existing variables
+if [ -z ${GPI_ROOT:-} ]; then
+    GPI_ROOT=/gemsoft/opt/gpi/
+    echo "GPI_ROOT not set. Using $GPI_ROOT"
+fi
+if ! [ -d $GPI_ROOT ]; then
+    echo "$GPI_ROOT directory not found"
+    exit -1
+fi
+
+# App variables
+app_name=gmp-server
+app_root=${GPI_ROOT}/gmp-server-$GMP_VERSION
+pid_file=${app_root}/bin/${app_name}.pid
+log_dir=${app_root}/logs/
+log_file=${log_dir}/${app_name}.out
+
+LD_LIBRARY_PATH=$GPI_ROOT/giapi-dist/lib
+
+# Check log dirs
+if ! [ -d ${log_dir} ]; then
+    echo "Log dir is missing, creating it at ${log_dir}"
+    mkdir -p ${log_dir}
+fi
+
+# check rpm root is available
+if ! [ -e ${app_root} ]; then
+    echo "gmp-server directory ${app_root} not found. Check your GPI_ROOT env variable"
+    exit -1
+fi
+
+# pax-runner needs this dir to run
+if ! [ -d $HOME/.pax/runner ]; then
+    mkdir -p $HOME/.pax/runner
+    touch $HOME/.pax/runner/org.ops4j.pax.runner.daemon.password.file
+fi
+
+#
+# - get PID from .pid file if it exists and check if it is running
+#
+pid=`[[ -e ${pid_file} ]] && cat ${pid_file} || echo "NO_PID_FILE"`
+set +e # disable because egrep will return 1 on mismatch
+pid_isrunning=`ps -eo pid | egrep ^[[:space:]]*${pid}$`
+# Check pax-runner lock is not there
+if [ -z ${pid_isrunning} ] && [ -e $HOME/.pax/runner/org.ops4j.pax.runner.daemon.lock ]; then
+    echo "Seems GMP is not running but the lock file is still in place"
+    echo "If you are sure GMP is not running delete $HOME/.pax/runner/org.ops4j.pax.runner.daemon.lock"
+fi
+
+set -e # reenable
+
+#
+# Start the GMP server if not already running
+#
+function startContainer() {
+    if [ -z ${pid_isrunning} ]; then
+        echo "Starting ${app_name} version $GMP_VERSION"
+        # Will start pax-runner as a daemon reading the configuration from the file bin/runner.args
+        pushd ${app_root}/bin > /dev/null;
+        java -cp ${app_root}/bin/pax-runner-${PAX_RUNNER_VERSION}.jar org.ops4j.pax.runner.daemon.DaemonLauncher --startd &> ${log_file}
+        wait $!
+        sleep 4
+        popd > /dev/null
+        # Get the pid with ps
+        ps -ef | awk '/java.*org.apache.felix.main.Main$/ {print $2}' > ${pid_file}
+        retval=$?
+        sleep 10
+        echo "Started ${app_name}"
+	kill `/sbin/pidof gpCmdEvent`
+/data/gpitlc/dev/gpi/current/rel/source/tlc/bin/linux64/gpCmdEvent -daemon -c /data/gpitlc/dev/gpi/current/rel/source/tlc/config/CONFIG.CmdEvent
+        return $retval
+    else
+        echo "${app_name} already running with pid ${pid}"
+    fi
+}
+
+#
+# Gracefully stop the GMP
+#
+function stopContainer() {
+  if [ ! -z ${pid_isrunning} ]; then
+    echo "Stopping ${app_name} with pid ${pid}"
+    exec java -cp ${app_root}/bin/pax-runner-${PAX_RUNNER_VERSION}.jar org.ops4j.pax.runner.daemon.DaemonLauncher --stop
+    wait $!
+    retval=$?
+    sleep 10
+    return $retval
+  else
+    echo "${app_name} not running"
+  fi
+}
+
+
+#
+# Kill the GMP process
+#
+function killContainer() {
+  if [ ! -z ${pid_isrunning} ]; then
+    echo "Killing ${app_name} with pid ${pid}"
+    kill `cat ${pid_file}`
+  else
+    echo "${app_name} not running"
+  fi
+}
+
+#
+# Check if GMP is still running
+#
+function containerStatus() {
+  if [ ! -z ${pid_isrunning} ]; then
+    echo "${app_name} is running with pid ${pid}"
+  else
+    echo "${app_name} not running"
+  fi
+}
+
+# Reenable as the script could be called without parameters
+set +u
+
+#
+# - main command dispatch ...
+#
+case "$1" in
+    start)
+      startContainer
+      ;;
+    stop)
+      stopContainer
+      ;;
+    kill)
+      killContainer
+      ;;
+    restart)
+      stopContainer
+      startContainer
+      ;;
+    status)
+      containerStatus
+      ;;
+    *)
+      echo "Usage: ${app_name} {start|stop|status|kill}"
+      exit 1
+      ;;
+esac
+# End of main
+exit 0
