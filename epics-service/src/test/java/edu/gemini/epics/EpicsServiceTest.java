c915a7db4be3ef0f30300707299b377b612efa8f
Javier Lührs <jluhrs@gemini.edu>
2021-03-16 14:53:39 -0300

    SEQNG-1255 Added selection of EventDispatcher for CAJ in epics-service.

--- epics-service/src/test/java/edu/gemini/epics/EpicsServiceTest.java
+++ epics-service/src/test/java/edu/gemini/epics/EpicsServiceTest.java
@@ -2,8 +2,13 @@ package edu.gemini.epics;
 
 import com.cosylab.epics.caj.CAJContext;
 import gov.aps.jca.Context;
+import gov.aps.jca.event.DirectEventDispatcher;
+import gov.aps.jca.event.LatestMonitorOnlyQueuedEventDispatcher;
+import gov.aps.jca.event.QueuedEventDispatcher;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.nio.file.FileAlreadyExistsException;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
@@ -81,5 +86,73 @@ public class EpicsServiceTest {
         assertNotNull(new EpicsService("127.0.0.1 0.0.0.0", 1.0, 0));
     }
 
+    @Test
+    public void testSetDirectEventDispatcher() {
+        epicsService.setEventDispatcherSelector(new DirectEventDispatcherSelector());
+
+        epicsService.startService();
+
+        assert(epicsService.getJCAContext().getEventDispatcher() instanceof DirectEventDispatcher);
+    }
+
+    @Test
+    public void testSetQueuedEventDispatcher() {
+        int QUEUE_LIMIT = 66;
+        int PRIORITY = Thread.NORM_PRIORITY + 1;
+        int CHANNEL_QUEUE_LIMIT = 4;
+
+        epicsService.setEventDispatcherSelector(new QueuedEventDispatcherSelector()
+                .setPriority(PRIORITY)
+                .setPerChannelLimit(CHANNEL_QUEUE_LIMIT)
+                .setQueueLimit(QUEUE_LIMIT)
+        );
+
+        epicsService.startService();
+
+        assert(epicsService.getJCAContext().getEventDispatcher() instanceof QueuedEventDispatcher);
+        QueuedEventDispatcher eventDispatcher = (QueuedEventDispatcher) epicsService.getJCAContext().getEventDispatcher();
+        assertEquals(eventDispatcher.getPriority(), PRIORITY);
+        // There are no "getters" for the next two attributes. The next best thing is to test the properties that
+        // QueuedEventDispatcher uses
+        assertEquals(Integer.parseInt(System.getProperty(QueuedEventDispatcher.class.getName() + ".queue_limit")), QUEUE_LIMIT);
+        assertEquals(Integer.parseInt(System.getProperty(QueuedEventDispatcher.class.getName() + ".channel_queue_limit")), CHANNEL_QUEUE_LIMIT);
+    }
+
+
+    @Test
+    public void testSetLatestMonitorOnlyQueuedEventDispatcherSelector() {
+        int QUEUE_LIMIT = 66;
+        int PRIORITY = Thread.NORM_PRIORITY + 1;
+        int CHANNEL_QUEUE_LIMIT = 4;
+        String FILE_OUTPUT = "stdout";
+
+        epicsService.setEventDispatcherSelector(new LatestMonitorOnlyQueuedEventDispatcherSelector()
+                .setPriority(PRIORITY)
+                .setPerChannelLimit(CHANNEL_QUEUE_LIMIT)
+                .setQueueLimit(QUEUE_LIMIT)
+                .setMonitorOutput(FILE_OUTPUT)
+        );
+
+        epicsService.startService();
+
+        assert(epicsService.getJCAContext().getEventDispatcher() instanceof LatestMonitorOnlyQueuedEventDispatcher);
+        LatestMonitorOnlyQueuedEventDispatcher eventDispatcher =
+                (LatestMonitorOnlyQueuedEventDispatcher) epicsService.getJCAContext().getEventDispatcher();
+        assertEquals(eventDispatcher.getPriority(), PRIORITY);
+        // There are no "getters" for the next attributes. The next best thing is to test the properties that
+        // LatestMonitorOnlyQueuedEventDispatcher uses
+        assertEquals(
+            Integer.parseInt(System.getProperty(LatestMonitorOnlyQueuedEventDispatcher.class.getName() + ".queue_limit")),
+            QUEUE_LIMIT
+        );
+        assertEquals(
+            Integer.parseInt(System.getProperty(LatestMonitorOnlyQueuedEventDispatcher.class.getName() + ".channel_queue_limit")),
+            CHANNEL_QUEUE_LIMIT
+        );
+        assertEquals(
+            System.getProperty(LatestMonitorOnlyQueuedEventDispatcher.class.getName() + ".monitor_output"),
+            FILE_OUTPUT
+        );
+    }
 
 }
